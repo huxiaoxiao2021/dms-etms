a0df4f7c40083c96277bc2d03ef7824c68ef2b7d
guoyongzhi <guoyongzhi@jd.com>
2015-08-25 16:40:03 +0800

    根据箱号获取装在单表数据 findWaybillInLoadBill

--- bluedragon-distribution-core/src/main/java/com/jd/bluedragon/distribution/globaltrade/service/LoadBillService.java
+++ bluedragon-distribution-core/src/main/java/com/jd/bluedragon/distribution/globaltrade/service/LoadBillService.java
@@ -1,21 +1,44 @@
 package com.jd.bluedragon.distribution.globaltrade.service;
 
+import java.util.List;
+import java.util.Map;
+
 import com.jd.bluedragon.distribution.globaltrade.domain.LoadBill;
 import com.jd.bluedragon.distribution.globaltrade.domain.LoadBillReport;
 
-import java.util.List;
-
 public interface LoadBillService {
 
-	int add(LoadBill loadBill);
-
-	int update(LoadBill loadBill);
-
 	/**
-	 * 将装载单的审批状态更新为  放行 或 未放行
+	 * 将装载单的审批状态更新为 放行 或 未放行
+	 *
 	 * @param report
 	 */
 	void updateLoadBillStatusByReport(LoadBillReport report);
+
+	/**
+	 * 分页查询装载单
+	 *
+	 * @param params
+	 * @return
+	 */
+	List<LoadBill> findPageLoadBill(Map<String, Object> params);
+
+	/**
+	 * 查询装载单的数量
+	 *
+	 * @param params
+	 * @return
+	 */
+	Integer findCountLoadBill(Map<String, Object> params);
+
+	/**
+	 * 根据批次号初始化装载单
+	 *
+	 * @param sendCode
+	 * @param string
+	 * @param integer
+	 */
+	int initialLoadBill(String sendCode, Integer userId, String userName);
 
     /**
      * 获取装载单表 根据箱号或者订单号获取
--- bluedragon-distribution-core/src/main/java/com/jd/bluedragon/distribution/globaltrade/service/LoadBillServiceImpl.java
+++ bluedragon-distribution-core/src/main/java/com/jd/bluedragon/distribution/globaltrade/service/LoadBillServiceImpl.java
@@ -1,5 +1,6 @@
 package com.jd.bluedragon.distribution.globaltrade.service;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -11,11 +12,17 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.jd.bluedragon.distribution.base.service.SiteService;
 import com.jd.bluedragon.distribution.globaltrade.dao.LoadBillDao;
 import com.jd.bluedragon.distribution.globaltrade.dao.LoadBillReadDao;
 import com.jd.bluedragon.distribution.globaltrade.dao.LoadBillReportDao;
 import com.jd.bluedragon.distribution.globaltrade.domain.LoadBill;
 import com.jd.bluedragon.distribution.globaltrade.domain.LoadBillReport;
+import com.jd.bluedragon.distribution.send.dao.SendDatailReadDao;
+import com.jd.bluedragon.distribution.send.domain.SendDetail;
+import com.jd.bluedragon.utils.PropertiesHelper;
+import com.jd.etms.basic.dto.BaseStaffSiteOrgDto;
+import com.jd.jsf.gd.util.StringUtils;
 
 @Service("loadBillService")
 public class LoadBillServiceImpl implements LoadBillService {
@@ -23,6 +30,17 @@ public class LoadBillServiceImpl implements LoadBillService {
 	private final Log logger = LogFactory.getLog(this.getClass());
 
 	private static final int SUCCESS = 1; // report的status,1为成功,2为失败
+
+	private static final String WAREHOUSE_ID = "globalTrade.loadBill.warehouseId"; // 仓库ID
+
+	private static final String DMS_CODE = "globalTrade.loadBill.dmsCode"; // 全球购的专用分拣中心
+
+	private static final String CTNO = "globalTrade.loadBill.ctno"; // 申报海关编码。默认：5165南沙旅检
+
+	private static final String GJNO = "globalTrade.loadBill.gjno"; // 申报国检编码。默认：000069申报地国检
+
+	private static final String TPL = "globalTrade.loadBill.tpl"; // 物流企业编码。默认：京配编号
+
 	@Autowired
 	private LoadBillDao loadBillDao;
 
@@ -32,14 +50,80 @@ public class LoadBillServiceImpl implements LoadBillService {
 	@Autowired
 	private LoadBillReportDao loadBillReportDao;
 
-	@Override
-	public int add(LoadBill loadBill) {
-		return 0;
-	}
+	@Autowired
+	private SendDatailReadDao sendDatailReadDao;
+
+	@Autowired
+	private SiteService siteService;
 
 	@Override
-	public int update(LoadBill loadBill) {
-		return 0;
+	@Transactional(propagation = Propagation.REQUIRED, readOnly = false)
+	public int initialLoadBill(String sendCode, Integer userId, String userName) {
+		String dmsCode = PropertiesHelper.newInstance().getValue(DMS_CODE);
+		if (StringUtils.isBlank(dmsCode)) {
+			logger.error("LoadBillServiceImpl initialLoadBill with dmsCode is null");
+			return 0;
+		}
+		Map<String, Object> params = new HashMap<String, Object>();
+		params.put("sendCode", sendCode);
+		params.put("dmsCode", Integer.parseInt(dmsCode));
+		List<SendDetail> sendDetailList = sendDatailReadDao.findBySendCodeAndDmsCode(params);
+		if (sendDetailList == null || sendDetailList.size() < 1) {
+			logger.info("LoadBillServiceImpl initialLoadBill with the num of SendDetail is 0");
+			return 0;
+		}
+		List<LoadBill> loadBillList = resolveLoadBill(sendDetailList, userId, userName);
+		for (LoadBill lb : loadBillList) {
+			// 不存在,则添加;存在,则忽略,更新会影响其他功能的更新操作
+			if (loadBillDao.findByPackageBarcode(lb.getPackageBarcode()) == null) {
+				loadBillDao.add(lb);
+			}
+		}
+		return loadBillList.size();
+	}
+
+	private List<LoadBill> resolveLoadBill(List<SendDetail> sendDetailList, Integer userId, String userName) {
+		if (sendDetailList == null || sendDetailList.size() < 1) {
+			return new ArrayList<LoadBill>();
+		}
+		List<LoadBill> loadBillList = new ArrayList<LoadBill>();
+		Map<Integer, String> dmsMap = new HashMap<Integer, String>();
+		for (SendDetail sd : sendDetailList) {
+			LoadBill lb = new LoadBill();
+			// 装载单注入SendDetail的必须字段
+			lb.setWaybillCode(sd.getWaybillCode());
+			lb.setPackageBarcode(sd.getPackageBarcode());
+			lb.setPackageAmount(sd.getPackageNum());
+			lb.setOrderId(sd.getWaybillCode());
+			lb.setBoxCode(sd.getBoxCode());
+			lb.setDmsCode(sd.getCreateSiteCode());
+			lb.setSendTime(sd.getCreateTime()); // 包裹发货数据的创建时间,就是发货时间
+			lb.setSendCode(sd.getSendCode());
+			lb.setWeight(sd.getWeight());
+			lb.setPackageUserCode(sd.getCreateUserCode());
+			lb.setPackageUser(sd.getCreateUser());
+			lb.setPackageTime(sd.getCreateTime());
+			lb.setApprovalCode(LoadBill.BEGINNING); // 装载单初始化状态
+			if (dmsMap.containsKey(sd.getCreateSiteCode())) {
+				lb.setDmsName(dmsMap.get(sd.getCreateSiteCode()));
+			} else {
+				BaseStaffSiteOrgDto site = siteService.getSite(sd.getCreateSiteCode());
+				if (site != null) {
+					dmsMap.put(sd.getCreateSiteCode(), site.getSiteName());
+					lb.setDmsName(site.getSiteName());
+				}
+			}
+			// 注入装载单其他信息
+			lb.setCreateUserCode(userId);
+			lb.setCreateUser(userName);
+			lb.setLoadId(sd.getWaybillCode());// loadId暂时用WaybillCode
+			lb.setWarehouseId(PropertiesHelper.newInstance().getValue(WAREHOUSE_ID));
+			lb.setCtno(PropertiesHelper.newInstance().getValue(CTNO));
+			lb.setGjno(PropertiesHelper.newInstance().getValue(GJNO));
+			lb.setTpl(PropertiesHelper.newInstance().getValue(TPL));
+			loadBillList.add(lb);
+		}
+		return loadBillList;
 	}
 
 	@Override
@@ -60,6 +144,19 @@ public class LoadBillServiceImpl implements LoadBillService {
 		}
 		return loadBillStatusMap;
 	}
+
+	@Override
+	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
+	public List<LoadBill> findPageLoadBill(Map<String, Object> params) {
+		return loadBillReadDao.findPageLoadBill(params);
+	}
+
+	@Override
+	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
+	public Integer findCountLoadBill(Map<String, Object> params) {
+		return loadBillReadDao.findCountLoadBill(params);
+	}
+
     @Override
     @Transactional(propagation = Propagation.REQUIRED, readOnly = false)
     public List<LoadBill> findWaybillInLoadBill(LoadBillReport report){
