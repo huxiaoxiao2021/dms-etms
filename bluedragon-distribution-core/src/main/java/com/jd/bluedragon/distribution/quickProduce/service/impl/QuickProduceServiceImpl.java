0326f3db42568242aa1c714a356465c43490b399
yanghongqiang <yanghongqiang@jd.com>
2015-09-07 17:00:00 +0800

    快生获取运单接口添加注释

--- bluedragon-distribution-core/src/main/java/com/jd/bluedragon/distribution/quickProduce/service/impl/QuickProduceServiceImpl.java
+++ bluedragon-distribution-core/src/main/java/com/jd/bluedragon/distribution/quickProduce/service/impl/QuickProduceServiceImpl.java
@@ -2,10 +2,12 @@ package com.jd.bluedragon.distribution.quickProduce.service.impl;
 
 import com.jd.bluedragon.distribution.quickProduce.domain.QuickProduceWabill;
 import com.jd.bluedragon.distribution.quickProduce.service.QuickProduceService;
+import org.springframework.stereotype.Service;
 
 /**
  * 快生项目获取运单信息
  */
+@Service("quickProduceService")
 public class QuickProduceServiceImpl implements QuickProduceService {
     /**
      * 快生项目获取运单信息
