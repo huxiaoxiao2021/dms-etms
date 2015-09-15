f8dd50e9666cbe749238e9c1fecc824088b9182f
huangliang <huangliang@ZLXX-HUANGLI.360buyAD.local>
2015-09-15 17:47:25 +0800

    新库管查询页面提交

--- bluedragon-distribution-web/src/main/java/com/jd/bluedragon/distribution/web/kuGuan/KuGuanController.java
+++ bluedragon-distribution-web/src/main/java/com/jd/bluedragon/distribution/web/kuGuan/KuGuanController.java
@@ -11,6 +11,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.jd.bluedragon.core.base.StockExportManager;
 import com.jd.bluedragon.distribution.kuguan.domain.KuGuanDomain;
 import com.jd.bluedragon.distribution.kuguan.service.KuGuanService;
 import com.jd.bluedragon.utils.ObjectMapHelper;
@@ -23,12 +25,11 @@ public class KuGuanController {
 	private final Log logger = LogFactory.getLog(this.getClass());
 	
 	@Autowired
-	KuGuanService tKuGuanService;
+	private StockExportManager stockExportManager;
 	
 	@HrmPrivilege("DMS-WEB-QUERY-KUGUANINIT")
 	@RequestMapping(value = "/goListPage", method = RequestMethod.GET)
 	public String goListpage(Model model) {
-		model.addAttribute("ddlType", "1");
 		model.addAttribute("errorMesage", "0");
 		return "kuguan/kuguan";
 	}
@@ -39,47 +40,28 @@ public class KuGuanController {
 
 		Map<String, Object> params = ObjectMapHelper
 				.makeObject2Map(kuGuanDomain);
-		String ddlType = (String) params.get("ddlType");
-		if (params.get("waybillCode") == null || params.get("ddlType") == null) {
+		if (params.get("waybillCode") == null) {
 			logger.error("根据订单号获取库管单信息参数错误");
 			return "kuguan/kuguan";
 		}
-		List<KuGuanDomain> domains = new ArrayList<KuGuanDomain>();
+
 		try {
-			logger.error("根据订单号获取库管单信息参数错误-queryByParams");
-			kuGuanDomain = tKuGuanService.queryByParams(params);
-			if(kuGuanDomain==null)
-				model.addAttribute("errorMesage", "1");
-			else if (kuGuanDomain.getlKdanhao() != null){
-				logger.error("根据订单号获取库管单信息参数错误-queryMingxi");
-				domains = tKuGuanService.queryMingxi(kuGuanDomain.getlKdanhao());
-			}
-			logger.error("根据订单号获取库管单信息参数错误-1");
-			if (domains != null && !domains.isEmpty()) {
-				double zj = 0;
-				int znum= 0;
-				for (KuGuanDomain domain : domains) {
-					String jine = domain.getLbljine();
-					String num = domain.getLblNum();
-					if (jine != null && !jine.equals(""))
-						zj += Double.parseDouble(jine);
-					if (num != null && !num.equals(""))
-						znum += Integer.parseInt(num);
-				}
-				kuGuanDomain.setLblstatistics("合计：" + znum
-						+ "件商品，共计"+zj+"元");
-			}
+			logger.error("根据订单号获取库管单信息-queryByParams");
+			kuGuanDomain = stockExportManager.queryByParams(params);
+			
 		} catch (Exception e) {
 			kuGuanDomain = new KuGuanDomain(); 
-			kuGuanDomain.setDdlType(ddlType);
 			kuGuanDomain.setWaybillCode(null);
 			model.addAttribute("errorMesage", "1");
 			logger.error("根据订单号获取库管单信息服务异常"+e);
 		}
-		model.addAttribute("kuguanLists", domains);
+		model.addAttribute("kuguanLists", kuGuanDomain.getStockDetails());
 		model.addAttribute("kuGuanDomain", kuGuanDomain);
 		return "kuguan/kuguan";
 	}
 	
-	
+	@RequestMapping(value = "/listForYanfa", method = RequestMethod.GET)
+	public String listForYanfa(KuGuanDomain kuGuanDomain, Model model) {
+		return queryOperateLog(kuGuanDomain, model);
+	}
 }
