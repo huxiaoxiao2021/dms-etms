a454298013b2b22b96623c0592ea1a308ac26b5b
wangtingwei <wangtingwei@jd.com>
2015-10-20 14:37:31 +0800

    页面添加权限

--- bluedragon-distribution-web/src/main/java/com/jd/bluedragon/distribution/web/admin/WorkerSettingController.java
+++ bluedragon-distribution-web/src/main/java/com/jd/bluedragon/distribution/web/admin/WorkerSettingController.java
@@ -1,8 +1,11 @@
 package com.jd.bluedragon.distribution.web.admin;
 
+import com.jd.bluedragon.Constants;
 import com.jd.bluedragon.Pager;
+import com.jd.bluedragon.distribution.web.ErpUserClient;
 import com.jd.bluedragon.distribution.worker.domain.TBTaskType;
 import com.jd.bluedragon.distribution.worker.service.TBTaskTypeService;
+import com.jd.bluedragon.utils.PropertiesHelper;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,22 +13,31 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * WORKER配置
  * Created by wangtingwei on 2015/9/29.
  */
 @Controller
-@RequestMapping("admin/worker-setting")
+@RequestMapping("worker-setting")
 public class WorkerSettingController {
 
     private static final Log logger= LogFactory.getLog(WorkerSettingController.class);
 
     @Autowired
     private TBTaskTypeService tbTaskTypeService;
+    private static final String MODIFY_USERS = "modifyUsers";
+    private static final List<String> modifyUsers;
+
+    static {
+        modifyUsers = Arrays.asList(PropertiesHelper.newInstance()
+                .getValue(WorkerSettingController.MODIFY_USERS).split(Constants.SEPARATOR_COMMA));
+    }
+
     /**
      * 任务列表
      * @return
@@ -44,42 +56,62 @@ public class WorkerSettingController {
     }
     @RequestMapping(value = "/create",method = RequestMethod.GET)
     public String create(HttpServletRequest request,Model model) throws Exception{
-        model.addAttribute("lastUrl",request.getHeader("referer"));
-        return "admin/worker-setting/create";
+        ErpUserClient.ErpUser erpUser = ErpUserClient.getCurrUser();
+        if (null!=erpUser&&WorkerSettingController.modifyUsers.contains(erpUser.getUserCode().toLowerCase())) {
+            model.addAttribute("lastUrl", request.getHeader("referer"));
+            return "admin/worker-setting/create";
+        }else{
+            return "index";
+        }
     }
 
     @RequestMapping(value = "/create",method = RequestMethod.POST)
     public String create(TBTaskType domain,String lastLastUrl,Model model) throws Exception {
-        try{
-            tbTaskTypeService.inserSingle(domain);
-            return "redirect:"+lastLastUrl;
-        }catch (Exception ex){
-            logger.error("ERROR",ex);
-            throw ex;
+        ErpUserClient.ErpUser erpUser = ErpUserClient.getCurrUser();
+        if (null!=erpUser&&WorkerSettingController.modifyUsers.contains(erpUser.getUserCode().toLowerCase())) {
+            try {
+                tbTaskTypeService.inserSingle(domain);
+                return "redirect:" + lastLastUrl;
+            } catch (Exception ex) {
+                logger.error("ERROR", ex);
+                throw ex;
+            }
+        }else{
+            return "index";
         }
     }
 
     @RequestMapping(value = "/edit",method = RequestMethod.GET)
     public String edit(int id,HttpServletRequest request,Model model) throws Exception{
-        try{
-            model.addAttribute("model",tbTaskTypeService.readById(id));
-            model.addAttribute("lastUrl",request.getHeader("referer"));
-            return "admin/worker-setting/edit";
-        }catch (Exception e){
-            logger.error("ERROR",e);
-            throw e;
+        ErpUserClient.ErpUser erpUser = ErpUserClient.getCurrUser();
+        if (null!=erpUser&& WorkerSettingController.modifyUsers.contains(erpUser.getUserCode().toLowerCase())) {
+            try {
+                model.addAttribute("model", tbTaskTypeService.readById(id));
+                model.addAttribute("lastUrl", request.getHeader("referer"));
+                return "admin/worker-setting/edit";
+            } catch (Exception e) {
+                logger.error("ERROR", e);
+                throw e;
+            }
+        }else {
+            return "index";
         }
 
     }
 
     @RequestMapping(value = "/edit",method = RequestMethod.POST)
     public String edit(TBTaskType domain,String lastLastUrl,Model model) throws Exception{
-        try{
-            tbTaskTypeService.updateSingleById(domain);
-            return "redirect:"+lastLastUrl;
-        }catch (Exception ex){
-            logger.error("ERROR",ex);
-            throw ex;
+        ErpUserClient.ErpUser erpUser = ErpUserClient.getCurrUser();
+        if (null!=erpUser&&WorkerSettingController.modifyUsers.contains(erpUser.getUserCode().toLowerCase())) {
+            try {
+                tbTaskTypeService.updateSingleById(domain);
+                return "redirect:" + lastLastUrl;
+            } catch (Exception ex) {
+                logger.error("ERROR", ex);
+                throw ex;
+            }
+        }else {
+            return "index";
         }
     }
 }
