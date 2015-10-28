bb48664787047b0ddf23b572ab8b276d1d11c405
wangtingwei <wangtingwei@jd.com>
2015-10-28 13:10:50 +0800

    redis任务插入限流开发：当队列长度超过配置值时，不进行插入REDIS

---
+++ bluedragon-distribution-core/src/main/java/com/jd/bluedragon/distribution/task/service/RedisTaskQueueService.java
@@ -0,0 +1,15 @@
+package com.jd.bluedragon.distribution.task.service;
+
+/**
+ * Created by wangtingwei on 2015/10/28.
+ */
+public interface RedisTaskQueueService {
+
+    /**
+     * 获取REDIS任务队列长度
+     * @param queueName 队列名
+     * @return
+     */
+    long getQueueLength(String queueName);
+
+}
