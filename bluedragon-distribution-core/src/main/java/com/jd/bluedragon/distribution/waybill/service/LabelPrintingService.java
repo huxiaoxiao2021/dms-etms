076a89d87c58aa592eecd68e2438e3011b59dcd6
yanghongqiang <yanghongqiang@jd.com>
2015-12-03 15:30:05 +0800

    包裹标签预分拣代码迁移至分拣中心

---
+++ bluedragon-distribution-core/src/main/java/com/jd/bluedragon/distribution/waybill/service/LabelPrintingService.java
@@ -0,0 +1,46 @@
+package com.jd.bluedragon.distribution.waybill.service;
+
+import com.jd.bluedragon.distribution.waybill.domain.BaseResponseIncidental;
+import com.jd.bluedragon.distribution.waybill.domain.LabelPrintingRequest;
+import com.jd.bluedragon.distribution.waybill.domain.LabelPrintingResponse;
+
+/**
+ * Created by yanghongqiang on 2015/11/30.
+ */
+public interface LabelPrintingService {
+
+    Integer PREPARE_SITE_CODE_NOTHING = 0;
+    String PREPARE_SITE_NAME_NOTHING = "未定位门店";
+
+    Integer PREPARE_SITE_CODE_OVER_LINE = -100;
+    String PREPARE_SITE_NAME_OVER_LINE = "超区分界线";
+
+    Integer PREPARE_SITE_CODE_OVER_AREA = -2;
+    String PREPARE_SITE_NAME_OVER_AREA = "超区";
+
+    Integer PREPARE_SITE_CODE_EMS_DIRECT = 999999999;
+    String PREPARE_SITE_NAME_EMS_DIRECT = "EMS全国直发";
+
+    String SPECIAL_MARK_AIRTRANSPORT ="空";
+    String SPECIAL_MARK_ARAYACAK_CABINET = "柜";
+    String SPECIAL_MARK_LOCAL_SCHEDULE = "调";
+    String SPECIAL_MARK_ARAYACAK_SITE = "提";
+    String SPECIAL_MARK_CROWD_SOURCING = "众";
+
+    int AIR_TRANSPORT = 1;
+    int ARAYACAK_CABINET = 1;
+    int LOCAL_SCHEDULE = 1;
+
+    static String ONLINE_PAYMENT="在线支付";
+    static int ONLINE_PAYMENT_SIGN = 4;
+
+    //自提点标示
+    static int ARAYACAK_SIGN = 64;
+
+    /**
+     * 包裹标签打印核心方法
+     * @return
+     */
+    BaseResponseIncidental<LabelPrintingResponse> packageLabelPrint(LabelPrintingRequest request);
+
+}
