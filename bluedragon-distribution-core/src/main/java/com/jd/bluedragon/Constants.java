e20b7f8997f25fb1c2cd086faff9e41eed3ea999
bjcaoyunkun <caoyunkun@jd.com>
2016-01-26 14:03:08 +0800

    Revert "消除无用代码及添加import"
    This reverts commit dce89e097a7f23f6e416f1a5172421a95f73055e.

--- bluedragon-distribution-core/src/main/java/com/jd/bluedragon/Constants.java
+++ bluedragon-distribution-core/src/main/java/com/jd/bluedragon/Constants.java
@@ -137,6 +137,7 @@ public class Constants {
     public static final Integer POP_LBP = 23;
     public static final Integer POP_SOPL = 25;
     public static final Integer ORDER_TYPE_B = 10000;
+    public static final char WAYBILL_SIGN_B='3';
     
     public static final Integer POP_SOP_EMS_CODE = 999999999;
     public static final String POP_SOP_EMS_NAME = "EMS全国直发";
