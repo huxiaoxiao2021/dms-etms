be2ef75fd0a907a15c783b7eea6bf67e85766a5b
yangwubing <yangwubing@jd.com>
2016-02-24 14:35:44 +0800

    1.删除无用单元测试用例。 2.交换mysql为主库，oralce为从库。

--- bluedragon-distribution-core/src/test/java/com/jd/bluedragon/distribution/dao/common/AbstractDaoIntegrationTest.java
+++ bluedragon-distribution-core/src/test/java/com/jd/bluedragon/distribution/dao/common/AbstractDaoIntegrationTest.java
@@ -5,7 +5,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = SpringAppContextConfigPath.MYSQL_APP_CONTEXT_PATH)
+@ContextConfiguration(locations = SpringAppContextConfigPath.ORACLE_APP_CONTEXT_PATH)
 public abstract class AbstractDaoIntegrationTest {
 
 }
