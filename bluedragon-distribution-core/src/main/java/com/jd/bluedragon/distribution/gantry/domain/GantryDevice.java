d1dc5b824053e670046e972fef48aa44116b8b18
dudong <dudong@jd.com>
2016-03-10 10:33:29 +0800

    龙门架设备dao,service,domain

---
+++ bluedragon-distribution-core/src/main/java/com/jd/bluedragon/distribution/gantry/domain/GantryDevice.java
@@ -0,0 +1,9 @@
+package com.jd.bluedragon.distribution.gantry.domain;
+
+/**
+ * @author dudong
+ * @version 1.0
+ * @date 2016/3/10
+ */
+public class GantryDevice {
+}
---
+++ bluedragon-distribution-core/src/main/java/com/jd/bluedragon/distribution/gantry/service/impl/GantryDeviceServiceImpl.java
@@ -0,0 +1,11 @@
+package com.jd.bluedragon.distribution.gantry.service.impl;
+
+import com.jd.bluedragon.distribution.gantry.service.GantryDeviceService;
+
+/**
+ * @author dudong
+ * @version 1.0
+ * @date 2016/3/10
+ */
+public class GantryDeviceServiceImpl implements GantryDeviceService{
+}
