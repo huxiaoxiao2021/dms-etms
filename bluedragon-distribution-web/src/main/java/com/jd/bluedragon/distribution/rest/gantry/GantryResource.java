5008149ff029c2515921749e751a74afa197d5ea
dudong <dudong@jd.com>
2016-03-10 10:30:00 +0800

    龙门架包信息定义

---
+++ bluedragon-distribution-web/src/main/java/com/jd/bluedragon/distribution/rest/gantry/GantryResource.java
@@ -0,0 +1,9 @@
+package com.jd.bluedragon.distribution.rest.gantry;
+
+/**
+ * @author dudong
+ * @version 1.0
+ * @date 2016/3/10
+ */
+public class GantryResource {
+}
---
+++ bluedragon-distribution-web/src/main/java/com/jd/bluedragon/distribution/web/gantry/GantryController.java
@@ -0,0 +1,9 @@
+package com.jd.bluedragon.distribution.web.gantry;
+
+/**
+ * @author dudong
+ * @version 1.0
+ * @date 2016/3/9
+ */
+public class GantryController {
+}
