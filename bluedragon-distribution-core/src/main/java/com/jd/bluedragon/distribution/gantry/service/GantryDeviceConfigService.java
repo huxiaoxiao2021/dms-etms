d74f0bdb4e785ac2cde037a1a1b682a8e30bf8c2
yanghongqiang <yanghongqiang@jd.com>
2016-03-11 17:10:50 +0800

    龙门架发货配置信息花取接口

---
+++ bluedragon-distribution-core/src/main/java/com/jd/bluedragon/distribution/gantry/service/GantryDeviceConfigService.java
@@ -0,0 +1,17 @@
+package com.jd.bluedragon.distribution.gantry.service;
+
+import com.jd.bluedragon.distribution.gantry.domain.GantryDeviceConfig;
+
+import java.sql.Date;
+
+/**
+ * Created by yanghongqiang on 2016/3/11.
+ */
+public interface GantryDeviceConfigService {
+
+    /**
+     * @param gatryNum 龙门架编号
+     * @param packageOperateTime 龙门架上传的包裹交接时间
+     */
+    GantryDeviceConfig getGantryDeviceOperateType(String gatryNum,Date packageOperateTime);
+}
