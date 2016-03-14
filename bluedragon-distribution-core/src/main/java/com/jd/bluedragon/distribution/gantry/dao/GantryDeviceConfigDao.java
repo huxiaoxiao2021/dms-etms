a8784f413aeef840f26f892fda1b18576416e5db
wangtingwei <wangtingwei@jd.com>
2016-03-14 16:12:22 +0800

    xxxxxxxxxxxx

--- bluedragon-distribution-core/src/main/java/com/jd/bluedragon/distribution/gantry/dao/GantryDeviceConfigDao.java
+++ bluedragon-distribution-core/src/main/java/com/jd/bluedragon/distribution/gantry/dao/GantryDeviceConfigDao.java
@@ -1,13 +1,14 @@
 package com.jd.bluedragon.distribution.gantry.dao;
 
 
+import com.jd.bluedragon.common.dao.BaseDao;
 import com.jd.bluedragon.distribution.gantry.domain.GantryDeviceConfig;
 import java.util.Date;
 
 /**
  * Created by yanghongqiang on 2016/3/11.
  */
-public class GantryDeviceConfigDao {
+public class GantryDeviceConfigDao extends BaseDao<GantryDeviceConfig> {
     public GantryDeviceConfig getGantryDeviceOperateType(String gantryNum,Date packageOperateTime){
         return  null;
     }
