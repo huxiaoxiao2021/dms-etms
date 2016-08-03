aa261398320342dd45829c657de0bf3176a7f9e2
yollock <yollock@163.com>
2016-08-03 14:30:03 +0800

    分拣计划2.0 样式

--- bluedragon-distribution-web/src/main/webapp/static/js/sortscheme/sort-scheme-index.js
+++ bluedragon-distribution-web/src/main/webapp/static/js/sortscheme/sort-scheme-index.js
@@ -223,10 +223,10 @@ function doQueryCrossSorting(params) {
                 temp += "<td>" + (dataList[i].receFlag == 1 ? '接收' : '未接收') + "</td>";
                 temp += "<td>" + (dataList[i].receTime == null ? '' : dataList[i].receTime) + "</td>";
                 temp += "<td>" + (dataList[i].yn == 1 ? '<font color="red">激活</font>' : '未激活') + "</td>";
-                temp += "<td>" + "<input type='button' value='批量导入' onclick='goImportExcel(" + dataList[i].id + ")' style='margin-right:10px;'>"
-                    + "<input type='button' value='批量导出' onclick='exportExcel(" + dataList[i].id + ")' style='margin-right:10px;'>"
-                    + "<input type='button' value='删除' onclick='sortSchemeDelete(" + dataList[i].id + ")' style='margin-right:10px;'>"
-                    + "<input type='button' value='查看明细' onclick='goDetailClick(" + dataList[i].id + ")'>" + "</td>";
+                temp += "<td>" + "<input type='button' value='导入' onclick='goImportExcel(" + dataList[i].id + ")' style='margin-right:5px;'>"
+                    + "<input type='button' value='导出' onclick='exportExcel(" + dataList[i].id + ")' style='margin-right:5px;'>"
+                    + "<input type='button' value='删除' onclick='sortSchemeDelete(" + dataList[i].id + ")' style='margin-right:5px;'>"
+                    + "<input type='button' value='明细' onclick='goDetailClick(" + dataList[i].id + ")'>" + "</td>";
                 temp += "</tr>";
             }
             $("#paperTable tbody").html(temp);
