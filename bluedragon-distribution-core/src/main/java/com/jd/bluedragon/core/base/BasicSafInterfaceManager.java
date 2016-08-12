f87054b3909a9c5dde4a457cc318286bda64a6f5
yangbo41 <yangbo7@jd.com>
2016-08-12 17:47:25 +0800

    跨箱号中转

---
+++ bluedragon-distribution-core/src/main/java/com/jd/bluedragon/core/base/BasicSafInterfaceManager.java
@@ -0,0 +1,21 @@
+package com.jd.bluedragon.core.base;
+
+import com.jd.ql.basic.domain.BaseResult;
+import com.jd.ql.basic.domain.CrossDmsBox;
+import com.jd.ql.basic.dto.BaseDmsStoreDto;
+
+public interface BasicSafInterfaceManager {
+
+    /**
+     * @param cky2
+     * @param orgId  机构号
+     * @param storeId 仓储号
+     * @return
+     */
+    public abstract BaseResult<BaseDmsStoreDto> getDmsInfoByStoreInfo(Integer cky2, Integer orgId,
+                                                                      Integer storeId);
+
+    public BaseResult<CrossDmsBox> getCrossDmsBoxByOriAndDes(Integer createSiteCode, Integer targetId);
+
+    BaseResult<String> getCrossDmsBox(Integer createSiteCode, Integer receiveSiteCode);
+}
