081e07c6a466a372f012f0ea2b2188b9bf07467a
dudong <dudong@jd.com>
2016-08-24 10:16:22 +0800

    基础资料升级员工ID序列

--- bluedragon-distribution-core/src/main/java/com/jd/bluedragon/core/base/BaseMajorManager.java
+++ bluedragon-distribution-core/src/main/java/com/jd/bluedragon/core/base/BaseMajorManager.java
@@ -73,4 +73,6 @@ public interface BaseMajorManager {
 
     public BaseStaffSiteOrgDto getBaseStaffByStaffIdNoCache(Integer paramInteger);
 
+    public BaseStaffSiteOrgDto getBaseStaffByErpNoCache(String erp);
+
 }
