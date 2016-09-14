abbab60482c895bdbc3484c27e19985394b795bb
wuzuxiang <wuzuxiang@jd.com>
2016-09-14 13:28:15 +0800

    阿尔法打印插件的自动升级--JssVersionServiceImpl 修改JSF接口

--- bluedragon-distribution-core/src/main/java/com/jd/bluedragon/alpha/jss/JssVersionServiceImpl.java
+++ bluedragon-distribution-core/src/main/java/com/jd/bluedragon/alpha/jss/JssVersionServiceImpl.java
@@ -91,7 +91,8 @@ public class JssVersionServiceImpl implements JssVersionService {
     public URI downloadVersion(String versionId)throws MalformedURLException {
         JingdongStorageService jss = getJss();
 
-        URI uri = jss.bucket(bucket).object(versionId).generatePresignedUrl(10000);//获得带有预签名的下载地址timeout == 10000
+        String key = versionId + ".rar";/** JSS的key值是版本号加上.rar的文件后缀 **/
+        URI uri = jss.bucket(bucket).object(key).generatePresignedUrl(10000);//获得带有预签名的下载地址timeout == 10000
         return uri;
     }
 
