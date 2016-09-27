028f5f627f559dc2030a605c4a51047e27c4e021
dudong <dudong@jd.com>
2016-09-27 14:33:02 +0800

    增加部分接口查索引表的逻辑,修改dao名字保持一致

--- bluedragon-distribution-core/src/main/java/com/jd/bluedragon/distribution/send/dao/SendDetailReadRouterDao.java
+++ bluedragon-distribution-core/src/main/java/com/jd/bluedragon/distribution/send/dao/SendDetailReadRouterDao.java
@@ -3,6 +3,7 @@ package com.jd.bluedragon.distribution.send.dao;
 import com.jd.bluedragon.distribution.api.response.SendBoxDetailResponse;
 import com.jd.bluedragon.distribution.base.dao.KvIndexDao;
 import com.jd.bluedragon.distribution.send.domain.SendDetail;
+import com.jd.bluedragon.utils.CollectionHelper;
 import com.jd.bluedragon.utils.StringHelper;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -13,22 +14,22 @@ import java.util.*;
 /**
  * Created by dudong on 2016/9/26.
  */
-public class SendDetailReadIndexDao extends SendDatailReadDao {
+public class SendDetailReadRouterDao extends SendDatailReadDao {
 
-    private static final Log logger = LogFactory.getLog(SendDetailReadIndexDao.class);
+    private static final Log logger = LogFactory.getLog(SendDetailReadRouterDao.class);
 
     @Autowired
     private KvIndexDao kvIndexDao;
 
-    @Override
-    public List<SendDetail> findUpdatewaybillCodeMessage(List<String> queryCondition) {
-        return super.findUpdatewaybillCodeMessage(queryCondition);
-    }
-
-    @Override
-    public List<String> findWaybillByBoxCode(String boxCode) {  //// FIXME: 2016/9/26
-        return super.findWaybillByBoxCode(boxCode);
-    }
+//    @Override
+//    public List<SendDetail> findUpdatewaybillCodeMessage(List<String> queryCondition) {
+//        return super.findUpdatewaybillCodeMessage(queryCondition);
+//    }
+//
+//    @Override
+//    public List<String> findWaybillByBoxCode(String boxCode) {  //// FIXME: 2016/9/26
+//        return super.findWaybillByBoxCode(boxCode);
+//    }
 
     @Override
     public List<SendBoxDetailResponse> findSendBoxByWaybillCode(String waybillCode) {  //// FIXME: 2016/9/26
@@ -38,6 +39,9 @@ public class SendDetailReadIndexDao extends SendDatailReadDao {
         try {
             List<SendBoxDetailResponse> sendResponseList = new ArrayList<SendBoxDetailResponse>();
             List<Integer> createSiteCodes = kvIndexDao.queryCreateSiteCodesByKey(waybillCode);
+            if (null == createSiteCodes || createSiteCodes.size() <= 0) {
+                return super.findSendBoxByWaybillCode(waybillCode);
+            }
             Map<String, Object> paramMap = new HashMap<String, Object>();
             paramMap.put("waybillCode", waybillCode);
             paramMap.put("createSiteCode", StringHelper.join(createSiteCodes,","));
@@ -64,8 +68,8 @@ public class SendDetailReadIndexDao extends SendDatailReadDao {
         return super.findSendBoxByWaybillCode(waybillCode);
     }
 
-    @Override
-    public List<SendDetail> findBySendCodeAndDmsCode(Map<String, Object> params) {
-        return super.findBySendCodeAndDmsCode(params);
-    }
+//    @Override
+//    public List<SendDetail> findBySendCodeAndDmsCode(Map<String, Object> params) {
+//        return super.findBySendCodeAndDmsCode(params);
+//    }
 }
---
+++ bluedragon-distribution-core/src/main/java/com/jd/bluedragon/distribution/send/dao/SendDetailRouterDao.java
@@ -0,0 +1,225 @@
+package com.jd.bluedragon.distribution.send.dao;
+
+import com.jd.bluedragon.distribution.base.dao.KvIndexDao;
+import com.jd.bluedragon.distribution.base.domain.KvIndex;
+import com.jd.bluedragon.distribution.send.domain.SendDetail;
+import com.jd.bluedragon.utils.JsonHelper;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * Created by dudong on 2016/9/26.
+ */
+public class SendDetailRouterDao extends SendDatailDao{
+
+    private static final Log logger = LogFactory.getLog(SendDetailRouterDao.class);
+
+    @Autowired
+    private KvIndexDao kvIndexDao;
+
+    @Override
+    public Integer add(String namespace, SendDetail entity) { //// FIXME: 2016/9/26
+        try {
+            KvIndex kvIndex = new KvIndex();
+            kvIndex.setKeyword(entity.getWaybillCode());
+            kvIndex.setValue(String.valueOf(entity.getCreateSiteCode()));
+            kvIndexDao.add(KvIndexDao.namespace,kvIndex);
+            kvIndex.setKeyword(entity.getBoxCode());
+            kvIndexDao.add(kvIndexDao.namespace,kvIndex);
+        } catch (Throwable e) {
+            logger.error("保存到中间索引表失败 " + JsonHelper.toJson(entity), e);
+        }
+        return super.add(namespace, entity);
+    }
+
+//    @Override
+//    public Integer update(String namespace, SendDetail entity) {
+//        return super.update(namespace, entity);
+//    }
+//
+//    @Override
+//    public List<SendDetail> querySendDatailsByBoxCode(SendDetail tSendDatail) { //// FIXME: 2016/9/26
+//        return super.querySendDatailsByBoxCode(tSendDatail);
+//    }
+//
+//    @Override
+//    public List<SendDetail> querySendDatailsByPackageCode(SendDetail tSendDatail) { //// FIXME: 2016/9/26
+//        return super.querySendDatailsByPackageCode(tSendDatail);
+//    }
+//
+//    @Override
+//    public SendDetail querySendDatailBySendStatus(SendDetail tSendDatail) {
+//        return super.querySendDatailBySendStatus(tSendDatail);
+//    }
+//
+//    @Override
+//    public SendDetail queryOneSendDatailByBoxCode(String boxCode) { //// FIXME: 2016/9/26
+//        return super.queryOneSendDatailByBoxCode(boxCode);
+//    }
+//
+//    @Override
+//    public List<SendDetail> findSendDetails(SendDetail param) {
+//        return super.findSendDetails(param);
+//    }
+//
+//    @Override
+//    public List<SendDetail> queryBySiteCodeAndSendCode(SendDetail query) {
+//        return super.queryBySiteCodeAndSendCode(query);
+//    }
+//
+//    @Override
+//    public List<SendDetail> querySendDatailsBySelective(SendDetail querySendDatail) {
+//        return super.querySendDatailsBySelective(querySendDatail);
+//    }
+//
+//    @Override
+//    public boolean updateSendDatail(SendDetail SendDatail) {
+//        return super.updateSendDatail(SendDatail);
+//    }
+//
+//    @Override
+//    public boolean cancelSendDatail(SendDetail sendDetial) {
+//        return super.cancelSendDatail(sendDetial);
+//    }
+//
+//    @Override
+//    public List<SendDetail> findUpdatewaybillCodeMessage() {
+//        return super.findUpdatewaybillCodeMessage();
+//    }
+//
+//    @Override
+//    public boolean updatewaybillCodeStatus(SendDetail sendDetial) {
+//        return super.updatewaybillCodeStatus(sendDetial);
+//    }
+//
+//    @Override
+//    public boolean updateSendStatusByPackage(SendDetail sendDetial) {
+//        return super.updateSendStatusByPackage(sendDetial);
+//    }
+//
+//    @Override
+//    public boolean updateWeight(SendDetail sendDetail) {
+//        return super.updateWeight(sendDetail);
+//    }
+//
+//    @Override
+//    public Boolean canCancel(SendDetail sendDetail) {
+//        return super.canCancel(sendDetail);
+//    }
+//
+//    @Override
+//    public Boolean canCancel2(SendDetail sendDetail) {
+//        return super.canCancel2(sendDetail);
+//    }
+//
+//    @Override
+//    public Boolean canCancelFuzzy(SendDetail sendDetail) {
+//        return super.canCancelFuzzy(sendDetail);
+//    }
+//
+//    @Override
+//    public Integer updateCancel(SendDetail sendDetail) {
+//        return super.updateCancel(sendDetail);
+//    }
+//
+//    @Override
+//    public Integer updateCancelBatch(SendDetail sendDetail) {
+//        return super.updateCancelBatch(sendDetail);
+//    }
+//
+//    @Override
+//    public List<SendDetail> querySendCodesByWaybills(String waybillCodeIn) { //// FIXME: 2016/9/26
+//        return super.querySendCodesByWaybills(waybillCodeIn);
+//    }
+//
+//    @Override
+//    public List<SendDetail> querySendCodesByWaybill(String waybillCodeIn) {
+//        return super.querySendCodesByWaybill(waybillCodeIn);
+//    }
+//
+//    @Override
+//    public List<SendDetail> getSendSiteID(SendDetail sendDetail) {
+//        return super.getSendSiteID(sendDetail);
+//    }
+//
+//    @Override
+//    public boolean checkSendByPackage(SendDetail sendDetail) {
+//        return super.checkSendByPackage(sendDetail);
+//    }
+//
+//    @Override
+//    public boolean checkSendSendCode(SendDetail sendDetail) {
+//        return super.checkSendSendCode(sendDetail);
+//    }
+//
+//    @Override
+//    public List<SendDetail> findOrder(SendDetail query) {
+//        return super.findOrder(query);
+//    }
+//
+//    @Override
+//    public List<SendDetail> findDeliveryPackageBySite(SendDetail query) { //// FIXME: 2016/9/26
+//        return super.findDeliveryPackageBySite(query);
+//    }
+//
+//    @Override
+//    public List<SendDetail> findDeliveryPackageByCode(SendDetail query) { //// FIXME: 2016/9/26
+//        return super.findDeliveryPackageByCode(query);
+//    }
+//
+//    @Override
+//    public List<SendDetail> querySortingDiff(SendDetail query) {
+//        return super.querySortingDiff(query);
+//    }
+//
+//    @Override
+//    public List<SendDetail> queryWithoutPackageNum(SendDetail query) {
+//        return super.queryWithoutPackageNum(query);
+//    }
+//
+//    @Override
+//    public Integer updatePackageNum(SendDetail sendDetail) {
+//        return super.updatePackageNum(sendDetail);
+//    }
+//
+//    @Override
+//    public Integer cancelDelivery(SendDetail sendDetail) {
+//        return super.cancelDelivery(sendDetail);
+//    }
+//
+//    @Override
+//    public List<SendDetail> queryBySendCodeAndSendType(SendDetail query) {
+//        return super.queryBySendCodeAndSendType(query);
+//    }
+//
+//    @Override
+//    public List<SendDetail> batchQuerySendDList(SendDetail querySendDatail) {
+//        return super.batchQuerySendDList(querySendDatail);
+//    }
+//
+//    @Override
+//    public List<SendDetail> queryWaybillsByPackCode(String packCode) {
+//        return super.queryWaybillsByPackCode(packCode);
+//    }
+//
+//    @Override
+//    public List<SendDetail> queryWaybillsByBoxCode(String boxCode) {
+//        return super.queryWaybillsByBoxCode(boxCode);
+//    }
+//
+//    @Override
+//    public List<SendDetail> queryWaybillsBySendCode(String sendCode) {
+//        return super.queryWaybillsBySendCode(sendCode);
+//    }
+//
+//    @Override
+//    public List<SendDetail> queryWaybillsByDepartID(Long departureID) {
+//        return super.queryWaybillsByDepartID(departureID);
+//    }
+//
+//    @Override
+//    public List<String> querySendCodesByDepartID(Long departureID) {
+//        return super.querySendCodesByDepartID(departureID);
+//    }
+}
