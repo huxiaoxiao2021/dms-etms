04c476d1dfbb9d6b615d4f2c0e90217ada0c0f7b
wangtingwei <wangtingwei@jd.com>
2016-12-09 14:27:39 +0800

    生成发货批次号

--- bluedragon-distribution-core/src/main/java/com/jd/bluedragon/distribution/auto/dao/ScannerFramwBatchSendDao.java
+++ bluedragon-distribution-core/src/main/java/com/jd/bluedragon/distribution/auto/dao/ScannerFramwBatchSendDao.java
@@ -2,9 +2,38 @@ package com.jd.bluedragon.distribution.auto.dao;
 
 import com.jd.bluedragon.common.dao.BaseDao;
 import com.jd.bluedragon.distribution.auto.domain.ScannerFrameBatchSend;
+import com.jd.bluedragon.utils.DateHelper;
+
+import java.util.*;
 
 /**
  * Created by wangtingwei on 2016/12/8.
  */
 public class ScannerFramwBatchSendDao extends BaseDao<ScannerFrameBatchSend> {
+    private static final String NAMESPACE=ScannerFramwBatchSendDao.class.getName();
+    private static final String selectCurrentBatchSend_ID = NAMESPACE + ".selectCurrentBatchSend";
+    private static final String MACHINE_ID = "machineId";
+    private static final String RECEIVE_SITE_CODE = "receiveSiteCode";
+    private static final String OPERATE_TIME = "operateTime";
+    private static final String OPERATE_TIME_SUB_24_HOURS = "operateTimeSub24Hours";
+
+    public Integer add(ScannerFrameBatchSend entity) {
+        return super.add(NAMESPACE, entity);
+    }
+
+    /**
+     *
+     * @param machineId         龙门架注册号
+     * @param receiveSiteCode   接收站点
+     * @param operateTime       龙门架扫描时间
+     * @return
+     */
+    public ScannerFrameBatchSend selectCurrentBatchSend(long machineId,long receiveSiteCode,Date operateTime){
+        Map<String,Object> map=new HashMap<String, Object>(4);
+        map.put(MACHINE_ID,machineId);
+        map.put(RECEIVE_SITE_CODE,receiveSiteCode);
+        map.put(OPERATE_TIME,operateTime);
+        map.put(OPERATE_TIME_SUB_24_HOURS,DateHelper.add(operateTime,Calendar.HOUR,-24));
+        return super.getSqlSession().selectOne(selectCurrentBatchSend_ID,map);
+    }
 }
--- bluedragon-distribution-core/src/main/java/com/jd/bluedragon/distribution/auto/service/ScannerFrameBatchSendServiceImpl.java
+++ bluedragon-distribution-core/src/main/java/com/jd/bluedragon/distribution/auto/service/ScannerFrameBatchSendServiceImpl.java
@@ -1,7 +1,13 @@
 package com.jd.bluedragon.distribution.auto.service;
 
+import com.jd.bluedragon.common.domain.SiteEntity;
+import com.jd.bluedragon.distribution.auto.dao.ScannerFramwBatchSendDao;
 import com.jd.bluedragon.distribution.auto.domain.ScannerFrameBatchSend;
+import com.jd.bluedragon.distribution.base.service.SiteService;
 import com.jd.bluedragon.distribution.gantry.domain.GantryDeviceConfig;
+import com.jd.bluedragon.utils.SerialRuleUtil;
+import com.jd.ql.basic.dto.BaseStaffSiteOrgDto;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.Date;
 
@@ -10,13 +16,43 @@ import java.util.Date;
  */
 public class ScannerFrameBatchSendServiceImpl implements ScannerFrameBatchSendService {
 
+    private static final byte YN_DEFAULT=1;
+    @Autowired
+    private ScannerFramwBatchSendDao scannerFramwBatchSendDao;
+
+    @Autowired
+    private SiteService siteService;
+
     @Override
     public ScannerFrameBatchSend getAndGenerate(Date operateTime, Integer receiveSiteCode, GantryDeviceConfig config) {
-        return null;
+        if(null==config){
+            throw new RuntimeException("the parameter of config can not be null");
+        }
+
+        ScannerFrameBatchSend batchSend=scannerFramwBatchSendDao.selectCurrentBatchSend(config.getMachineId(),receiveSiteCode,operateTime);
+        if(null==batchSend){
+            batchSend=new ScannerFrameBatchSend();
+            batchSend.setCreateSiteCode(config.getCreateSiteCode());
+            batchSend.setCreateSiteName(config.getCreateSiteName());
+            batchSend.setReceiveSiteCode(receiveSiteCode);
+            BaseStaffSiteOrgDto site=siteService.getSite(receiveSiteCode);
+            if(null!=site) {
+                batchSend.setReceiveSiteName(site.getSiteName());
+            }
+            batchSend.setCreateTime(operateTime);
+            batchSend.setCreateUserCode(config.getOperateUserId());
+            batchSend.setCreateUserName(config.getOperateUserName());
+            batchSend.setYn(YN_DEFAULT);
+            batchSend.setUpdateTime(batchSend.getCreateTime());
+            batchSend.setSendCode(SerialRuleUtil.generateSendCode(batchSend.getCreateSiteCode(),batchSend.getReceiveSiteCode(),batchSend.getCreateTime()));
+            generateSend(batchSend);
+        }
+
+        return batchSend;
     }
 
     @Override
     public boolean generateSend(ScannerFrameBatchSend domain) {
-        return false;
+        return scannerFramwBatchSendDao.add(domain)>0;
     }
 }
