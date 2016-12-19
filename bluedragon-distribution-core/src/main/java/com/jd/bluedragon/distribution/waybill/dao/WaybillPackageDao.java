10fd6ac39a5992f26ef424b4ec35ac2593b6eaa6
zhanglei236 <zhanglei51@jd.com>
2016-12-19 14:37:39 +0800

    泛型问题

--- bluedragon-distribution-core/src/main/java/com/jd/bluedragon/distribution/waybill/dao/WaybillPackageDao.java
+++ bluedragon-distribution-core/src/main/java/com/jd/bluedragon/distribution/waybill/dao/WaybillPackageDao.java
@@ -1,13 +1,12 @@
 package com.jd.bluedragon.distribution.waybill.dao;
 
 import com.jd.bluedragon.common.dao.BaseDao;
-import com.jd.bluedragon.distribution.waybill.domain.FreshWaybill;
 import com.jd.bluedragon.distribution.waybill.domain.WaybillPackageDTO;
 
 /**
  * Created by zhanglei51 on 2016/12/15.
  */
-public class WaybillPackageDao extends BaseDao<FreshWaybill> {
+public class WaybillPackageDao extends BaseDao<WaybillPackageDao> {
     private static final String namespace = WaybillPackageDao.class.getName();
 
     public WaybillPackageDTO get(String packageCode){
