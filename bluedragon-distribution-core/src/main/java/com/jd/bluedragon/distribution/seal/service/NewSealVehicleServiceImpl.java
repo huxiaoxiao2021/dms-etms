410b180bc31035257e982842d9ce9643482bd97b
zhanglei236 <zhanglei51@jd.com>
2017-05-23 10:27:02 +0800

    去掉无用的注释。

--- bluedragon-distribution-core/src/main/java/com/jd/bluedragon/distribution/seal/service/NewSealVehicleServiceImpl.java
+++ bluedragon-distribution-core/src/main/java/com/jd/bluedragon/distribution/seal/service/NewSealVehicleServiceImpl.java
@@ -10,8 +10,6 @@ import com.jd.ump.annotation.JProfiler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 @Service("newSealVehicleService")
@@ -33,26 +31,6 @@ public class NewSealVehicleServiceImpl implements NewSealVehicleService {
 	@JProfiler(jKey = "Bluedragon_dms_center.web.method.vos.findSealInfo", mState = {JProEnum.TP})
 	public CommonDto<PageDto<SealCarDto>> findSealInfo(SealCarDto request,PageDto<SealCarDto> pageDto) {
 		CommonDto<PageDto<SealCarDto>> sealCarInfo = vosQueryWS.querySealCarPage(request,pageDto);
-//		CommonDto<PageDto<SealCarDto>> sealCarInfo = new CommonDto<PageDto<SealCarDto>>();
-//		sealCarInfo.setCode(1);
-//		sealCarInfo.setMessage("成功");
-//
-//		PageDto<SealCarDto> pageDto1 = new PageDto<SealCarDto>();
-//
-//		List<SealCarDto> sealCarDtos = new ArrayList<SealCarDto>();
-//		for(int i = 0;i<2;i++){
-//			SealCarDto sealCarDto = new SealCarDto();
-//			sealCarDto.setEndSiteId(i);
-//			sealCarDto.setEndOrgName("北京"+i);
-//			sealCarDto.setEndOrgCode(""+i);
-//			sealCarDto.setStartSiteId(i);
-//			sealCarDto.setCreateTime(new Date());
-//			sealCarDtos.add(sealCarDto);
-//		}
-//
-//		pageDto1.setResult(sealCarDtos);
-//
-//		sealCarInfo.setData(pageDto1);
 		return sealCarInfo;
 	}
 
