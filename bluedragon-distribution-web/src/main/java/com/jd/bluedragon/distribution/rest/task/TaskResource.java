c8cbb7a54f9115601d6a543c29b3337d1133611e
lixin456 <lixin39@jd.com>
2017-07-18 15:57:43 +0800

    千灯亚一分拣机自动发货-添加注释

--- bluedragon-distribution-web/src/main/java/com/jd/bluedragon/distribution/rest/task/TaskResource.java
+++ bluedragon-distribution-web/src/main/java/com/jd/bluedragon/distribution/rest/task/TaskResource.java
@@ -279,7 +279,7 @@ public class TaskResource {
     @Path("/task/saveTask")
     public InvokeResult saveTask(@Context HttpServletRequest request, UploadData domain) {
         Integer source = domain.getSource();
-        // 判断请求来源
+        // 判断请求来源 2-分拣机 其他来源目前均为走龙门架逻辑
         if (source == 2) { //分拣机
             return saveSortMachineTask(domain);
         } else { // 龙门架
