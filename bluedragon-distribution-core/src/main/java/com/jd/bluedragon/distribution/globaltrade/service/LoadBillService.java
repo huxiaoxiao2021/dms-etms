dc9e762f96b4d9d4f044f1a88fcced79d137fdeb
lixin39 <lixin39@jd.com>
2017-12-13 20:59:34 +0800

    代码格式化

--- bluedragon-distribution-core/src/main/java/com/jd/bluedragon/distribution/globaltrade/service/LoadBillService.java
+++ bluedragon-distribution-core/src/main/java/com/jd/bluedragon/distribution/globaltrade/service/LoadBillService.java
@@ -14,53 +14,52 @@ import java.util.Map;
 
 public interface LoadBillService {
 
-	/**
-	 * 将装载单的审批状态更新为 放行 或 未放行
-	 *
-	 * @param report
-	 */
-	int updateLoadBillStatusByReport(LoadBillReport report);
+    /**
+     * 将装载单的审批状态更新为 放行 或 未放行
+     *
+     * @param report
+     */
+    int updateLoadBillStatusByReport(LoadBillReport report);
 
-	/**
-	 * 分页查询装载单
-	 * 
-	 * @param params
-	 * @return
-	 */
-	List<LoadBill> findPageLoadBill(Map<String, Object> params);
+    /**
+     * 分页查询装载单
+     *
+     * @param params
+     * @return
+     */
+    List<LoadBill> findPageLoadBill(Map<String, Object> params);
 
-	/**
-	 * 查询装载单的数量
-	 * 
-	 * @param params
-	 * @return
-	 */
-	Integer findCountLoadBill(Map<String, Object> params);
+    /**
+     * 查询装载单的数量
+     *
+     * @param params
+     * @return
+     */
+    Integer findCountLoadBill(Map<String, Object> params);
 
-	/**
-	 * 根据批次号初始化装载单
-	 * 
-	 * @param sendCode
-	 * @param string
-	 * @param integer
-	 * @throws Exception 
-	 */
-	int initialLoadBill(String sendCode, Integer userId, String userName) throws Exception;
-	
-	/**
-	 * 通过订单号取消预装载
-	 * 
-	 * */
-	JdResponse cancelPreloaded(List<LoadBill> request);
-	
-	/**
-	 * 通过id查询订单装载信息
-	 * 
-	 * */
-	LoadBill findLoadbillByID(Long id);
+    /**
+     * 根据批次号初始化装载单
+     *
+     * @param sendCode
+     * @param userId
+     * @param userName
+     * @throws Exception
+     */
+    int initialLoadBill(String sendCode, Integer userId, String userName) throws Exception;
+
+    /**
+     * 通过订单号取消预装载
+     */
+    JdResponse cancelPreloaded(List<LoadBill> request);
+
+    /**
+     * 通过id查询订单装载信息
+     */
+    LoadBill findLoadbillByID(Long id);
 
     /**
      * 获取装载单表 根据箱号或者订单号获取
+     *
      * @param report
      * @return
      */
@@ -69,6 +68,7 @@ public interface LoadBillService {
     /**
      * 处理预装载任务，上传数据至卓志接口、
      * 如果卓志返回成功，则更新装载单状态
+     *
      * @param task
      * @return
      */
@@ -79,10 +79,11 @@ public interface LoadBillService {
      * 首先根据这些id获取装载单数据
      * （1）如果其中有已经装载的，则直接提示失败；
      * （2）如果都是未装载状态，则调用卓志预装载接口，接口返回成功，更改装载单车次号和状态
+     *
      * @param id
      * @param trunkNo 车牌号
-     * @throws Exception
      * @return 装载数量
+     * @throws Exception
      */
     Integer preLoadBill(List<Long> id, String trunkNo) throws Exception;
 
