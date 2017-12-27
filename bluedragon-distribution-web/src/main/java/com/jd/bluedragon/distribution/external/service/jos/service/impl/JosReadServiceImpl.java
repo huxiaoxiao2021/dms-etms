eb44860f1de5d2a4a2628cacebaba90b09f4607e
wuzuxiang <wuzuxiang@jd.com>
2017-12-27 16:56:51 +0800

    武汉邮政的jsf接口编写

--- bluedragon-distribution-web/src/main/java/com/jd/bluedragon/distribution/external/service/jos/service/impl/JosReadServiceImpl.java
+++ bluedragon-distribution-web/src/main/java/com/jd/bluedragon/distribution/external/service/jos/service/impl/JosReadServiceImpl.java
@@ -1,7 +1,13 @@
 package com.jd.bluedragon.distribution.external.service.jos.service.impl;
 
 import com.jd.bluedragon.distribution.external.jos.service.JosReadService;
+import com.jd.bluedragon.distribution.jsf.domain.WhemsWaybillResponse;
+import com.jd.bluedragon.distribution.send.service.ReverseDeliveryService;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * Created by yangbo7 on 2015/9/1.
@@ -9,4 +15,24 @@ import org.springframework.stereotype.Service;
 @Service("josReadService")
 public class JosReadServiceImpl implements JosReadService {
 
+    private final Logger logger = Logger.getLogger(JosReadServiceImpl.class);
+
+    @Autowired
+    ReverseDeliveryService reverseDelivery;
+
+    @Override
+    public WhemsWaybillResponse getWhemsWaybill(List<String> request) {
+        this.logger.error("武汉邮政接口被调用，运单号"+ request.toArray());
+////        String realIP = servletRequest.getHeader("X-Forwarded-For");
+//        String emsIp = PropertiesHelper.newInstance().getValue("EMSIP");
+//        if (realIP != null && !realIP.contains(emsIp)) {
+//            return new WhemsWaybillResponse(JdResponse.CODE_PARAM_ERROR,
+//                    "非法IP调用");
+//        }
+//        if (request == null || request.isEmpty() || request.size() > 50) {
+//            return new WhemsWaybillResponse(JdResponse.CODE_PARAM_ERROR,
+//                    JdResponse.MESSAGE_PARAM_ERROR);
+//        }
+        return reverseDelivery.getWhemsWaybill(request);
+    }
 }
