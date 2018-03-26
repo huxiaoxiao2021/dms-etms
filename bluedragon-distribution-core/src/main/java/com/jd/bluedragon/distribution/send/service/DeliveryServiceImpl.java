ceeca8ab305a879bfa436d9935822303d66e77e2
xumei3 <xumei3@jd.com>
2018-03-26 16:21:39 +0800

    去除定义重复的getWayBillCodesByBoxCode方法

--- bluedragon-distribution-core/src/main/java/com/jd/bluedragon/distribution/send/service/DeliveryServiceImpl.java
+++ bluedragon-distribution-core/src/main/java/com/jd/bluedragon/distribution/send/service/DeliveryServiceImpl.java
@@ -445,21 +445,6 @@ public class DeliveryServiceImpl implements DeliveryService {
      * @param boxCode
      * @return
      */
-    private List<String>  getWayBillCodesByBoxCode(String boxCode){
-        Box box = this.boxService.findBoxByCode(boxCode);
-        if(box != null) {
-            return sendDatailReadDao.findWaybillByBoxCode(boxCode, box.getCreateSiteCode());
-        }else{
-            logger.warn("一车一单发货箱号为空："+boxCode);
-        }
-        return null;
-    }
-
-    /**
-     * 根据箱号查询箱号的运单号
-     * @param boxCode
-     * @return
-     */
     private List<String>  getWaybillCodesByBoxCodeAndFetchNum(String boxCode,Integer fetchNum){
         Box box = this.boxService.findBoxByCode(boxCode);
         if(box != null) {
