fa51ccf59b60f09d3aab579abd967a88767f4339
wuyoude <wuyoude@jd.com>
2018-04-12 09:36:57 +0800

    包裹半收需求

---
+++ bluedragon-distribution-core/src/main/java/com/jd/bluedragon/distribution/half/dao/PackageHalfApproveDao.java
@@ -0,0 +1,17 @@
+package com.jd.bluedragon.distribution.half.dao;
+
+import com.jd.bluedragon.distribution.half.domain.PackageHalfApprove;
+import com.jd.ql.dms.common.web.mvc.api.Dao;
+
+/**
+ *
+ * @ClassName: PackageHalfApproveDao
+ * @Description: 包裹半收协商再投终端提交业务表--Dao接口
+ * @author wuyoude
+ * @date 2018年04月11日 18:45:19
+ *
+ */
+public interface PackageHalfApproveDao extends Dao<PackageHalfApprove> {
+
+
+}
---
+++ bluedragon-distribution-core/src/main/java/com/jd/bluedragon/distribution/half/dao/impl/PackageHalfApproveDaoImpl.java
@@ -0,0 +1,21 @@
+package com.jd.bluedragon.distribution.half.dao.impl;
+
+import org.springframework.stereotype.Repository;
+
+import com.jd.bluedragon.distribution.half.domain.PackageHalfApprove;
+import com.jd.bluedragon.distribution.half.dao.PackageHalfApproveDao;
+import com.jd.ql.dms.common.web.mvc.mybatis.BaseDao;
+
+/**
+ *
+ * @ClassName: PackageHalfApproveDaoImpl
+ * @Description: 包裹半收协商再投终端提交业务表--Dao接口实现
+ * @author wuyoude
+ * @date 2018年04月11日 18:45:19
+ *
+ */
+@Repository("packageHalfApproveDao")
+public class PackageHalfApproveDaoImpl extends BaseDao<PackageHalfApprove> implements PackageHalfApproveDao {
+
+
+}
