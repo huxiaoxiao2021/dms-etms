d90a9cc8e4c184f4bc74101b682bfebf7226e80f
shipeilin <shipeilin@jd.com>
2018-04-16 18:17:30 +0800

    ucc升级及配置迁移

--- bluedragon-distribution-core/src/main/java/com/jd/bluedragon/configuration/ucc/UccPropertyConfiguration.java
+++ bluedragon-distribution-core/src/main/java/com/jd/bluedragon/configuration/ucc/UccPropertyConfiguration.java
@@ -11,6 +11,19 @@ public class UccPropertyConfiguration {
     /** cassandra服务的全局开关 **/
     private boolean cassandraGlobalSwitch;
 
+    /** cassandra服务的全局开关 **/
+    private String asynBufferDynamicProducerProducerType;
+
+    private String asynBufferNotenabledTaskKeyword1;
+
+    private boolean asynBufferJmqComsumerTaskProcessorPostTaskStoreEnbaled;
+
+    private boolean migrationDbBackupReplicateIgnoreExp;
+
+    private boolean migrationDbBackupReplicateEnable;
+
+    private String workerFetchWithoutFailedTable;
+
     public String getAsynbufferEnabledTaskType() {
         return asynbufferEnabledTaskType;
     }
@@ -25,5 +38,53 @@ public class UccPropertyConfiguration {
 
     public void setCassandraGlobalSwitch(boolean cassandraGlobalSwitch) {
         this.cassandraGlobalSwitch = cassandraGlobalSwitch;
+    }
+
+    public String getAsynBufferDynamicProducerProducerType() {
+        return asynBufferDynamicProducerProducerType;
+    }
+
+    public void setAsynBufferDynamicProducerProducerType(String asynBufferDynamicProducerProducerType) {
+        this.asynBufferDynamicProducerProducerType = asynBufferDynamicProducerProducerType;
+    }
+
+    public String getAsynBufferNotenabledTaskKeyword1() {
+        return asynBufferNotenabledTaskKeyword1;
+    }
+
+    public void setAsynBufferNotenabledTaskKeyword1(String asynBufferNotenabledTaskKeyword1) {
+        this.asynBufferNotenabledTaskKeyword1 = asynBufferNotenabledTaskKeyword1;
+    }
+
+    public boolean getAsynBufferJmqComsumerTaskProcessorPostTaskStoreEnbaled() {
+        return asynBufferJmqComsumerTaskProcessorPostTaskStoreEnbaled;
+    }
+
+    public void setAsynBufferJmqComsumerTaskProcessorPostTaskStoreEnbaled(boolean asynBufferJmqComsumerTaskProcessorPostTaskStoreEnbaled) {
+        this.asynBufferJmqComsumerTaskProcessorPostTaskStoreEnbaled = asynBufferJmqComsumerTaskProcessorPostTaskStoreEnbaled;
+    }
+
+    public boolean getMigrationDbBackupReplicateIgnoreExp() {
+        return migrationDbBackupReplicateIgnoreExp;
+    }
+
+    public void setMigrationDbBackupReplicateIgnoreExp(boolean migrationDbBackupReplicateIgnoreExp) {
+        this.migrationDbBackupReplicateIgnoreExp = migrationDbBackupReplicateIgnoreExp;
+    }
+
+    public boolean getMigrationDbBackupReplicateEnable() {
+        return migrationDbBackupReplicateEnable;
+    }
+
+    public void setMigrationDbBackupReplicateEnable(boolean migrationDbBackupReplicateEnable) {
+        this.migrationDbBackupReplicateEnable = migrationDbBackupReplicateEnable;
+    }
+
+    public String getWorkerFetchWithoutFailedTable() {
+        return workerFetchWithoutFailedTable;
+    }
+
+    public void setWorkerFetchWithoutFailedTable(String workerFetchWithoutFailedTable) {
+        this.workerFetchWithoutFailedTable = workerFetchWithoutFailedTable;
     }
 }
