999cc00ce2d9f66776d02852040445d053d108f6
xumei3 <xumei3@jd.com>
2018-04-17 10:36:26 +0800

    C网运费现结修改货款字段取值

--- bluedragon-distribution-core/src/main/java/com/jd/bluedragon/common/service/impl/WaybillCommonServiceImpl.java
+++ bluedragon-distribution-core/src/main/java/com/jd/bluedragon/common/service/impl/WaybillCommonServiceImpl.java
@@ -546,7 +546,7 @@ public class WaybillCommonServiceImpl implements WaybillCommonService {
         	}
         	//货款字段金额等于0时，则货款位置显示为【在线支付】
         	//货款字段金额大于0时，则货款位置显示为【货到付款】
-        	if(NumberHelper.gt0(waybill.getRecMoney())){
+        	if(NumberHelper.gt0(waybill.getCodMoney())){
         		goodsPaymentText = TextConstants.GOODS_PAYMENT_COD;
         	}else{
         		goodsPaymentText = TextConstants.GOODS_PAYMENT_ONLINE;
@@ -562,11 +562,16 @@ public class WaybillCommonServiceImpl implements WaybillCommonService {
 
             //货款：waybillSign 货款大于0时，25位为2或3时显示【货到付款】，25位不为2且不为3时显示应收金额，
             //货款等于0时，显示【在线支付】
-            if(NumberHelper.gt0(waybill.getRecMoney())){
+            if(NumberHelper.gt0(waybill.getCodMoney())){
                 if(BusinessHelper.isSignInChars(waybill.getWaybillSign(),25,'2','3')){
                     goodsPaymentText = TextConstants.GOODS_PAYMENT_COD;
                 }else{
                     goodsPaymentText = "￥"+ waybill.getCodMoney();
+                    if(waybill.getPayment()!=null){
+                        if(waybill.getPayment()==ComposeService.ONLINE_PAYMENT_SIGN){
+                            goodsPaymentText = TextConstants.GOODS_PAYMENT_ONLINE;
+                        }
+                    }
                 }
             } else{
                 goodsPaymentText = TextConstants.GOODS_PAYMENT_ONLINE;
