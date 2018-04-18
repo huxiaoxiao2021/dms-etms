0da702ad0f52559dff76c0efcaffb53a79bc4ca0
xumei3 <xumei3@jd.com>
2018-04-18 09:40:30 +0800

    B网运费和货款打标增加设置模板的逻辑

--- bluedragon-distribution-core/src/main/java/com/jd/bluedragon/common/service/impl/WaybillCommonServiceImpl.java
+++ bluedragon-distribution-core/src/main/java/com/jd/bluedragon/common/service/impl/WaybillCommonServiceImpl.java
@@ -551,6 +551,7 @@ public class WaybillCommonServiceImpl implements WaybillCommonService {
         	}else{
         		goodsPaymentText = TextConstants.GOODS_PAYMENT_ONLINE;
         	}
+            target.setTemplateName("dms-nopaper-b2b-m");
         }else{
             //C网运费和货款
             //运费：waybillSign 25位为2时【到付现结】；25位为3时【寄付现结】
