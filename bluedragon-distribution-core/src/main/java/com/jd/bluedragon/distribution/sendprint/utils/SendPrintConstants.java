00c5ad03e78ac722b6039291eaa1c240d4620094
lixin39 <lixin39@jd.com>
2018-05-22 21:46:16 +0800

    优化清单打印逻辑

---
+++ bluedragon-distribution-core/src/main/java/com/jd/bluedragon/distribution/sendprint/utils/SendPrintConstants.java
@@ -0,0 +1,36 @@
+package com.jd.bluedragon.distribution.sendprint.utils;
+
+/**
+ * <p>
+ * Created by lixin39 on 2018/5/22.
+ */
+public class SendPrintConstants {
+
+    /**
+     * 文本是
+     */
+    public final static String TEXT_YES = "是";
+
+    /**
+     * 文本否
+     */
+    public final static String TEXT_NO = "否";
+
+    /**
+     * 文本 自营
+     */
+    public final static String TEXT_SELF_SUPPORT = "自营";
+
+    /**
+     * 文本 一般订单
+     */
+    public final static String TEXT_GENERAL_ORDER = "一般订单";
+
+    /**
+     * 默认初始价格
+     */
+    public final static String TEXT_DEFAULT_PRICE = "0.00";
+
+    public final static String TEXT_DOUBLE_BAR = "--";
+
+}
