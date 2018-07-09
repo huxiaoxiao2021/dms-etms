9a6a484ffe185adfc776caf0be222f7e10c098f3
hujiping1 <hujiping>
2018-07-09 16:48:59 +0800

    外单航空打标逻辑变更

--- bluedragon-distribution-core/src/main/java/com/jd/bluedragon/common/service/impl/WaybillCommonServiceImpl.java
+++ bluedragon-distribution-core/src/main/java/com/jd/bluedragon/common/service/impl/WaybillCommonServiceImpl.java
@@ -644,7 +644,7 @@ public class WaybillCommonServiceImpl implements WaybillCommonService {
             target.setTransportMode(ComposeService.PREPARE_SITE_NAME_FRESH_SEND);
         }
         //waybill_sign标识位，第三十一位为0且第六十七位为1，打航填标
-        if(!BusinessHelper.isExternal(waybill.getWaybillSign()) &&
+        if(BusinessHelper.isExternal(waybill.getWaybillSign()) &&
                 BusinessHelper.isSignChar(waybill.getWaybillSign(),31,'0') &&
                 BusinessHelper.isSignChar(waybill.getWaybillSign(),67,'1')){
             target.appendSpecialMark(ComposeService.SPECIAL_MARK_AIRTRANSPORT_FILL);
