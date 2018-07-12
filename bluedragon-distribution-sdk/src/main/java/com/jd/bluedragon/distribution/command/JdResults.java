40c281313a95f7fde524dc0e7ea9e10c5ea4f264
wuyoude <wuyoude@jd.com>
2018-07-12 14:22:17 +0800

    打印接口写入自定义日志

--- bluedragon-distribution-sdk/src/main/java/com/jd/bluedragon/distribution/command/JdResults.java
+++ bluedragon-distribution-sdk/src/main/java/com/jd/bluedragon/distribution/command/JdResults.java
@@ -1,6 +1,5 @@
 package com.jd.bluedragon.distribution.command;
 
-import java.io.Serializable;
 
 /**
  * 
@@ -12,6 +11,9 @@ import java.io.Serializable;
  */
 public class JdResults{
 	public static final JdResult REST_SUC_DATA_NULL = new JdResult(JdResult.CODE_SUC,201,"201-调用服务成功，数据为空！");
+	/**
+	 * 参数错误返回对象
+	 */
 	public static final JdResult REST_FAIL_PARAM_ERROR = new JdResult(JdResult.CODE_FAIL,401,"401-调用服务失败，参数错误！");
 	public static final JdResult REST_FAIL_SERVER_NOT_FIND = new JdResult(JdResult.CODE_FAIL,404,"404-请求的服务不存在！");
 	public static final JdResult REST_ERROR_SERVER_EXCEPTION = new JdResult(JdResult.CODE_ERROR,501,"501-请求的服务异常！");
