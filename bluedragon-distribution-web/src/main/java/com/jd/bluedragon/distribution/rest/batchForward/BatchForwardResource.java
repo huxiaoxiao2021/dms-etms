a73631eff3f1eb0c761cb4b8781f6386612dbc7b
hujiping1 <hujiping>
2018-08-13 10:57:46 +0800

    批次转发-批次转发任务逻辑

--- bluedragon-distribution-web/src/main/java/com/jd/bluedragon/distribution/rest/batchForward/BatchForwardResource.java
+++ bluedragon-distribution-web/src/main/java/com/jd/bluedragon/distribution/rest/batchForward/BatchForwardResource.java
@@ -4,21 +4,15 @@ import com.jd.bluedragon.Constants;
 import com.jd.bluedragon.distribution.api.request.BatchForwardRequest;
 import com.jd.bluedragon.distribution.api.utils.JsonHelper;
 import com.jd.bluedragon.distribution.base.domain.InvokeResult;
-import com.jd.bluedragon.distribution.base.service.BaseService;
-import com.jd.bluedragon.distribution.batchForward.SendCodeInfo;
 import com.jd.bluedragon.distribution.batchForward.service.BatchForwardService;
-import com.jd.bluedragon.utils.SerialRuleUtil;
-import com.jd.ql.basic.dto.BaseStaffSiteOrgDto;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -33,52 +27,7 @@ public class BatchForwardResource {
     private final Log logger = LogFactory.getLog(this.getClass());
 
     @Autowired
-    private BaseService baseService;
-
-    @Autowired
     private BatchForwardService batchForwardService;
-
-    /**
-     * 通过批次号获得始发站点和目的站点
-     * @param sendCode
-     * @return
-     */
-    @GET
-    @Path("/batchForward/getCreateSiteAndReceiveSite/{sendCode}")
-    public InvokeResult<SendCodeInfo> getCreateSiteAndReceiveSite(@PathParam("sendCode") String sendCode){
-        this.logger.info("批次号"+ sendCode + "不能为空");
-        InvokeResult<SendCodeInfo> result = new InvokeResult<SendCodeInfo>();
-        Integer createSiteCode = SerialRuleUtil.getCreateSiteCodeFromSendCode(sendCode);
-        Integer receiveSiteCode = SerialRuleUtil.getReceiveSiteCodeFromSendCode(sendCode);
-        BaseStaffSiteOrgDto createSite = null;
-        BaseStaffSiteOrgDto receiveSite = null;
-        try{
-            if(createSiteCode != null && receiveSiteCode != null){
-                createSite = baseService.queryDmsBaseSiteByCode(createSiteCode.toString());
-                receiveSite = baseService.queryDmsBaseSiteByCode(receiveSiteCode.toString());
-                if(createSite != null && receiveSite != null){
-                    SendCodeInfo sendCodeInfo = new SendCodeInfo();
-                    sendCodeInfo.setCreateSiteCode(createSite.getSiteCode());
-                    sendCodeInfo.setCreateSiteName(createSite.getSiteName());
-                    sendCodeInfo.setReceiveSiteCode(receiveSite.getSiteCode());
-                    sendCodeInfo.setCreateSiteName(receiveSite.getSiteName());
-                    result.setCode(InvokeResult.RESULT_SUCCESS_CODE);
-                    result.setData(sendCodeInfo);
-                    return result;
-                }
-                result.customMessage(400,"不存在该站点");
-                return result;
-            }
-            result.setCode(InvokeResult.RESULT_NULL_CODE);
-            result.setMessage(InvokeResult.RESULT_NULL_MESSAGE);
-        }catch (Exception e){
-            this.logger.error("通过站点id获得站点信息失败",e);
-            result.error(e);
-            return result;
-        }
-        return result;
-    }
-
 
     /**
      * 批次号整批转发
