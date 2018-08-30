687eb8d24a6d8ef7463af1167eb36db21378ae1a
shipeilin <shipeilin@jd.com>
2018-08-30 18:09:56 +0800

    新增-耗材查询导出增加状态条件

--- bluedragon-distribution-web/src/main/webapp/static/js/consumable/waybillConsumableRecord.js
+++ bluedragon-distribution-web/src/main/webapp/static/js/consumable/waybillConsumableRecord.js
@@ -1,85 +1,98 @@
 $(function() {
-	var saveUrl = '/consumable/waybillConsumableRecord/save';
-	var confirmUrl = '/consumable/waybillConsumableRecord/confirmByIds';
+    var saveUrl = '/consumable/waybillConsumableRecord/save';
+    var confirmUrl = '/consumable/waybillConsumableRecord/confirmByIds';
     var detailUrl = '/consumable/waybillConsumableRecord/detail/';
     var queryUrl = '/consumable/waybillConsumableRecord/listData';
-	var detailPageUrl = '/consumable/waybillConsumableRelation/toIndex';
-	var exportDataUrl = '/consumable/waybillConsumableRecord/export';
+    var detailPageUrl = '/consumable/waybillConsumableRelation/toIndex';
+    var exportDataUrl = '/consumable/waybillConsumableRecord/export';
 
     /*最多查询近x天数据*/
     var PAST_LIMIT = 94;
     var PERIOD_LIMIT = 32;
-
-	var tableInit = function() {
-		var oTableInit = new Object();
-		oTableInit.init = function() {
-			$('#dataTable').bootstrapTable({
-				url : queryUrl, // 请求后台的URL（*）
-				method : 'post', // 请求方式（*）
-				toolbar : '#toolbar', // 工具按钮用哪个容器
-				queryParams : oTableInit.getSearchParams, // 查询参数（*）
-				height : 500, // 行高，如果没有设置height属性，表格自动根据记录条数觉得表格高度
-				uniqueId : "ID", // 每一行的唯一标识，一般为主键列
-				pagination : true, // 是否显示分页（*）
-				pageNumber : 1, // 初始化加载第一页，默认第一页
-				pageSize : 10, // 每页的记录行数（*）
-				pageList : [ 10, 25, 50, 100 ], // 可供选择的每页的行数（*）
-				cache : false, // 是否使用缓存，默认为true，所以一般情况下需要设置一下这个属性（*）
-				sidePagination : "server", // 分页方式：client客户端分页，server服务端分页（*）
-				striped : true, // 是否显示行间隔色
-				showColumns : true, // 是否显示所有的列
-				sortable : true, // 是否启用排序
-				sortOrder : "asc", // 排序方式
+    $.combobox.createNew('confirmStatus',{
+        width: '150',
+        placeholder:'确认状态',
+        allowClear:true,
+        data:[
+            {id:'0',text:'未确认'},
+            {id:'1',text:'已确认'},
+            {id:'2',text:'全部'}
+        ]
+    });
+    var tableInit = function() {
+        var oTableInit = new Object();
+        oTableInit.init = function() {
+            $('#dataTable').bootstrapTable({
+                url : queryUrl, // 请求后台的URL（*）
+                method : 'post', // 请求方式（*）
+                toolbar : '#toolbar', // 工具按钮用哪个容器
+                queryParams : oTableInit.getSearchParams, // 查询参数（*）
+                height : 500, // 行高，如果没有设置height属性，表格自动根据记录条数觉得表格高度
+                uniqueId : "ID", // 每一行的唯一标识，一般为主键列
+                pagination : true, // 是否显示分页（*）
+                pageNumber : 1, // 初始化加载第一页，默认第一页
+                pageSize : 10, // 每页的记录行数（*）
+                pageList : [ 10, 25, 50, 100 ], // 可供选择的每页的行数（*）
+                cache : false, // 是否使用缓存，默认为true，所以一般情况下需要设置一下这个属性（*）
+                sidePagination : "server", // 分页方式：client客户端分页，server服务端分页（*）
+                striped : true, // 是否显示行间隔色
+                showColumns : true, // 是否显示所有的列
+                sortable : true, // 是否启用排序
+                sortOrder : "asc", // 排序方式
 //				search : true, // 是否显示表格搜索，此搜索是客户端搜索，不会进服务端，所以，个人感觉意义不大
-				showRefresh : true, // 是否显示刷新按钮
-				minimumCountColumns : 2, // 最少允许的列数
-				clickToSelect : true, // 是否启用点击选中行
-				showToggle : true, // 是否显示详细视图和列表视图的切换按钮
+                showRefresh : true, // 是否显示刷新按钮
+                minimumCountColumns : 2, // 最少允许的列数
+                clickToSelect : true, // 是否启用点击选中行
+                showToggle : true, // 是否显示详细视图和列表视图的切换按钮
 //				showPaginationSwitch : true, // 是否显示分页关闭按钮
-				strictSearch : true,
-				// icons: {refresh: "glyphicon-repeat", toggle:
-				// "glyphicon-list-alt", columns: "glyphicon-list"},
-				// search:false,
-				// cardView: true, //是否显示详细视图
-				// detailView: true, //是否显示父子表
-				// showFooter:true,
-				// paginationVAlign:'center',
-				// singleSelect:true,
-				columns : oTableInit.tableColums
-			});
-		};
-		oTableInit.getSearchParams = function(params) {
-			var temp = oTableInit.getSearchCondition();
-			if(!temp){
-				temp={};
-			}
-			temp.offset = params.offset;
-			temp.limit = params.limit;
-			// 这里的键的名字和控制器的变量名必须一直，这边改动，控制器也需要改成一样的
-			return temp;
-		};
-		/**
-		 * 获取查询参数
-		 * @param _selector 选择器（默认为'.search-param'）
-		 */
-		oTableInit.getSearchCondition = function(_selector) {
-		    var params = {};
-		    if (!_selector) {
-		        _selector = ".search-param";
-		    }
+                strictSearch : true,
+                // icons: {refresh: "glyphicon-repeat", toggle:
+                // "glyphicon-list-alt", columns: "glyphicon-list"},
+                // search:false,
+                // cardView: true, //是否显示详细视图
+                // detailView: true, //是否显示父子表
+                // showFooter:true,
+                // paginationVAlign:'center',
+                // singleSelect:true,
+                columns : oTableInit.tableColums
+            });
+        };
+        oTableInit.getSearchParams = function(params) {
+            var temp = oTableInit.getSearchCondition();
+            if(!temp){
+                temp={};
+            }
+            temp.offset = params.offset;
+            temp.limit = params.limit;
+            // 这里的键的名字和控制器的变量名必须一直，这边改动，控制器也需要改成一样的
+            return temp;
+        };
+        /**
+         * 获取查询参数
+         * @param _selector 选择器（默认为'.search-param'）
+         */
+        oTableInit.getSearchCondition = function(_selector) {
+            var params = {};
+            if (!_selector) {
+                _selector = ".search-param";
+            }
             $(_selector).each(function () {
                 var _k = this.id;
                 var _v = $(this).val();
                 if(_k && _v){
                     if(_k == 'startTime' || _k =='endTime'){
                         params[_k]=new Date(_v).getTime();;
-                    }else{
-                        params[_k]=_v;
+                    }else {
+                        if(_k == 'confirmStatus' && _v == "2"){
+
+                        }else{
+                            params[_k]=_v;
+                        }
                     }
                 }
             });
-		    return params;
-		};
+            return params;
+        };
         oTableInit.tableColums = [ {
             checkbox : true
         },{
@@ -138,7 +151,7 @@ $(function() {
                             var waybillCode = row.waybillCode;
                             var frameId = document.getElementById("detailFrame").getElementsByTagName("iframe")[0].id;
                             var frameWindow = $('#' + frameId)[0].contentWindow;
-							frameWindow.$('#waybillCode-value-input').val(waybillCode);
+                            frameWindow.$('#waybillCode-value-input').val(waybillCode);
                             frameWindow.$('#btn_query').click();
                         }
                     });
@@ -151,9 +164,9 @@ $(function() {
         };
         return oTableInit;
     };
-	var pageInit = function() {
-		var oInit = new Object();
-		oInit.init = function() {
+    var pageInit = function() {
+        var oInit = new Object();
+        oInit.init = function() {
             $('#dataEditDiv').hide();
             /*起始时间*/ /*截止时间*/
             $.datePicker.createNew({
@@ -178,76 +191,77 @@ $(function() {
                     $('#query-form').data("bootstrapValidator").resetForm();
                 }
             });
-		    $('#btn_query').click(function() {
+
+            $('#btn_query').click(function() {
                 var flag = $.formValidator.isValid('query-form');
                 if(flag == true){
                     tableInit().refresh();
                 }else{
                     $.msg.warn('查询条件有误','请您检查查询条件是否有误');
                 };
-			});
-			$('#btn_add').click(function() {
-			    $('.edit-param').each(function () {
-			    	var _k = this.id;
-			        if(_k){
-			        	$(this).val('');
-			        }
-			    });
-			    $('#edit-form #typeGroup').val(null).trigger('change');
-			    $('#edit-form #parentId').val(null).trigger('change');
-				$('#dataTableDiv').hide();
-				$('#dataEditDiv').show();
-			});
-			// 修改操作
-			$('#btn_edit').click(function() {
-				var rows = $('#dataTable').bootstrapTable('getSelections');
-				if (rows.length > 1) {
-					alert("修改操作，只能选择一条数据");
-					return;
-				}
-				if (rows.length == 0) {
-					alert("请选择一条数据");
-					return;
-				}
-			    $.ajaxHelper.doPostSync(detailUrl+rows[0].id,null,function(res){
-			    	if(res&&res.succeed&&res.data){
-					    $('.edit-param').each(function () {
-					    	var _k = this.id;
-					        var _v = res.data[_k];
-					        if(_k){
-					        	if(_v != null && _v != undefined){
-						        	$(this).val(_v);
-						        }else{
-						        	$(this).val('');
-						        }
-					        } 
-					    });
-			    	}
-			    });
-				$('#dataTableDiv').hide();
-				$('#dataEditDiv').show();
-			});
+            });
+            $('#btn_add').click(function() {
+                $('.edit-param').each(function () {
+                    var _k = this.id;
+                    if(_k){
+                        $(this).val('');
+                    }
+                });
+                $('#edit-form #typeGroup').val(null).trigger('change');
+                $('#edit-form #parentId').val(null).trigger('change');
+                $('#dataTableDiv').hide();
+                $('#dataEditDiv').show();
+            });
+            // 修改操作
+            $('#btn_edit').click(function() {
+                var rows = $('#dataTable').bootstrapTable('getSelections');
+                if (rows.length > 1) {
+                    alert("修改操作，只能选择一条数据");
+                    return;
+                }
+                if (rows.length == 0) {
+                    alert("请选择一条数据");
+                    return;
+                }
+                $.ajaxHelper.doPostSync(detailUrl+rows[0].id,null,function(res){
+                    if(res&&res.succeed&&res.data){
+                        $('.edit-param').each(function () {
+                            var _k = this.id;
+                            var _v = res.data[_k];
+                            if(_k){
+                                if(_v != null && _v != undefined){
+                                    $(this).val(_v);
+                                }else{
+                                    $(this).val('');
+                                }
+                            }
+                        });
+                    }
+                });
+                $('#dataTableDiv').hide();
+                $('#dataEditDiv').show();
+            });
 
-			// 批量确认
-			$('#btn_confirm').click(function() {
-				var rows = $('#dataTable').bootstrapTable('getSelections');
-				if (rows.length < 1) {
-					alert("错误，未选中数据");
-					return;
-				}
-				var flag = confirm("是否确认这些运单?");
-				if (flag == true) {
-					var params = [];
-					for(var i in rows){
-					    if(rows[i].confirmStatus != 1){
+            // 批量确认
+            $('#btn_confirm').click(function() {
+                var rows = $('#dataTable').bootstrapTable('getSelections');
+                if (rows.length < 1) {
+                    alert("错误，未选中数据");
+                    return;
+                }
+                var flag = confirm("是否确认这些运单?");
+                if (flag == true) {
+                    var params = [];
+                    for(var i in rows){
+                        if(rows[i].confirmStatus != 1){
                             var data = {};
                             data["id"] = rows[i].id;
                             data["waybillCode"] = rows[i].waybillCode;
                             data["dmsId"] = rows[i].dmsId;
                             params.push(data);
                         }
-				    };
-					if(params.length == 0){
+                    };
+                    if(params.length == 0){
                         alert("至少选中一条未确认的数据，已确认的运单不会重复确认！");
                     }else{
                         $.ajaxHelper.doPostSync(confirmUrl,JSON.stringify(params),function(res){
@@ -259,32 +273,32 @@ $(function() {
                             }
                         });
                     }
-				}
-			});
-			$('#btn_submit').click(function() {
-				var params = {};
-				$('.edit-param').each(function () {
-			    	var _k = this.id;
-			        var _v = $(this).val();
-			        if(_k && _v){
-			        	params[_k]=_v;
-			        }
-			    });
-				$.ajaxHelper.doPostSync(saveUrl,JSON.stringify(params),function(res){
-			    	if(res&&res.succeed){
-			    		alert('操作成功');
-			    		tableInit().refresh();
-			    	}else{
-			    		alert('操作异常');
-			    	}
-			    });
-				$('#dataEditDiv').hide();
-				$('#dataTableDiv').show();
-			});	
-			$('#btn_return').click(function() {
-				$('#dataEditDiv').hide();
-				$('#dataTableDiv').show();
-			});
+                }
+            });
+            $('#btn_submit').click(function() {
+                var params = {};
+                $('.edit-param').each(function () {
+                    var _k = this.id;
+                    var _v = $(this).val();
+                    if(_k && _v){
+                        params[_k]=_v;
+                    }
+                });
+                $.ajaxHelper.doPostSync(saveUrl,JSON.stringify(params),function(res){
+                    if(res&&res.succeed){
+                        alert('操作成功');
+                        tableInit().refresh();
+                    }else{
+                        alert('操作异常');
+                    }
+                });
+                $('#dataEditDiv').hide();
+                $('#dataTableDiv').show();
+            });
+            $('#btn_return').click(function() {
+                $('#dataEditDiv').hide();
+                $('#dataTableDiv').show();
+            });
 
             /*表单验证*/
             $.formValidator.createNew('query-form',{
@@ -314,11 +328,12 @@ $(function() {
                 var flag = $.formValidator.isValid('query-form');
                 if(flag == true) {
                     /*时间校验*/
-                   var now = new Date();
+                    var now = new Date();
                     var startTimeStr = $.datePicker.getValue('startTime');
                     var endTimeStr = $.datePicker.getValue('endTime');
                     var receiveUserErp = $("#receiveUserErp").val();
                     var waybillCode = $("#waybillCode").val();
+                    var confirmStatus = $("#confirmStatus").val();
                     /* var startTime = $.dateHelper.parseDateTime(startTimeStr);
                     var endTime = $.dateHelper.parseDateTime(endTimeStr);
 
@@ -350,6 +365,9 @@ $(function() {
                         if(waybillCode != ""){
                             exportUrl = exportUrl + '&waybillCode=' + waybillCode;
                         }
+                        if(confirmStatus != null && confirmStatus != "2"){
+                            exportUrl = exportUrl + '&confirmStatus=' + confirmStatus;
+                        }
                         console.log(exportUrl);
                         window.open(exportUrl);
                     });
@@ -357,8 +375,8 @@ $(function() {
                     $.msg.warn('导出查询条件有误','请您检查导出查询条件是否有误');
                 };
             })};
-		return oInit;
-	};
+        return oInit;
+    };
 
     initDateQuery();
     tableInit().init();
