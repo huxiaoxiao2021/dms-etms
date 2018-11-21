1e4f7d8e4e2a63a16cd96d9100e46b3039f2e2c6
tangchunqing <373535065@qq.com>
2018-11-21 16:14:16 +0800

    托寄物品名打印

--- bluedragon-distribution-core/src/main/java/com/jd/bluedragon/utils/BusinessHelper.java
+++ bluedragon-distribution-core/src/main/java/com/jd/bluedragon/utils/BusinessHelper.java
@@ -14,7 +14,7 @@ import java.util.regex.Pattern;
 public class BusinessHelper {
 
     private final static Logger logger = Logger.getLogger(BusinessHelper.class);
-    public final static String SEND_CODE_REG = "^\\d{1,10}-\\d{1,10}-\\d{16}$"; //批次号正则
+    public final static String SEND_CODE_REG = "^\\d+-\\d+-\\d{17}$"; //批次号正则
     public static final String PACKAGE_SEPARATOR = "-";
     public static final String PACKAGE_IDENTIFIER_SUM = "S";
     public static final String PACKAGE_IDENTIFIER_NUMBER = "N";
