f4e4b41fc66ed49002970ae5c574f361c317c67c
xumei3 <xumei3@jd.com>
2018-11-27 15:22:18 +0800

    同城当日达专用面单判断逻辑新增条件waybillsign第63位等于1

--- bluedragon-distribution-core/src/main/java/com/jd/bluedragon/utils/BusinessHelper.java
+++ bluedragon-distribution-core/src/main/java/com/jd/bluedragon/utils/BusinessHelper.java
@@ -666,4 +666,21 @@ public class BusinessHelper {
         }
         return sendCode.matches(SEND_CODE_REG);
     }
+
+    /**
+     * 判断是否满足同城当日达的条件
+     * waybill_sign
+     * 第55位等于0 （表示非生鲜专送）
+     * 且第16位等于1 （表示当日达）
+     * 且第31位等于2 （表示同城配送）
+     * 且第63位等于1 （中心站网络）
+     * @param waybillSign
+     * @return
+     */
+    public static boolean isSameCityOneDay(String waybillSign){
+        return BusinessUtil.isSignChar(waybillSign,55,'0') &&
+                BusinessUtil.isSignChar(waybillSign,16,'1') &&
+                BusinessUtil.isSignChar(waybillSign,31,'2') &&
+                BusinessUtil.isSignChar(waybillSign,63,'1');
+    }
 }
