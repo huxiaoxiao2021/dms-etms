24d054a35176d9c8f82f38c914b7f0a2d726f15a
xumei3 <xumei3@jd.com>
2018-11-28 18:08:13 +0800

    增加多删除的引用

--- bluedragon-distribution-core/src/main/java/com/jd/bluedragon/common/service/impl/WaybillCommonServiceImpl.java
+++ bluedragon-distribution-core/src/main/java/com/jd/bluedragon/common/service/impl/WaybillCommonServiceImpl.java
@@ -9,6 +9,7 @@ import java.util.Map;
 import java.util.TreeMap;
 
 import com.jd.bluedragon.distribution.print.service.HideInfoService;
+import com.jd.bluedragon.dms.utils.BusinessUtil;
 import com.jd.etms.waybill.api.WaybillPickupTaskApi;
 import com.jd.etms.waybill.domain.*;
 
