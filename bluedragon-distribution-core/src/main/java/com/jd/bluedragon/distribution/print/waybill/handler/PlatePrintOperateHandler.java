4d1a8b2585bc71f5fa46de214bb641eea44415b9
tangchunqing <373535065@qq.com>
2018-12-21 13:57:24 +0800

    快运发货批次号重复使用提示语优化

--- bluedragon-distribution-core/src/main/java/com/jd/bluedragon/distribution/print/waybill/handler/PlatePrintOperateHandler.java
+++ bluedragon-distribution-core/src/main/java/com/jd/bluedragon/distribution/print/waybill/handler/PlatePrintOperateHandler.java
@@ -1,72 +1,91 @@
 package com.jd.bluedragon.distribution.print.waybill.handler;
 
-import java.util.List;
-
 import com.jd.bluedragon.distribution.command.JdResult;
 import com.jd.bluedragon.distribution.handler.Handler;
 import com.jd.bluedragon.distribution.handler.InterceptHandler;
 import com.jd.bluedragon.distribution.handler.InterceptResult;
+import com.jd.bluedragon.utils.BusinessHelper;
 import com.jd.bluedragon.utils.JsonHelper;
+import com.jd.etms.waybill.api.WaybillTraceApi;
+import com.jd.etms.waybill.domain.BaseEntity;
+import com.jd.etms.waybill.domain.PackageState;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+import static com.jd.bluedragon.Constants.RESULT_SUCCESS;
+import static com.jd.bluedragon.Constants.WAYBILLTRACE_STATE;
+import static com.jd.bluedragon.distribution.handler.InterceptResult.MESSAGE_NEED_RECEIVE;
+
 /**
- * 
  * @ClassName: PlatePrintOperateHandler
  * @Description: 平台打印操作
  * @author: wuyoude
  * @date: 2018年2月6日 上午9:10:00
- *
  */
-public class PlatePrintOperateHandler implements InterceptHandler<WaybillPrintContext,String>{
-	/**
-	 * 处理逻辑列表
-	 */
-	private List<Handler<WaybillPrintContext,JdResult<String>>> handlers;
-	
-	@Override
-	public InterceptResult<String> handle(WaybillPrintContext context) {
-		InterceptResult<String> interceptResult = new InterceptResult<String>();
-		interceptResult.toSuccess();
-		if(handlers != null && !handlers.isEmpty()){
-			for(Handler<WaybillPrintContext,JdResult<String>> handler:handlers){
-				//拦截类型的处理
-				if(handler instanceof InterceptHandler){
-					InterceptResult<String> InterceptResult = (InterceptResult<String>)((InterceptHandler)handler).handle(context);
-					if(InterceptResult != null && InterceptResult.isPassed()){
-						if(InterceptResult.isWeakPassed()){
-							context.appendMessage(InterceptResult.getMessage());
-							context.setStatus(InterceptResult.getStatus());
-						}
-					}else{
-						return InterceptResult;
-					}
-				}else{
-					JdResult<String> jdResult = handler.handle(context);
-					if(jdResult == null || !jdResult.isSucceed()){
-						context.setStatus(InterceptResult.STATUS_NO_PASSED);
-						interceptResult.toFail(jdResult.getMessageCode(), jdResult.getMessage());
-						return interceptResult;
-					}
-				}
-			}
-		}
-		interceptResult.setData(JsonHelper.toJson(context.getResponse()));
-		if(context.getStatus()==InterceptResult.STATUS_WEAK_PASSED){
-			interceptResult.setStatus(context.getStatus());
-			interceptResult.setMessage(context.getMessages().get(0));
-			interceptResult.setCode(JdResult.CODE_SUC);
-		}
-		return interceptResult;
-	}
-	/**
-	 * @return the handlers
-	 */
-	public List<Handler<WaybillPrintContext, JdResult<String>>> getHandlers() {
-		return handlers;
-	}
-	/**
-	 * @param handlers the handlers to set
-	 */
-	public void setHandlers(
-			List<Handler<WaybillPrintContext, JdResult<String>>> handlers) {
-		this.handlers = handlers;
-	}
+public class PlatePrintOperateHandler implements InterceptHandler<WaybillPrintContext, String> {
+    @Autowired
+    private WaybillTraceApi waybillTraceApi;
+    /**
+     * 处理逻辑列表
+     */
+    private List<Handler<WaybillPrintContext, JdResult<String>>> handlers;
+
+    @Override
+    public InterceptResult<String> handle(WaybillPrintContext context) {
+        InterceptResult<String> interceptResult = new InterceptResult<String>();
+        interceptResult.toSuccess();
+        if (BusinessHelper.isC2c(context.getWaybill().getWaybillSign())) {
+            BaseEntity<List<PackageState>> baseEntity = waybillTraceApi.getPkStateByWCodeAndState(context.getWaybill().getWaybillCode(), WAYBILLTRACE_STATE);
+            if (!(baseEntity != null && baseEntity.getResultCode() == RESULT_SUCCESS && baseEntity.getData() != null && baseEntity.getData().size() > 0)) {
+                interceptResult.toFail(InterceptResult.STATUS_NO_PASSED, MESSAGE_NEED_RECEIVE);
+                return interceptResult;
+            }
+        }
+        if (handlers != null && !handlers.isEmpty()) {
+            for (Handler<WaybillPrintContext, JdResult<String>> handler : handlers) {
+                //拦截类型的处理
+                if (handler instanceof InterceptHandler) {
+                    InterceptResult<String> InterceptResult = (InterceptResult<String>) ((InterceptHandler) handler).handle(context);
+                    if (InterceptResult != null && InterceptResult.isPassed()) {
+                        if (InterceptResult.isWeakPassed()) {
+                            context.appendMessage(InterceptResult.getMessage());
+                            context.setStatus(InterceptResult.getStatus());
+                        }
+                    } else {
+                        return InterceptResult;
+                    }
+                } else {
+                    JdResult<String> jdResult = handler.handle(context);
+                    if (jdResult == null || !jdResult.isSucceed()) {
+                        context.setStatus(InterceptResult.STATUS_NO_PASSED);
+                        interceptResult.toFail(jdResult.getMessageCode(), jdResult.getMessage());
+                        return interceptResult;
+                    }
+                }
+            }
+        }
+        interceptResult.setData(JsonHelper.toJson(context.getResponse()));
+        if (context.getStatus() == InterceptResult.STATUS_WEAK_PASSED) {
+            interceptResult.setStatus(context.getStatus());
+            interceptResult.setMessage(context.getMessages().get(0));
+            interceptResult.setCode(JdResult.CODE_SUC);
+        }
+        return interceptResult;
+    }
+
+    /**
+     * @return the handlers
+     */
+    public List<Handler<WaybillPrintContext, JdResult<String>>> getHandlers() {
+        return handlers;
+    }
+
+    /**
+     * @param handlers the handlers to set
+     */
+    public void setHandlers(
+            List<Handler<WaybillPrintContext, JdResult<String>>> handlers) {
+        this.handlers = handlers;
+    }
 }
