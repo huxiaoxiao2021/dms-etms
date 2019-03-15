91454085d02c93b892c3b27883a6d81f494ccdf8
liuduo8 <liuduo3@jd.com>
2019-03-15 15:33:53 +0800

    缺量退仓集齐开关

--- bluedragon-distribution-core/src/main/java/com/jd/bluedragon/distribution/send/service/DeliveryServiceImpl.java
+++ bluedragon-distribution-core/src/main/java/com/jd/bluedragon/distribution/send/service/DeliveryServiceImpl.java
@@ -3202,7 +3202,7 @@ public class DeliveryServiceImpl implements DeliveryService {
             tDeliveryResponse = reverseComputer.compute(allList, false);
 
             //退仓时 增加 支持半退逻辑
-            if(sysConfigService.getConfigByName("reverse.part.not.check.switch")){
+            if(!sysConfigService.getConfigByName("reverse.part.not.check.switch")){
                 ThreeDeliveryResponse response = checkReversePartSend(tDeliveryResponse,allList);
                 if(response.getCode().equals(DeliveryResponse.CODE_Delivery_PART_SEND_ERROR) || response.getCode().equals(DeliveryResponse.CODE_Delivery_PART_SEND) ){
                     return response;
