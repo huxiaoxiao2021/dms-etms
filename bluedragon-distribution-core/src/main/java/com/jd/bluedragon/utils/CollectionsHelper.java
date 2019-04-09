d64b131a13692fe26c693ee182fad26728b14993
xumigen <xumigen@jd.com>
2019-04-09 18:22:43 +0800

    分拣、发货入口包裹滑道号不全拦截

---
+++ bluedragon-distribution-core/src/main/java/com/jd/bluedragon/utils/CollectionsHelper.java
@@ -0,0 +1,32 @@
+package com.jd.bluedragon.utils;
+
+
+public class CollectionsHelper {
+
+//    private static Logger logger = Logger.getLogger(CollectionsHelper.class);
+//
+//    public static String join(Collection<?> objects, String methodName, String separator,
+//                              String wrapSeparator) {
+//        StringBuilder sb = new StringBuilder();
+//
+//        for (Iterator<?> iterator = objects.iterator(); iterator.hasNext();) {
+//            Object o = iterator.next();
+//
+//            try {
+//                Method method = o.getClass().getMethod(methodName);
+//                String value = String.valueOf(method.invoke(o));
+//
+//                if (value != null) {
+//                    sb.append(wrapSeparator);
+//                    sb.append(value);
+//                    sb.append(wrapSeparator);
+//                }
+//
+//                if (iterator.hasNext()) {
+//                    sb.append(separator);
+//                }
+//        }
+//
+//        return sb.toString();
+//    }
+}
