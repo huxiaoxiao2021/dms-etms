2b4d4beee35c1197af8c443d865619562ceba0cb
xumigen <xumigen@jd.com>
2019-04-12 21:31:37 +0800

    安全检查测试

--- bluedragon-distribution-web/src/test/java/com/jd/bluedragon/distribution/rest/sorting/SortingResourceTest.java
+++ bluedragon-distribution-web/src/test/java/com/jd/bluedragon/distribution/rest/sorting/SortingResourceTest.java
@@ -1,5 +1,5 @@
 package com.jd.bluedragon.distribution.rest.sorting;
-
+import com.jd.bluedragon.distribution.api.request.SortingRequest;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,7 +10,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
  * Created by xumigen on 2019/4/11.
  */
 
-@ContextConfiguration( {"classpath:distribution-web-context.xml"})
+@ContextConfiguration( {"classpath:distribution-web-context-test.xml"})
 @RunWith(SpringJUnit4ClassRunner.class)
 public class SortingResourceTest {
 
@@ -19,6 +19,28 @@ public class SortingResourceTest {
 
     @Test
     public void testcancelPackage(){
-        sortingResource.cancelPackage(null);
+        SortingRequest request = new SortingRequest();
+        request.setBoxCode("85434861526-1-1-1");
+        request.setPackageCode("85434861526-1-1-1");
+        request.setWaybillCode("85434861526");
+        request.setIsCancel(0);
+        request.setReceiveSiteCode(0);
+        request.setReceiveSiteName("");
+        request.setSealCode("");
+        request.setIsLoss(0);
+        request.setFeatureType(0);
+        request.setWhReverse(0);
+//        request.setPackages(Lists.newArrayList());
+        request.setBsendCode("");
+        request.setKey("");
+        request.setUserCode(0);
+        request.setUserName("");
+        request.setSiteCode(0);
+        request.setSiteName("");
+        request.setBusinessType(0);
+        request.setId(0);
+        request.setOperateTime("");
+
+        sortingResource.cancelPackage(request);
     }
 }
