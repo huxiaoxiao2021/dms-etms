64f90b7d8cb2db209e098a949c0342b8f171bff4
liuduo8 <liuduo3@jd.com>
2019-04-15 15:17:18 +0800

    新分拣任务日志监控变更UMP应用

--- bluedragon-distribution-core/src/main/java/com/jd/bluedragon/distribution/sorting/service/SortingPackServiceImpl.java
+++ bluedragon-distribution-core/src/main/java/com/jd/bluedragon/distribution/sorting/service/SortingPackServiceImpl.java
@@ -27,7 +27,7 @@ public class SortingPackServiceImpl extends SortingCommonSerivce{
 
 
     @Override
-    @JProfiler(jKey = "DMS.BASE.SortingPackServiceImpl.doSorting", mState = {JProEnum.TP, JProEnum.FunctionError},jAppName= Constants.UMP_APP_NAME_DMSWEB)
+    @JProfiler(jKey = "DMS.BASE.SortingPackServiceImpl.doSorting", mState = {JProEnum.TP, JProEnum.FunctionError},jAppName= Constants.UMP_APP_NAME_DMSWORKER)
     public boolean doSorting(SortingVO sorting) {
         if(sorting.getIsCancel().equals(SortingService.SORTING_CANCEL_NORMAL)){
             //分拣
--- bluedragon-distribution-core/src/main/java/com/jd/bluedragon/distribution/sorting/service/SortingWaybillServiceImpl.java
+++ bluedragon-distribution-core/src/main/java/com/jd/bluedragon/distribution/sorting/service/SortingWaybillServiceImpl.java
@@ -32,7 +32,7 @@ public class SortingWaybillServiceImpl extends SortingCommonSerivce{
     public static int WAYBILL_SPLIT_NUM = 100;
 
     @Override
-    @JProfiler(jKey = "DMS.BASE.SortingWaybillServiceImpl.doSorting", mState = {JProEnum.TP, JProEnum.FunctionError},jAppName= Constants.UMP_APP_NAME_DMSWEB)
+    @JProfiler(jKey = "DMS.BASE.SortingWaybillServiceImpl.doSorting", mState = {JProEnum.TP, JProEnum.FunctionError},jAppName= Constants.UMP_APP_NAME_DMSWORKER)
     public boolean doSorting(SortingVO sorting) {
         if (sorting.getIsCancel().equals(getSortingService().SORTING_CANCEL_NORMAL)) {
 
