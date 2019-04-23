006baff2a6cd4f7b9bdcf9788fe4ef711eaf6a8f
wuyoude <wuyoude@jd.com>
2019-04-23 10:47:39 +0800

    utils包新增2个常量，用于定义标位相关的常量

---
+++ bluedragon-distribution-utils/src/main/java/com/jd/bluedragon/dms/utils/SendPayConstants.java
@@ -0,0 +1,12 @@
+package com.jd.bluedragon.dms.utils;
+/**
+ * 
+ * @ClassName: SendPayConstants
+ * @Description: sendPay相关的常量定义
+ * @author: wuyoude
+ * @date: 2019年4月23日 上午10:19:10
+ *
+ */
+public interface SendPayConstants {
+	
+}
