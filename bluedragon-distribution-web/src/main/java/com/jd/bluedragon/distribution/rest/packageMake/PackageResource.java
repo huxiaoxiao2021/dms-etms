fe638a77c1d0182943045f8fd8919d9c51eef367
wuyoude <wuyoude@jd.com>
2019-04-25 20:41:16 +0800

    包裹补打-触发全程跟踪逻辑修改

--- bluedragon-distribution-web/src/main/java/com/jd/bluedragon/distribution/rest/packageMake/PackageResource.java
+++ bluedragon-distribution-web/src/main/java/com/jd/bluedragon/distribution/rest/packageMake/PackageResource.java
@@ -4,6 +4,7 @@ import java.util.Date;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -11,7 +12,6 @@ import javax.ws.rs.core.MediaType;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.codehaus.jra.Post;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
