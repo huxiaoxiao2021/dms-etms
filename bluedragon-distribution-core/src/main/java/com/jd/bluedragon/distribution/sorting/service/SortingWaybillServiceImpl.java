66b233037ece880cb17c952b373e90dd2af1399e
xumei3 <xumei3@jd.com>
2019-05-06 16:52:09 +0800

    SortingWaybillServiceImpl#isNeedInspection增加运单为空的判断条件

--- bluedragon-distribution-core/src/main/java/com/jd/bluedragon/distribution/sorting/service/SortingWaybillServiceImpl.java
+++ bluedragon-distribution-core/src/main/java/com/jd/bluedragon/distribution/sorting/service/SortingWaybillServiceImpl.java
@@ -14,6 +14,7 @@ import com.jd.etms.waybill.dto.BigWaybillDto;
 import com.jd.ql.basic.dto.BaseStaffSiteOrgDto;
 import com.jd.ump.annotation.JProEnum;
 import com.jd.ump.annotation.JProfiler;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
@@ -89,7 +90,7 @@ public class SortingWaybillServiceImpl extends SortingCommonSerivce{
     public boolean isNeedInspection(SortingVO sorting) {
         BaseStaffSiteOrgDto createSite = sorting.getCreateSite();
         //B网建箱自动触发验货全程跟踪
-        if (createSite==null || Constants.B2B_SITE_TYPE!=createSite.getSubType()){
+        if (createSite==null || Constants.B2B_SITE_TYPE!=createSite.getSubType() || StringUtils.isBlank(sorting.getWaybillCode())){
             return false;
         }
         Inspection inspectionQ=new Inspection();
