bb6c1d8d8738aa9019d5fd6cb03775292be698d0
lixin456 <lixin39@jd.com>
2019-05-17 17:49:44 +0800

    箱号和批次号标签增加笼车号 kb#79

--- bluedragon-distribution-sdk/src/main/java/com/jd/bluedragon/distribution/api/response/BoxResponse.java
+++ bluedragon-distribution-sdk/src/main/java/com/jd/bluedragon/distribution/api/response/BoxResponse.java
@@ -6,18 +6,16 @@ import java.util.Date;
 
 public class BoxResponse extends JdResponse {
 
-	private static final long serialVersionUID = 6421643159029953636L;
+    private static final long serialVersionUID = 6421643159029953636L;
 
+    public static final Integer CODE_BOX_NOT_FOUND = 20101;
+    public static final String MESSAGE_BOX_NOT_FOUND = "无箱号信息";
 
+    public static final Integer CODE_BOX_NO_PRINT = 20102;
+    public static final String MESSAGE_BOX_NO_PRINT = "箱号无打印";
 
-	public static final Integer CODE_BOX_NOT_FOUND = 20101;
-	public static final String MESSAGE_BOX_NOT_FOUND = "无箱号信息";
-
-	public static final Integer CODE_BOX_NO_PRINT = 20102;
-	public static final String MESSAGE_BOX_NO_PRINT = "箱号无打印";
-
-	public static final Integer CODE_BOX_SENDED = 20104;
-	public static final String MESSAGE_BOX_SENDED = "此箱号已经发货";
+    public static final Integer CODE_BOX_SENDED = 20104;
+    public static final String MESSAGE_BOX_SENDED = "此箱号已经发货";
 
     public static final Integer CODE_SITE_SENDED = 20105;
     public static final String MESSAGE_SITE_SENDED = "此箱号对应站点异常";
@@ -25,82 +23,118 @@ public class BoxResponse extends JdResponse {
     public static final Integer CODE_BOX_ROUTER = 20106;
     public static final String MESSAGE_BOX_ROUTER = "获取路由信息异常";
 
+    /**
+     * 全局唯一ID
+     */
+    private Long id;
 
+    /**
+     * 创建时间
+     */
+    private String createTime;
 
-	/** 全局唯一ID */
-	private Long id;
+    /**
+     * 箱号
+     */
+    private String boxCode;
 
-	/** 创建时间 */
-	private String createTime;
+    /**
+     * 箱号
+     */
+    private String boxCodes;
 
-	/** 箱号 */
-	private String boxCode;
+    /**
+     * 创建站点编号
+     */
+    private Integer createSiteCode;
 
-	/** 箱号 */
-	private String boxCodes;
+    /**
+     * 创建站点名称
+     */
+    private String createSiteName;
 
-	/** 创建站点编号 */
-	private Integer createSiteCode;
+    /**
+     * 接收站点编号
+     */
+    private Integer receiveSiteCode;
 
-	/** 创建站点名称 */
-	private String createSiteName;
+    /**
+     * 接收站点名称
+     */
+    private String receiveSiteName;
 
-	/** 接收站点编号 */
-	private Integer receiveSiteCode;
+    private String type;
 
-	/** 接收站点名称 */
-	private String receiveSiteName;
+    /**
+     * 运输方式 '1' 航空运输 '2' 公路运输 '3' 铁路运输
+     */
+    private Integer transportType;
 
-	private String type;
+    /**
+     * 箱子的路由信息
+     */
+    private String[] routerInfo;
 
-	/** 运输方式 '1' 航空运输 '2' 公路运输 '3' 铁路运输 */
-	private Integer transportType;
-	
-	/** 箱子的路由信息 */
-	private String[] routerInfo;
+    /**
+     * 箱子路由信息 汉字描述，超过5个站点，打印系统直接用他打印
+     */
+    private String routerText;
 
-	/** 箱子路由信息 汉字描述，超过5个站点，打印系统直接用他打印*/
-	private String routerText;
+    /**
+     * 箱子路由信息 站点id
+     */
+    private String[] routerFullId;
 
-	/** 箱子路由信息 站点id */
-	private String[] routerFullId;
-
-    /** 站点类型**/
+    /**
+     * 站点类型
+     **/
     private Integer siteType;
 
-	/** 预计发货时间*/
-	private Date predictSendTime ;
+    /**
+     * 预计发货时间
+     */
+    private Date predictSendTime;
 
-	/**
-	 * 混包类型 1混包，0非混包
-	 */
-	private Integer mixBoxType;
+    /**
+     * 混包类型 1混包，0非混包
+     */
+    private Integer mixBoxType;
 
-	public String getRouterText() {
-		return routerText;
-	}
+    /**
+     * 目的道口号
+     **/
+    private String destinationCrossCode;
 
-	public void setRouterText(String routerText) {
-		this.routerText = routerText;
-	}
+    /**
+     * 目的笼车号
+     **/
+    private String destinationTabletrolleyCode;
 
-	public Date getPredictSendTime() {
-		return predictSendTime;
-	}
+    public String getRouterText() {
+        return routerText;
+    }
 
-	public void setPredictSendTime(Date predictSendTime) {
-		this.predictSendTime = predictSendTime;
-	}
+    public void setRouterText(String routerText) {
+        this.routerText = routerText;
+    }
 
-	public String[] getRouterFullId() {
-		return routerFullId;
-	}
+    public Date getPredictSendTime() {
+        return predictSendTime;
+    }
 
-	public void setRouterFullId(String[] routerFullId) {
-		this.routerFullId = routerFullId;
-	}
+    public void setPredictSendTime(Date predictSendTime) {
+        this.predictSendTime = predictSendTime;
+    }
 
-	public Integer getSiteType() {
+    public String[] getRouterFullId() {
+        return routerFullId;
+    }
+
+    public void setRouterFullId(String[] routerFullId) {
+        this.routerFullId = routerFullId;
+    }
+
+    public Integer getSiteType() {
         return siteType;
     }
 
@@ -109,106 +143,122 @@ public class BoxResponse extends JdResponse {
     }
 
     public Integer getTransportType() {
-		return transportType;
-	}
+        return transportType;
+    }
 
-	public void setTransportType(Integer transportType) {
-		this.transportType = transportType;
-	}
+    public void setTransportType(Integer transportType) {
+        this.transportType = transportType;
+    }
 
-	public String getType() {
-		return type;
-	}
+    public String getType() {
+        return type;
+    }
 
-	public void setType(String type) {
-		this.type = type;
-	}
+    public void setType(String type) {
+        this.type = type;
+    }
 
-	public BoxResponse() {
-		super();
-	}
+    public BoxResponse() {
+        super();
+    }
 
-	public BoxResponse(Integer code, String message) {
-		super(code, message);
-	}
+    public BoxResponse(Integer code, String message) {
+        super(code, message);
+    }
 
-	public Long getId() {
-		return this.id;
-	}
+    public Long getId() {
+        return this.id;
+    }
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-	public String getBoxCode() {
-		return this.boxCode;
-	}
+    public String getBoxCode() {
+        return this.boxCode;
+    }
 
-	public void setBoxCode(String boxCode) {
-		this.boxCode = boxCode;
-	}
+    public void setBoxCode(String boxCode) {
+        this.boxCode = boxCode;
+    }
 
-	public String getCreateTime() {
-		return this.createTime;
-	}
+    public String getCreateTime() {
+        return this.createTime;
+    }
 
-	public void setCreateTime(String createTime) {
-		this.createTime = createTime;
-	}
+    public void setCreateTime(String createTime) {
+        this.createTime = createTime;
+    }
 
-	public Integer getCreateSiteCode() {
-		return this.createSiteCode;
-	}
+    public Integer getCreateSiteCode() {
+        return this.createSiteCode;
+    }
 
-	public void setCreateSiteCode(Integer createSiteCode) {
-		this.createSiteCode = createSiteCode;
-	}
+    public void setCreateSiteCode(Integer createSiteCode) {
+        this.createSiteCode = createSiteCode;
+    }
 
-	public String getCreateSiteName() {
-		return this.createSiteName;
-	}
+    public String getCreateSiteName() {
+        return this.createSiteName;
+    }
 
-	public void setCreateSiteName(String createSiteName) {
-		this.createSiteName = createSiteName;
-	}
+    public void setCreateSiteName(String createSiteName) {
+        this.createSiteName = createSiteName;
+    }
 
-	public Integer getReceiveSiteCode() {
-		return this.receiveSiteCode;
-	}
+    public Integer getReceiveSiteCode() {
+        return this.receiveSiteCode;
+    }
 
-	public void setReceiveSiteCode(Integer receiveSiteCode) {
-		this.receiveSiteCode = receiveSiteCode;
-	}
+    public void setReceiveSiteCode(Integer receiveSiteCode) {
+        this.receiveSiteCode = receiveSiteCode;
+    }
 
-	public String getReceiveSiteName() {
-		return this.receiveSiteName;
-	}
+    public String getReceiveSiteName() {
+        return this.receiveSiteName;
+    }
 
-	public void setReceiveSiteName(String receiveSiteName) {
-		this.receiveSiteName = receiveSiteName;
-	}
+    public void setReceiveSiteName(String receiveSiteName) {
+        this.receiveSiteName = receiveSiteName;
+    }
 
-	public String getBoxCodes() {
-		return this.boxCodes;
-	}
+    public String getBoxCodes() {
+        return this.boxCodes;
+    }
 
-	public void setBoxCodes(String boxCodes) {
-		this.boxCodes = boxCodes;
-	}
+    public void setBoxCodes(String boxCodes) {
+        this.boxCodes = boxCodes;
+    }
 
-	public String[] getRouterInfo() {
-		return routerInfo;
-	}
+    public String[] getRouterInfo() {
+        return routerInfo;
+    }
 
-	public void setRouterInfo(String[] routerInfo) {
-		this.routerInfo = routerInfo;
-	}
+    public void setRouterInfo(String[] routerInfo) {
+        this.routerInfo = routerInfo;
+    }
 
-	public Integer getMixBoxType() {
-		return mixBoxType;
-	}
+    public Integer getMixBoxType() {
+        return mixBoxType;
+    }
 
-	public void setMixBoxType(Integer mixBoxType) {
-		this.mixBoxType = mixBoxType;
-	}
+    public void setMixBoxType(Integer mixBoxType) {
+        this.mixBoxType = mixBoxType;
+    }
+
+    public String getDestinationCrossCode() {
+        return destinationCrossCode;
+    }
+
+    public void setDestinationCrossCode(String destinationCrossCode) {
+        this.destinationCrossCode = destinationCrossCode;
+    }
+
+    public String getDestinationTabletrolleyCode() {
+        return destinationTabletrolleyCode;
+    }
+
+    public void setDestinationTabletrolleyCode(String destinationTabletrolleyCode) {
+        this.destinationTabletrolleyCode = destinationTabletrolleyCode;
+    }
 }
