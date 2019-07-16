b6d42de9b587620fc7fd8a50e0c03b5881c31f10
xumigen <xumigen@jd.com>
2019-07-16 20:00:10 +0800

    添加 根据运单号获取 预分拣站点 与自营站点

---
+++ bluedragon-distribution-web/src/main/java/com/jd/bluedragon/distribution/external/gateway/service/impl/WaybillGateWayServiceImpl.java
@@ -0,0 +1,34 @@
+package com.jd.bluedragon.distribution.external.gateway.service.impl;
+
+import com.jd.bluedragon.common.dto.base.response.JdCResponse;
+import com.jd.bluedragon.distribution.base.domain.InvokeResult;
+import com.jd.bluedragon.distribution.rest.base.BaseResource;
+import com.jd.bluedragon.external.gateway.service.WaybillGateWayService;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ * 运单相关
+ * @author : xumigen
+ * @date : 2019/7/16
+ */
+public class WaybillGateWayServiceImpl implements WaybillGateWayService {
+
+    @Resource
+    private BaseResource baseResource;
+
+    @Override
+    public JdCResponse<List<Integer>> getPerAndSelfSiteByWaybillCode(String waybillCode){
+        InvokeResult<List<Integer>> invokeResult = baseResource.perAndSelfSite(waybillCode);
+        JdCResponse<List<Integer>> jdCResponse = new JdCResponse<>();
+        if(InvokeResult.RESULT_SUCCESS_CODE == invokeResult.getCode()){
+            jdCResponse.toSucceed(invokeResult.getMessage());
+            jdCResponse.setData(invokeResult.getData());
+            return jdCResponse;
+        }
+        jdCResponse.toError(invokeResult.getMessage());
+        return jdCResponse;
+    }
+
+}
