809b427d3fc790be9a71e3e58b65fa16e5cc4f70
hujiping1 <hujiping>
2019-07-18 22:28:37 +0800

    分拣复重抽检任务统计优化

--- bluedragon-distribution-web/src/main/webapp/static/js/weightAndVolumeCheck/reviewWeightSpotCheck.js
+++ bluedragon-distribution-web/src/main/webapp/static/js/weightAndVolumeCheck/reviewWeightSpotCheck.js
@@ -22,8 +22,8 @@ $(function () {
                 uniqueId: "ID", // 每一行的唯一标识，一般为主键列
                 pagination: true, // 是否显示分页（*）
                 pageNumber: 1, // 初始化加载第一页，默认第一页
-                pageSize: 10, // 每页的记录行数（*）
-                pageList: [10, 25, 50, 100], // 可供选择的每页的行数（*）
+                pageSize: 500, // 每页的记录行数（*）
+                pageList: [200, 500], // 可供选择的每页的行数（*）
                 cache: false, // 是否使用缓存，默认为true，所以一般情况下需要设置一下这个属性（*）
                 sidePagination: "server", // 分页方式：client客户端分页，server服务端分页（*）
                 striped: true, // 是否显示行间隔色
