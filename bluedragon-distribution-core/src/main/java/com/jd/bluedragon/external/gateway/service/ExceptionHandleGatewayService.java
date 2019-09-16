0b95393d2c1434100c23305385fb44eb90f14b9d
lixin456 <lixin39@jd.com>
2019-09-16 22:21:23 +0800

    安卓版本PDA 异常提交 - 接口补充

--- bluedragon-distribution-core/src/main/java/com/jd/bluedragon/external/gateway/service/ExceptionHandleGatewayService.java
+++ bluedragon-distribution-core/src/main/java/com/jd/bluedragon/external/gateway/service/ExceptionHandleGatewayService.java
@@ -1,7 +1,10 @@
 package com.jd.bluedragon.external.gateway.service;
 
 import com.jd.bluedragon.distribution.api.request.QualityControlRequest;
+import com.jd.bluedragon.distribution.api.request.RedeliveryCheckRequest;
 import com.jd.bluedragon.distribution.api.response.QualityControlResponse;
+import com.jd.bluedragon.distribution.base.domain.InvokeResult;
+import com.jd.bluedragon.distribution.qualityControl.domain.RedeliveryMode;
 
 /**
  * @author lixin39
@@ -10,6 +13,14 @@ import com.jd.bluedragon.distribution.api.response.QualityControlResponse;
  * @date 2019/9/16
  */
 public interface ExceptionHandleGatewayService {
+
+    /**
+     * 协商再投校验
+     *
+     * @param request
+     * @return
+     */
+    InvokeResult<RedeliveryMode> reDeliveryCheck(RedeliveryCheckRequest request);
 
     /**
      * 异常提交
--- bluedragon-distribution-web/src/main/java/com/jd/bluedragon/distribution/external/gateway/service/impl/ExceptionHandleGatewayServiceImpl.java
+++ bluedragon-distribution-web/src/main/java/com/jd/bluedragon/distribution/external/gateway/service/impl/ExceptionHandleGatewayServiceImpl.java
@@ -1,7 +1,10 @@
 package com.jd.bluedragon.distribution.external.gateway.service.impl;
 
 import com.jd.bluedragon.distribution.api.request.QualityControlRequest;
+import com.jd.bluedragon.distribution.api.request.RedeliveryCheckRequest;
 import com.jd.bluedragon.distribution.api.response.QualityControlResponse;
+import com.jd.bluedragon.distribution.base.domain.InvokeResult;
+import com.jd.bluedragon.distribution.qualityControl.domain.RedeliveryMode;
 import com.jd.bluedragon.distribution.rest.QualityControl.QualityControlResource;
 import com.jd.bluedragon.external.gateway.service.ExceptionHandleGatewayService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,6 +21,11 @@ public class ExceptionHandleGatewayServiceImpl implements ExceptionHandleGateway
     @Autowired
     @Qualifier("qualityControlResource")
     private QualityControlResource qualityControlResource;
+
+    @Override
+    public InvokeResult<RedeliveryMode> reDeliveryCheck(RedeliveryCheckRequest request) {
+        return qualityControlResource.redeliveryCheckNew(request);
+    }
 
     @Override
     public QualityControlResponse exceptionSubmit(QualityControlRequest request) {
