baf7f8b91baa9bf981857c13c7beb48d327ced15
xumigen <xumigen@jd.com>
2019-09-21 11:58:04 +0800

    铁路计费 补单元测试

---
+++ bluedragon-distribution-core/src/test/java/com/jd/bluedragon/distribution/base/service/impl/DmsBaseDictServiceImplTest.java
@@ -0,0 +1,26 @@
+package com.jd.bluedragon.distribution.base.service.impl;
+
+import com.jd.bluedragon.Constants;
+import com.jd.bluedragon.distribution.base.service.DmsBaseDictService;
+import com.jd.bluedragon.distribution.dao.common.AbstractDaoIntegrationTest;
+import org.junit.Assert;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Map;
+
+/**
+ * @author : xumigen
+ * @date : 2019/9/21
+ */
+public class DmsBaseDictServiceImplTest extends AbstractDaoIntegrationTest {
+
+    @Autowired
+    private DmsBaseDictService dmsBaseDictService;
+
+    @Test
+    public void testqueryMapKeyTypeCodeByParentId(){
+        Map<Integer,String> map = dmsBaseDictService.queryMapKeyTypeCodeByParentId(Constants.BASEDICT_GOODS_TYPE_PARENTID);
+        Assert.assertTrue(map.size() == 3);
+    }
+}
