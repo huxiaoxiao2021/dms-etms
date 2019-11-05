843abd94184428928e8ab4a77db07df081d2abc6
xumigen <xumigen@jd.com>
2019-11-05 18:06:15 +0800

    安卓 空铁-新发货登记 切换通过web调用运输

--- bluedragon-distribution-core/src/main/java/com/jd/bluedragon/core/base/EcpAirWSManagerImpl.java
+++ bluedragon-distribution-core/src/main/java/com/jd/bluedragon/core/base/EcpAirWSManagerImpl.java
@@ -1,6 +1,7 @@
 package com.jd.bluedragon.core.base;
 
 import com.jd.bluedragon.Constants;
+import com.jd.bluedragon.distribution.base.domain.InvokeResult;
 import com.jd.bluedragon.utils.JsonHelper;
 import com.jd.tms.ecp.dto.AirDepartInfoDto;
 import com.jd.tms.ecp.dto.CommonDto;
@@ -26,25 +27,39 @@ public class EcpAirWSManagerImpl implements EcpAirWSManager {
 
     @Override
     @JProfiler(jKey = "DMS.BASE.EcpAirWSManagerImpl.submitSortAirDepartInfo", mState = {JProEnum.TP, JProEnum.FunctionError},jAppName= Constants.UMP_APP_NAME_DMSWEB)
-    public boolean submitSortAirDepartInfo(AirDepartInfoDto param) {
+    public InvokeResult<String> submitSortAirDepartInfo(AirDepartInfoDto param) {
+        InvokeResult<String> result = new InvokeResult<>();
+        result.success();
         CommonDto<String> commonDto = ecpAirWS.submitSortAirDepartInfo(param);
         logger.info("分拣发货登记提交调用运输接口param[{}]commonDto[{}]", JsonHelper.toJson(param),JsonHelper.toJson(commonDto));
-        if(commonDto == null || commonDto.getCode() != CommonDto.CODE_SUCCESS){
-            return false;
+        if(commonDto == null){
+            result.error("发货登记提交失败！");
+            return result;
         }
-        return true;
+        if(commonDto.getCode() != CommonDto.CODE_SUCCESS){
+            result.error(commonDto.getMessage());
+            return result;
+        }
+        return result;
     }
 
 
     @Override
     @JProfiler(jKey = "DMS.BASE.EcpAirWSManagerImpl.supplementSortAirDepartInfo", mState = {JProEnum.TP, JProEnum.FunctionError},jAppName= Constants.UMP_APP_NAME_DMSWEB)
-    public boolean supplementSortAirDepartInfo(AirDepartInfoDto param) {
+    public InvokeResult<String> supplementSortAirDepartInfo(AirDepartInfoDto param) {
+        InvokeResult<String> result = new InvokeResult<>();
+        result.success();
         CommonDto<String> commonDto = ecpAirWS.supplementSortAirDepartInfo(param);
-        logger.info("分拣发货登记提交调用运输接口param[{}]commonDto[{}]", JsonHelper.toJson(param),JsonHelper.toJson(commonDto));
-        if(commonDto == null || commonDto.getCode() != CommonDto.CODE_SUCCESS){
-            return false;
+        logger.info("分拣发补货登记提交调用运输接口param[{}]commonDto[{}]", JsonHelper.toJson(param),JsonHelper.toJson(commonDto));
+        if(commonDto == null){
+            result.error("补加批次提交失败！");
+            return result;
         }
-        return true;
+        if(commonDto.getCode() != CommonDto.CODE_SUCCESS){
+            result.error(commonDto.getMessage());
+            return result;
+        }
+        return result;
     }
 
 
