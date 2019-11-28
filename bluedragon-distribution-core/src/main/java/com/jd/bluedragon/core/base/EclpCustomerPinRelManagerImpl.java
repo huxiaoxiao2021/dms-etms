82bde3e28fc2bbd387cea6ceb2e11173bdf7346f
lixin456 <lixin39@jd.com>
2019-11-28 14:05:59 +0800

    日志打印调整

--- bluedragon-distribution-core/src/main/java/com/jd/bluedragon/core/base/EclpCustomerPinRelManagerImpl.java
+++ bluedragon-distribution-core/src/main/java/com/jd/bluedragon/core/base/EclpCustomerPinRelManagerImpl.java
@@ -1,12 +1,15 @@
 package com.jd.bluedragon.core.base;
 
+import com.jd.bluedragon.Constants;
 import com.jd.eclp.core.ApiResponse;
 import com.jd.eclp.master.constant.ChannelEnum;
 import com.jd.logistics.customer.center.domain.CustomerPinRel;
 import com.jd.logistics.customer.customerPinRel.CustomerPinRelService;
+import com.jd.ump.annotation.JProEnum;
+import com.jd.ump.annotation.JProfiler;
 import org.apache.commons.lang.StringUtils;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -19,11 +22,13 @@ import org.springframework.stereotype.Service;
 @Service
 public class EclpCustomerPinRelManagerImpl implements EclpCustomerPinRelManager {
 
-    private final Log logger = LogFactory.getLog(this.getClass());
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
 
     @Autowired
     private CustomerPinRelService customerPinRelService;
 
+    @JProfiler(jKey = "DMS.BASE.EclpCustomerPinRelManagerImpl.getCustomerPinRelByPin", jAppName = Constants.UMP_APP_NAME_DMSWEB,
+            mState = {JProEnum.TP, JProEnum.FunctionError})
     @Override
     public CustomerPinRel getCustomerPinRelByPin(String pin) {
         if (StringUtils.isBlank(pin)) {
@@ -33,7 +38,7 @@ public class EclpCustomerPinRelManagerImpl implements EclpCustomerPinRelManager 
         if (response.isSuccess()) {
             return response.getData();
         } else {
-            logger.error(String.format("[ECLP开放平台-客户中心-客户跟pin关系接口]查询失败，状态码：%s，信息：%s", response.getCode(), response.getMsg()));
+            log.error("[ECLP开放平台-客户中心-客户跟pin关系接口]查询失败，状态码：{}，信息：{}", response.getCode(), response.getMsg());
         }
         return null;
     }
