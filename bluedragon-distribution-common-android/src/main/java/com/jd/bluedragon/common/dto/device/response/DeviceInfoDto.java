a9b1164e34be603443022f359b469a449fd01cff
wuzuxiang <wuzuxiang@jd.com>
2019-12-02 17:08:12 +0800

    添加Android的sdk，设备相关类，并升级到1.0.8

---
+++ bluedragon-distribution-common-android/src/main/java/com/jd/bluedragon/common/dto/device/response/DeviceInfoDto.java
@@ -0,0 +1,80 @@
+package com.jd.bluedragon.common.dto.device.response;
+
+import java.io.Serializable;
+
+/**
+ * <p>
+ *     设备信息返回类
+ *
+ * @author wuzuxiang
+ * @since 2019/12/2
+ **/
+public class DeviceInfoDto implements Serializable {
+
+    private static final long serialVersionUID = -1L;
+
+    /**
+     * 设备号
+     */
+    private String machineCode;
+
+    /**
+     * 设备创建站点
+     */
+    private String siteCode;
+
+    /**
+     * 设备类型编号
+     */
+    private String deviceTypeCode;
+
+    /**
+     * 设备类型名称
+     */
+    private String deviceTypeName;
+
+    /**
+     * 是否启用
+     */
+    private Integer isEnable;
+
+    public String getMachineCode() {
+        return machineCode;
+    }
+
+    public void setMachineCode(String machineCode) {
+        this.machineCode = machineCode;
+    }
+
+    public String getSiteCode() {
+        return siteCode;
+    }
+
+    public void setSiteCode(String siteCode) {
+        this.siteCode = siteCode;
+    }
+
+    public String getDeviceTypeCode() {
+        return deviceTypeCode;
+    }
+
+    public void setDeviceTypeCode(String deviceTypeCode) {
+        this.deviceTypeCode = deviceTypeCode;
+    }
+
+    public String getDeviceTypeName() {
+        return deviceTypeName;
+    }
+
+    public void setDeviceTypeName(String deviceTypeName) {
+        this.deviceTypeName = deviceTypeName;
+    }
+
+    public Integer getIsEnable() {
+        return isEnable;
+    }
+
+    public void setIsEnable(Integer isEnable) {
+        this.isEnable = isEnable;
+    }
+}
