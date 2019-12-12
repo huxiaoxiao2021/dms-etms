665f19e63a11e0ea7aa38ba05ea909b2a913b9f3
shipeilin <shipeilin@jd.com>
2019-12-12 15:08:13 +0800

    web日志优化：30个类

--- bluedragon-distribution-web/src/main/java/com/jd/bluedragon/distribution/rest/reverse/ReverseRejectResource.java
+++ bluedragon-distribution-web/src/main/java/com/jd/bluedragon/distribution/rest/reverse/ReverseRejectResource.java
@@ -1,21 +1,5 @@
 package com.jd.bluedragon.distribution.rest.reverse;
 
-import java.util.List;
-
-import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
-
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.stereotype.Component;
-
 import com.jd.bluedragon.Constants;
 import com.jd.bluedragon.common.domain.Waybill;
 import com.jd.bluedragon.common.service.WaybillCommonService;
@@ -23,14 +7,15 @@ import com.jd.bluedragon.distribution.api.JdResponse;
 import com.jd.bluedragon.distribution.api.response.BaseResponse;
 import com.jd.bluedragon.distribution.base.service.BaseService;
 import com.jd.bluedragon.distribution.reverse.domain.ReverseReject;
-import com.jd.bluedragon.distribution.reverse.domain.ReverseSendSpwmsTest;
 import com.jd.bluedragon.distribution.reverse.domain.ReverseSendWms;
 import com.jd.bluedragon.distribution.reverse.service.ReverseRejectService;
-import com.jd.bluedragon.distribution.reverse.service.ReverseSendService;
-import com.jd.bluedragon.distribution.task.domain.Task;
-import com.jd.bluedragon.distribution.task.service.TaskService;
-import com.jd.bluedragon.distribution.wss.service.ReverseWssService;
-import com.jd.bluedragon.utils.XmlHelper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
 
 @Component
 @Path(Constants.REST_URL)
@@ -38,7 +23,7 @@ import com.jd.bluedragon.utils.XmlHelper;
 @Produces({ MediaType.APPLICATION_JSON })
 public class ReverseRejectResource {
 
-	private final Log logger = LogFactory.getLog(this.getClass());
+	private final Logger log = LoggerFactory.getLogger(this.getClass());
 
 	@Autowired
 	private BaseService tBaseService;
@@ -52,7 +37,7 @@ public class ReverseRejectResource {
 	@GET
 	@Path("/base/getWaybill/{code}")
 	public BaseResponse getWaybill(@PathParam("code") String code) {
-		this.logger.info("getWaybill is " + code);
+		this.log.info("getWaybill is {}", code);
 
 		Waybill wb = waybillCommonService.getWaybillFromOrderService(code);
 		if (null != wb) {
@@ -61,7 +46,7 @@ public class ReverseRejectResource {
 			response.setCarId(wb.getSendPay());
 			return response;
 		} else {
-			this.logger.error(code + "获取订单中间件ooms订单数据失败");
+			this.log.warn("{}获取订单中间件ooms订单数据失败",code);
 			Waybill hwb = waybillCommonService.getHisWaybillFromOrderService(code);
 			if (null != hwb) {
 				BaseResponse response = new BaseResponse(JdResponse.CODE_OK, JdResponse.MESSAGE_OK);
@@ -69,7 +54,7 @@ public class ReverseRejectResource {
 				response.setCarId(hwb.getSendPay());
 				return response;
 			} else {
-				this.logger.error(code + "获取订单中间件ooms历史表订单数据失败");
+				this.log.warn("{}获取订单中间件ooms历史表订单数据失败",code);
 				BaseResponse response = new BaseResponse(JdResponse.CODE_OK, JdResponse.MESSAGE_OK);
 				response.setCarCode("none");
 				response.setCarId("none");
