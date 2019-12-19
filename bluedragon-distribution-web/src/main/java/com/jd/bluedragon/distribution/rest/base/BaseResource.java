b0db38067ff1e76ff93e221ac70bcc2ddef5bec4
liuduo8 <liuduo3@jd.com>
2019-12-19 16:17:20 +0800

    增加基础资料字典接口

--- bluedragon-distribution-web/src/main/java/com/jd/bluedragon/distribution/rest/base/BaseResource.java
+++ bluedragon-distribution-web/src/main/java/com/jd/bluedragon/distribution/rest/base/BaseResource.java
@@ -1606,6 +1606,28 @@ public class BaseResource {
 
 		return result;
 	}
+
+	@Path("/getBaseDataDictList/{parentId}/{nodeLevel}/{typeGroup}")
+	@GET
+	/**
+	 * 基础资料字典获取接口
+	 */
+	public InvokeResult<List<BaseDataDict>>  getBaseDataDictList(@PathParam("parentId")Integer parentId, @PathParam("nodeLevel")Integer nodeLevel,@PathParam("typeGroup")Integer typeGroup){
+        InvokeResult<List<BaseDataDict>> result = new InvokeResult<>();
+	    if(parentId == null || nodeLevel == null || typeGroup == null){
+			result.setCode(InvokeResult.RESULT_THIRD_ERROR_CODE);
+			result.setMessage(InvokeResult.PARAM_ERROR);
+			return result;
+		}
+		try{
+			result.setData(this.baseService.getBaseDataDictList(parentId, nodeLevel, typeGroup));
+		}catch (Exception e){
+	    	log.error("获取基础资料字典数据异常{}，{}，{}",parentId,nodeLevel,typeGroup,e);
+			result.setCode(InvokeResult.SERVER_ERROR_CODE);
+			result.setMessage(InvokeResult.SERVER_ERROR_MESSAGE);
+		}
+		return result;
+	}
 }
 
 
