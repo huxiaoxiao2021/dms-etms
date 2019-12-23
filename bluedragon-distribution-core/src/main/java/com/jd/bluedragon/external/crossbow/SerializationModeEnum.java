f4a03f27bafbb9af2ce737573c16bf7acb263ae5
wuzuxiang <wuzuxiang@jd.com>
2019-12-23 17:13:56 +0800

    武汉邮政改造接口添加完成

---
+++ bluedragon-distribution-core/src/main/java/com/jd/bluedragon/external/crossbow/SerializationModeEnum.java
@@ -0,0 +1,26 @@
+package com.jd.bluedragon.external.crossbow;
+
+/**
+ * <p>
+ *     定义序列化反序列化方式
+ *
+ * @author wuzuxiang
+ * @since 2019/12/18
+ **/
+public enum SerializationModeEnum {
+
+    /**
+     * XML序列化反序列化方式
+     */
+    XML,
+
+    /**
+     * JSON序列化反序列化方式
+     */
+    JSON,
+
+    /**
+     * SOAP的报文
+     */
+    SOAP;
+}
