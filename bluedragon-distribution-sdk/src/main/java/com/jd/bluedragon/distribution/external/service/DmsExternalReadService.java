783d3f4ef4d484c462ecee5ada2bdeb62be008df
hujiping1 <hujiping>
2020-01-02 13:34:06 +0800

    无用jsf剔除

--- bluedragon-distribution-sdk/src/main/java/com/jd/bluedragon/distribution/external/service/DmsExternalReadService.java
+++ bluedragon-distribution-sdk/src/main/java/com/jd/bluedragon/distribution/external/service/DmsExternalReadService.java
@@ -1,10 +1,8 @@
 package com.jd.bluedragon.distribution.external.service;
 
-import com.jd.bluedragon.distribution.api.request.PackageSummaryRequest;
 import com.jd.bluedragon.distribution.api.response.BoxResponse;
 import com.jd.bluedragon.distribution.api.response.DmsBaseResponse;
 import com.jd.bluedragon.distribution.api.response.SendBoxDetailResponse;
-import com.jd.bluedragon.distribution.api.response.WaybillInfoResponse;
 import com.jd.bluedragon.distribution.jsf.domain.InvokeResult;
 import com.jd.bluedragon.distribution.saf.WaybillSafResponse;
 import com.jd.bluedragon.distribution.send.domain.SendDSimple;
@@ -93,14 +91,6 @@ public interface DmsExternalReadService {
 	 * */
 	public List<DepartureWaybillDto> getWaybillsByDeparture(String code, Integer type);
 
-
-	//////////////////////EmsOrderJosSafService//////////////////////
-	/**
-	 * 根据运单号获取发货信息
-	 * @param waybillCode 运单号
-	 * @return
-	 * */
-	public WaybillInfoResponse getWaybillInfo(String waybillCode);
 
 	////////////////////////OrdersResourceSafService//////////////////////
 	/**
