a1077eebad5664786519abf5a8a11362feb2f241
liangjiaqi6 <liangjiaqi6@jd.com>
2020-01-16 15:48:21 +0800

    h2数据库测试

---
+++ bluedragon-distribution-core/src/test/resources/h2/dml.sql
@@ -0,0 +1,5 @@
+insert into pop_abnormal_order (id, serial_number, abnormal_type, waybill_code, order_code, pop_sup_no, rsv1,
+                                current_num, actual_num, confirm_num, operator_code, operator_name, create_site_code,
+                                create_site_name, create_time, confirm_time, update_time, abnormal_state, memo, yn)
+values (1217691424705740800, 'Mary', 1, 'Jax', 'Joe', 'Jim', 'Jone', 678, 557, 673, 737, 'Mary', 7, 'Joe', NOW(),
+        '2020-01-16 14:13:53.841', NOW(), 1, 'Stone', 1);
