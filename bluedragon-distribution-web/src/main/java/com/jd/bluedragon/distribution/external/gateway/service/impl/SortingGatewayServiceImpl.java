fe449d97df6586ee2f2149639716aab8d5cff997
biyubo <biyubo@jd.com>
2020-03-06 17:10:35 +0800

    分拣拦截改为WEB

--- bluedragon-distribution-web/src/main/java/com/jd/bluedragon/distribution/external/gateway/service/impl/SortingGatewayServiceImpl.java
+++ bluedragon-distribution-web/src/main/java/com/jd/bluedragon/distribution/external/gateway/service/impl/SortingGatewayServiceImpl.java
@@ -1,10 +1,16 @@
 package com.jd.bluedragon.distribution.external.gateway.service.impl;
 
+import com.alibaba.fastjson.JSON;
 import com.jd.bluedragon.Constants;
 import com.jd.bluedragon.common.dto.base.response.JdCResponse;
+import com.jd.bluedragon.common.dto.base.response.JdVerifyResponse;
+import com.jd.bluedragon.common.dto.base.response.MsgBoxTypeEnum;
 import com.jd.bluedragon.common.dto.sorting.request.SortingCancelRequest;
+import com.jd.bluedragon.common.dto.sorting.request.SortingCheckRequest;
 import com.jd.bluedragon.distribution.api.request.SortingRequest;
 import com.jd.bluedragon.distribution.api.response.SortingResponse;
+import com.jd.bluedragon.distribution.client.domain.PdaOperateRequest;
+import com.jd.bluedragon.distribution.jsf.domain.SortingJsfResponse;
 import com.jd.bluedragon.distribution.rest.sorting.SortingResource;
 import com.jd.bluedragon.dms.utils.BusinessUtil;
 import com.jd.bluedragon.external.gateway.service.SortingGatewayService;
@@ -64,5 +70,60 @@ public class SortingGatewayServiceImpl implements SortingGatewayService {
         }
         response.toFail(sortingResponse.getMessage());
         return response;
+    }
+
+    @Override
+    @BusinessLog(sourceSys = 1,bizType = 2002,operateType = 20021)
+    @JProfiler(jKey = "DMSWEB.SortingGatewayServiceImpl.sortingPostCheck", mState = {JProEnum.TP, JProEnum.FunctionError}, jAppName = Constants.UMP_APP_NAME_DMSWEB)
+    public JdVerifyResponse sortingPostCheck(SortingCheckRequest checkRequest) {
+        PdaOperateRequest pdaOperateRequest = new PdaOperateRequest();
+        pdaOperateRequest.setBoxCode(checkRequest.getBoxCode());
+        pdaOperateRequest.setBusinessType(checkRequest.getBusinessType());
+        pdaOperateRequest.setIsLoss(checkRequest.getIsLoss());
+        if(checkRequest.getIsGather == null){
+            pdaOperateRequest.setIsGather(0);
+        }else{
+            pdaOperateRequest.setIsGather(checkRequest.getIsGather);
+        }
+        pdaOperateRequest.setOperateType(checkRequest.getOperateType());
+        pdaOperateRequest.setPackageCode(checkRequest.getPackageCode());
+        pdaOperateRequest.setReceiveSiteCode(checkRequest.getReceiveSiteCode());
+        pdaOperateRequest.setCreateSiteCode(checkRequest.getCurrentOperate().getSiteCode());
+        pdaOperateRequest.setCreateSiteName(checkRequest.getCurrentOperate().getSiteName());
+        pdaOperateRequest.setOperateTime(DateUtil.format(checkRequest.getCurrentOperate().getOperateTime(),DateUtil.FORMAT_DATE_TIME));
+        pdaOperateRequest.setOperateUserCode(checkRequest.getUser().getUserCode());
+        pdaOperateRequest.setOperateUserName(checkRequest.getUser().getUserName());
+
+        JdVerifyResponse jdVerifyResponse = new JdVerifyResponse();
+        try {
+            SortingJsfResponse sortingResponse = sortingResource.check(pdaOperateRequest);
+            JdVerifyResponse.MsgBox msgBox = null;
+            //校验通过
+            if(org.jboss.util.Objects.equals(sortingResponse.getCode(),SortingResponse.CODE_OK)){
+                jdVerifyResponse.toSuccess(sortingResponse.getMessage());
+                return jdVerifyResponse;
+            }
+            //已知可以认为是接口异常的 code
+            if(org.jboss.util.Objects.equals(sortingResponse.getCode(),SortingResponse.CODE_SERVICE_ERROR)
+                    || org.jboss.util.Objects.equals(sortingResponse.getCode(),SortingResponse.CODE_PARAM_ERROR)){
+                jdVerifyResponse.toError(sortingResponse.getMessage());
+                return jdVerifyResponse;
+            }
+            //业务类提示
+            if(sortingResponse.getCode() >= 30000 && sortingResponse.getCode() <= 40000){
+                jdVerifyResponse.toSuccess();
+                msgBox = new JdVerifyResponse.MsgBox(MsgBoxTypeEnum.CONFIRM,sortingResponse.getCode(),sortingResponse.getMessage());
+                //暂未做： 分拣理货  CODE_39123 pda端有特殊提示 确认是否可以不做
+                jdVerifyResponse.addBox(msgBox);
+                return jdVerifyResponse;
+            }
+            //业务类 强制拦截
+            msgBox = new JdVerifyResponse.MsgBox(MsgBoxTypeEnum.INTERCEPT,sortingResponse.getCode(),sortingResponse.getMessage());
+            jdVerifyResponse.toSuccess();
+            jdVerifyResponse.addBox(msgBox);
+        } catch (Exception e) {
+            jdVerifyResponse.toError(e.getMessage());
+        }
+        return jdVerifyResponse;
     }
 }
