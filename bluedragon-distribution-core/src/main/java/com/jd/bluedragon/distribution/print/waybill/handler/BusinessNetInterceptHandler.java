9b6684b1c945498e22e005adffea2e493838e465
hujiping1 <hujiping>
2020-04-10 12:42:42 +0800

    经济网-去除10*5面单、remark备注（添加注释）

--- bluedragon-distribution-core/src/main/java/com/jd/bluedragon/distribution/print/waybill/handler/BusinessNetInterceptHandler.java
+++ bluedragon-distribution-core/src/main/java/com/jd/bluedragon/distribution/print/waybill/handler/BusinessNetInterceptHandler.java
@@ -1,7 +1,6 @@
 package com.jd.bluedragon.distribution.print.waybill.handler;
 
 import com.jd.bluedragon.common.domain.Waybill;
-import com.jd.bluedragon.distribution.api.request.WaybillPrintRequest;
 import com.jd.bluedragon.distribution.handler.InterceptHandler;
 import com.jd.bluedragon.distribution.handler.InterceptResult;
 import com.jd.bluedragon.distribution.print.domain.BasePrintWaybill;
@@ -12,6 +11,7 @@ import org.springframework.stereotype.Service;
  * 经济网处理器
  *      经济网不显示任何京东标识
  *      经济网不支持10x5面单，只使用10x10面单（dms-unite1010-m）
+ *      (二期：去除10*5面单限制、去除remark备注)
  * @author: hujiping
  * @date: 2020/1/13 14:42
  */
@@ -21,7 +21,6 @@ public class BusinessNetInterceptHandler implements InterceptHandler<WaybillPrin
     @Override
     public InterceptResult<String> handle(WaybillPrintContext context) {
         InterceptResult<String> result = context.getResult();
-        WaybillPrintRequest request = context.getRequest();
         BasePrintWaybill basePrintWaybill = context.getBasePrintWaybill();
         Waybill waybill = context.getWaybill();
         String waybillSign = waybill==null?null:waybill.getWaybillSign();
