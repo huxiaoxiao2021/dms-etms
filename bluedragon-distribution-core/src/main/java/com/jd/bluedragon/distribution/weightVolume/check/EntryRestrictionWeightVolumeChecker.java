ef5ff276f665ac92a18d0650ec8a100f9d50b852
hujiping1 <hujiping>
2020-04-22 14:43:28 +0800

    平台打印增加众邮箱号称重

--- bluedragon-distribution-core/src/main/java/com/jd/bluedragon/distribution/weightVolume/check/EntryRestrictionWeightVolumeChecker.java
+++ bluedragon-distribution-core/src/main/java/com/jd/bluedragon/distribution/weightVolume/check/EntryRestrictionWeightVolumeChecker.java
@@ -16,6 +16,7 @@ import org.springframework.stereotype.Service;
  *     1. 按箱称重量方只能在新的入口中操作对应的sourceCode为：DMS_CLIENT_WEIGHT_VOLUME
  *     2. B网称重页面只能支持按运单操作
  *     3. 交接称重（目前有加盟商交接时间截止到2020-1-15）只能在站点平台打印和平台打印中操作
+ *     4. 平台打印新增按箱称重量方
  *
  * @author wuzuxiang
  * @since 2020/1/15
@@ -33,7 +34,9 @@ public class EntryRestrictionWeightVolumeChecker implements IWeightVolumeChecker
     public InvokeResult<Boolean> checkOperateWeightVolume(WeightVolumeEntity entity) {
         InvokeResult<Boolean> result = new InvokeResult<>();
         /* 按箱称重的目前只能在经济网项目新开的那个客户端页面中称重 */
-        if (BusinessUtil.isBoxcode(entity.getBarCode()) && !FromSourceEnum.DMS_CLIENT_WEIGHT_VOLUME.equals(entity.getSourceCode())){
+        if (BusinessUtil.isBoxcode(entity.getBarCode())
+                && !FromSourceEnum.DMS_CLIENT_WEIGHT_VOLUME.equals(entity.getSourceCode())
+                && !FromSourceEnum.DMS_CLIENT_PLATE_PRINT.equals(entity.getSourceCode())){
             result.error("当前页面不允许操作按箱称重");
             result.setData(Boolean.FALSE);
             return result;
---
+++ bluedragon-distribution-web/src/test/java/com/jd/bluedragon/distribution/external/gateway/box/BoxGateWayExternalServiceImplTest.java
@@ -0,0 +1,52 @@
+package com.jd.bluedragon.distribution.external.gateway.box;
+
+import com.jd.bluedragon.Constants;
+import com.jd.bluedragon.external.gateway.base.GateWayBaseResponse;
+import com.jd.bluedragon.external.gateway.box.BoxGateWayExternalService;
+import com.jd.bluedragon.external.gateway.dto.request.BoxGenerateRequest;
+import com.jd.bluedragon.external.gateway.dto.response.BoxDto;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * 类描述信息
+ *
+ * @author: hujiping
+ * @date: 2020/4/20 18:59
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "classpath*:/spring/distribution-core-context.xml" })
+public class BoxGateWayExternalServiceImplTest {
+
+    @Autowired
+    private BoxGateWayExternalService boxGateWayExternalService;
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void generateBoxCodes() {
+        try {
+            BoxGenerateRequest request = new BoxGenerateRequest();
+            request.setBoxType("BC");
+            request.setStartSiteCode("910");
+            request.setEndSiteCode("39");
+            request.setNum(3);
+            request.setOperatorId(910);
+            request.setOperatorName("刑松");
+            request.setOperatorUnitName("北京通州分拣中心");
+            request.setTenantCode(Constants.TENANT_CODE_ECONOMIC);
+            String pin = "";
+            GateWayBaseResponse<BoxDto> result = boxGateWayExternalService.generateBoxCodes(request, pin);
+            String boxCode = result.getData().getBoxCodes().get(0);
+        }catch (Exception e){
+            Assert.assertTrue(Boolean.FALSE);
+        }
+    }
+}
