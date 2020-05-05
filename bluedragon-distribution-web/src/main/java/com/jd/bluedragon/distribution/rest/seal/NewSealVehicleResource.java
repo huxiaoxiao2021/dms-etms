04cbad87955f6f72f3a670255424cb48efce8599
hujiping1 <hujiping>
2020-05-05 16:30:08 +0800

    封车体积校验

--- bluedragon-distribution-web/src/main/java/com/jd/bluedragon/distribution/rest/seal/NewSealVehicleResource.java
+++ bluedragon-distribution-web/src/main/java/com/jd/bluedragon/distribution/rest/seal/NewSealVehicleResource.java
@@ -1,6 +1,7 @@
 package com.jd.bluedragon.distribution.rest.seal;
 
 import com.jd.bluedragon.Constants;
+import com.jd.bluedragon.common.dto.base.response.JdCResponse;
 import com.jd.bluedragon.common.dto.blockcar.enumeration.FerrySealCarSceneEnum;
 import com.jd.bluedragon.common.dto.blockcar.enumeration.SealCarSourceEnum;
 import com.jd.bluedragon.common.dto.blockcar.request.SealCarPreRequest;
@@ -540,7 +541,7 @@ public class NewSealVehicleResource {
         SealVehicleVolumeVerifyResponse response = new SealVehicleVolumeVerifyResponse(JdResponse.CODE_SERVICE_ERROR, JdResponse.MESSAGE_SERVICE_ERROR);
 
         if (request == null) {
-            log.warn("NewSealVehicleResource seal --> 传入参数非法");
+            log.warn("NewSealVehicleResource verifySendVolume --> 传入参数非法");
             response.setCode(JdResponse.CODE_PARAM_ERROR);
             response.setMessage(JdResponse.MESSAGE_PARAM_ERROR);
             return response;
@@ -564,13 +565,24 @@ public class NewSealVehicleResource {
             sealCarDto.setWeight(request.getWeight());
 
             CommonDto<String> dto = newsealVehicleService.verifySealVehicleVolume(sealCarDto);
-            response.setCode(dto.getCode());
-            response.setMessage(dto.getMessage());
-            response.setData(dto.getData());
-
+            if(dto == null){
+                response.setCode(JdCResponse.CODE_ERROR);
+                response.setMessage(JdCResponse.MESSAGE_ERROR);
+                return response;
+            }
+            if(dto.getCode() == CommonDto.CODE_SUCCESS){
+                response.setCode(JdCResponse.CODE_SUCCESS);
+                response.setMessage(JdCResponse.MESSAGE_SUCCESS);
+            }else if(dto.getCode() == CommonDto.CODE_FAIL){
+                response.setCode(JdCResponse.CODE_CONFIRM);
+                response.setMessage(dto.getMessage());
+            }else {
+                response.setCode(JdCResponse.CODE_FAIL);
+                response.setMessage(dto.getMessage());
+            }
         }catch(Exception e){
-            response.setCode(JdResponse.CODE_SERVICE_ERROR);
-            response.setMessage(JdResponse.MESSAGE_SERVICE_ERROR);
+            response.setCode(JdCResponse.CODE_ERROR);
+            response.setMessage(JdCResponse.MESSAGE_ERROR);
             this.log.error("校验批次的体积异常，批次号:{}",request.getBatchCodes().toString(), e);
         }
         return response;
