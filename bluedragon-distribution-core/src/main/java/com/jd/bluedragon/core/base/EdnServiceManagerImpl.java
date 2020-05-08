0259a9969beab3e2c38951313651afe9201a6dc0
wuyoude <wuyoude@jd.com>
2020-05-08 18:14:39 +0800

    企配仓需求，页面展示优化

--- bluedragon-distribution-core/src/main/java/com/jd/bluedragon/core/base/EdnServiceManagerImpl.java
+++ bluedragon-distribution-core/src/main/java/com/jd/bluedragon/core/base/EdnServiceManagerImpl.java
@@ -8,7 +8,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.jd.bluedragon.Constants;
+import com.jd.bluedragon.common.utils.ProfilerHelper;
 import com.jd.bluedragon.distribution.command.JdResult;
 import com.jd.bluedragon.distribution.schedule.vo.DmsEdnBatchVo;
 import com.jd.bluedragon.utils.JsonHelper;
@@ -16,8 +16,8 @@ import com.jd.ka.edn.sdk.common.SdkRpcResult;
 import com.jd.ka.edn.sdk.dto.DeliveryReceiptResDto;
 import com.jd.ka.edn.sdk.enums.ReturnCodeEnum;
 import com.jd.ka.edn.sdk.service.JpEdnDeliveryReceiptService;
-import com.jd.ump.annotation.JProEnum;
-import com.jd.ump.annotation.JProfiler;
+import com.jd.ump.profiler.CallerInfo;
+import com.jd.ump.profiler.proxy.Profiler;
 
 /**
  * 金鹏相关服务管理类
@@ -26,8 +26,8 @@ import com.jd.ump.annotation.JProfiler;
  */
 @Service("ednServiceManager")
 public class EdnServiceManagerImpl implements EdnServiceManager{
-	private static final Logger log = LoggerFactory.getLogger(MrdFeedbackManagerImpl.class);
-	
+	private static final Logger log = LoggerFactory.getLogger(EdnServiceManagerImpl.class);
+	private static final String UMP_KEY_batchGetDeliveryReceipt = "dmsWeb.rpc.client.edn.jpEdnDeliveryReceiptService.batchGetDeliveryReceipt";
 	@Autowired
 	JpEdnDeliveryReceiptService jpEdnDeliveryReceiptService;
 	/**
@@ -35,32 +35,35 @@ public class EdnServiceManagerImpl implements EdnServiceManager{
 	 * @param ednBatchNumList
 	 * @return
 	 */
-    @JProfiler(jKey = "dmsWeb.rpc.client.edn.jpEdnDeliveryReceiptService.batchGetDeliveryReceipt",jAppName=Constants.UMP_APP_NAME_DMSWEB,
-    		mState = {JProEnum.TP, JProEnum.FunctionError})
 	public JdResult<List<DmsEdnBatchVo>> batchGetDeliveryReceipt(List<String> ednBatchNumList){
+    	CallerInfo callerInfo = ProfilerHelper.registerInfo(UMP_KEY_batchGetDeliveryReceipt);
 		JdResult<List<DmsEdnBatchVo>> result = new JdResult<List<DmsEdnBatchVo>>();
-		SdkRpcResult<List<DeliveryReceiptResDto>> rpcResult = jpEdnDeliveryReceiptService.batchGetDeliveryReceipt(ednBatchNumList);
-		if(log.isDebugEnabled()){
-			log.debug("调用金鹏接口失败！return:{}",JsonHelper.toJson(rpcResult));
-		}
-		if(rpcResult != null 
-				&& rpcResult.isSuccess() 
-				&& ReturnCodeEnum.SUCCESS.getCode().equals(rpcResult.getResultCode())){
-			if(rpcResult.getResult() != null && !rpcResult.getResult().isEmpty()){
-				List<DmsEdnBatchVo> dataList = new ArrayList<DmsEdnBatchVo>();
-				result.setData(dataList);
-				for(DeliveryReceiptResDto rpcObj:rpcResult.getResult()){
-					DmsEdnBatchVo dataItem = new DmsEdnBatchVo();
-					dataItem.setEdnBatchNum(rpcObj.getEdnBatchNum());
-					dataItem.setDeliveryReceiptUrl(rpcObj.getDeliveryReceiptUrl());
-					dataList.add(dataItem);
+		try {
+			SdkRpcResult<List<DeliveryReceiptResDto>> rpcResult = jpEdnDeliveryReceiptService.batchGetDeliveryReceipt(ednBatchNumList);
+			if(rpcResult != null
+					&& rpcResult.isSuccess() 
+					&& ReturnCodeEnum.SUCCESS.getCode().equals(rpcResult.getResultCode())){
+				if(rpcResult.getResult() != null && !rpcResult.getResult().isEmpty()){
+					List<DmsEdnBatchVo> dataList = new ArrayList<DmsEdnBatchVo>();
+					result.setData(dataList);
+					result.toSuccess();
+					for(DeliveryReceiptResDto rpcObj:rpcResult.getResult()){
+						DmsEdnBatchVo dataItem = new DmsEdnBatchVo();
+						dataItem.setEdnBatchNum(rpcObj.getEdnBatchNum());
+						dataItem.setDeliveryReceiptUrl(rpcObj.getDeliveryReceiptUrl());
+						dataList.add(dataItem);
+					}
 				}
+			}else{
+				log.warn("调用金鹏接口批量获取配送单失败！return:{}",JsonHelper.toJson(rpcResult));
+				result.toFail("调用金鹏接口批量获取配送单失败！");
 			}
-		}else if(rpcResult != null){
-			log.warn("调用金鹏接口失败！return:{}",JsonHelper.toJson(rpcResult));
-			result.toFail(rpcResult.getResultCode()+ rpcResult.getResultMessage());
-		}else{
-			result.toFail("调用金鹏接口失败！");
+		} catch (Exception e) {
+			log.error("调用金鹏接口批量获取配送单异常！",e);
+			result.toError("调用金鹏接口批量获取配送单异常！");
+			Profiler.functionError(callerInfo);
+		}finally{
+			Profiler.registerInfoEnd(callerInfo);
 		}
 		return result;
 	}
