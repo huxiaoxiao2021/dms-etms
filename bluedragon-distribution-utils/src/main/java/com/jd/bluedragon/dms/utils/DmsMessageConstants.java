49f2b9b71d070b6f93b185688c5e82404616c634
wuyoude <wuyoude@jd.com>
2020-05-15 10:07:11 +0800

    预售提示语修改

--- bluedragon-distribution-utils/src/main/java/com/jd/bluedragon/dms/utils/DmsMessageConstants.java
+++ bluedragon-distribution-utils/src/main/java/com/jd/bluedragon/dms/utils/DmsMessageConstants.java
@@ -11,5 +11,5 @@ public interface DmsMessageConstants {
 	 * 预售拦截消息码
 	 */
     public static final Integer CODE_29419 = 29419;
-    public static final String MESSAGE_29419 = "此单未付尾款，异常处理选24预售原因，换单回仓”";
+    public static final String MESSAGE_29419 = "此单未付尾款，异常处理选24预售原因，换单回仓！";
 }
