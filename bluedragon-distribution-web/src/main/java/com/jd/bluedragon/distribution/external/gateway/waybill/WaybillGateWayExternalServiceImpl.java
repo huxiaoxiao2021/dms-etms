0fc8f01cc3e78fe080931befc7813a2c721c999c
xumigen <xumigen@jd.com>
2020-06-23 12:25:00 +0800

    三方装箱  给经济快递包装jsf

--- bluedragon-distribution-web/src/main/java/com/jd/bluedragon/distribution/external/gateway/waybill/WaybillGateWayExternalServiceImpl.java
+++ bluedragon-distribution-web/src/main/java/com/jd/bluedragon/distribution/external/gateway/waybill/WaybillGateWayExternalServiceImpl.java
@@ -24,6 +24,7 @@ import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.Date;
 
@@ -32,6 +33,7 @@ import java.util.Date;
  * @author : xumigen
  * @date : 2020/1/2
  */
+@Service("waybillGateWayExternalService")
 public class WaybillGateWayExternalServiceImpl implements WaybillGateWayExternalService {
     private final Logger logger = LoggerFactory.getLogger(WaybillGateWayExternalServiceImpl.class);
 
