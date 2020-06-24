67b6704508fb64f7de74b4a34b5297a395d114f9
hujiping1 <hujiping>
2020-06-24 09:43:58 +0800

    装卸车接口实现

---
+++ bluedragon-distribution-core/src/main/java/com/jd/bluedragon/distribution/loadAndUnload/service/UnloadCarService.java
@@ -0,0 +1,13 @@
+package com.jd.bluedragon.distribution.loadAndUnload.service;
+
+/**
+ * 卸车任务实现
+ *
+ * @author: hujiping
+ * @date: 2020/6/23 20:05
+ */
+public interface UnloadCarService {
+
+
+
+}
---
+++ bluedragon-distribution-core/src/main/java/com/jd/bluedragon/distribution/loadAndUnload/service/impl/UnloadCarServiceImpl.java
@@ -0,0 +1,19 @@
+package com.jd.bluedragon.distribution.loadAndUnload.service.impl;
+
+import com.jd.bluedragon.distribution.loadAndUnload.service.UnloadCarService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+/**
+ * 卸车任务实现
+ *
+ * @author: hujiping
+ * @date: 2020/6/23 20:06
+ */
+@Service("unloadCarService")
+public class UnloadCarServiceImpl implements UnloadCarService {
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+}
