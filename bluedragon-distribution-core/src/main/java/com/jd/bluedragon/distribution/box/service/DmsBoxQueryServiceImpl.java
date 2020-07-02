3705fce68a4958f24911f7ccd544fa69161ab7c9
xumigen <xumigen@jd.com>
2020-07-02 22:11:53 +0800

    经济网箱号 接口

--- bluedragon-distribution-core/src/main/java/com/jd/bluedragon/distribution/box/service/DmsBoxQueryServiceImpl.java
+++ bluedragon-distribution-core/src/main/java/com/jd/bluedragon/distribution/box/service/DmsBoxQueryServiceImpl.java
@@ -7,12 +7,17 @@ import com.jd.bluedragon.distribution.box.domain.Box;
 import com.jd.ql.basic.dto.BaseStaffSiteOrgDto;
 import com.jd.ump.annotation.JProEnum;
 import com.jd.ump.annotation.JProfiler;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Objects;
 
 /**
  * @author : xumigen
  * @date : 2020/7/2
  */
+@Service("dmsBoxQueryService")
 public class DmsBoxQueryServiceImpl implements DmsBoxQueryService{
 
     @Autowired
@@ -25,6 +30,10 @@ public class DmsBoxQueryServiceImpl implements DmsBoxQueryService{
     public Response<Boolean> isEconomicNetBox(String boxCode) {
         Response<Boolean> response = new Response<>();
         response.toSucceed();
+        if(StringUtils.isEmpty(boxCode)){
+            response.toWarn("箱号不能为空");
+            return response;
+        }
         Box box = boxService.findBoxByCode(boxCode);
         if(box == null){
             response.toError("箱号不存在");
@@ -37,7 +46,7 @@ public class DmsBoxQueryServiceImpl implements DmsBoxQueryService{
             return response;
         }
         //校验始发为经济网站点
-        if(!Constants.THIRD_ENET_SITE_TYPE.equals(startSite.getSiteType())){
+        if(Objects.equals(Constants.THIRD_ENET_SITE_TYPE,startSite.getSiteType())){
             response.setData(Boolean.TRUE);
             return response;
         }
