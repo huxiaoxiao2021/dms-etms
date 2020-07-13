a021202da9272b96d1221e0c69331c59aff4929c
fanggang7 <fanggang7@jd.com>
2020-07-13 16:01:02 +0800

    Revert "Merge branch 'feature/20200702_notice' into 'master'"
    This reverts merge request !176

--- bluedragon-distribution-core/src/main/java/com/jd/bluedragon/distribution/notice/dao/NoticeDao.java
+++ bluedragon-distribution-core/src/main/java/com/jd/bluedragon/distribution/notice/dao/NoticeDao.java
@@ -2,7 +2,6 @@ package com.jd.bluedragon.distribution.notice.dao;
 
 import com.jd.bluedragon.common.dao.BaseDao;
 import com.jd.bluedragon.distribution.notice.domain.Notice;
-import com.jd.bluedragon.distribution.notice.request.NoticeQuery;
 
 import java.util.List;
 import java.util.Map;
@@ -27,39 +26,6 @@ public class NoticeDao extends BaseDao<Notice> {
     }
 
     /**
-     * 按条件查询总数
-     * @param noticeQuery 查询参数
-     * @return 数据列表
-     * @author fanggang7
-     * @date 2020-07-02 16:27:55 周四
-     */
-    public Long queryCount(NoticeQuery noticeQuery) {
-        return this.getSqlSession().selectOne(NoticeDao.NAMESPACE + ".queryCount", noticeQuery);
-    }
-
-    /**
-     * 按条件查询列表
-     * @param noticeQuery 查询参数
-     * @return 数据列表
-     * @author fanggang7
-     * @date 2020-07-02 16:27:55 周四
-     */
-    public List<Notice> queryList(NoticeQuery noticeQuery) {
-        return this.getSqlSession().selectList(NoticeDao.NAMESPACE + ".queryList", noticeQuery);
-    }
-
-    /**
-     * 按主键查询
-     * @param id 主键ID
-     * @return 数据列表
-     * @author fanggang7
-     * @date 2020-07-02 16:27:55 周四
-     */
-    public Notice getByPrimaryKey(Long id) {
-        return this.getSqlSession().selectOne(NoticeDao.NAMESPACE + ".getByPrimaryKey", id);
-    }
-
-    /**
      * 新增
      *
      * @param notice
@@ -77,28 +43,6 @@ public class NoticeDao extends BaseDao<Notice> {
      */
     public int logicDelete(Map<String, Object> parameter) {
         return this.getSqlSession().update(NoticeDao.NAMESPACE + ".logicDelete", parameter);
-    }
-
-    /**
-     * 按主键更新
-     * @param notice 参数
-     * @return 数据列表
-     * @author fanggang7
-     * @date 2020-07-02 16:27:55 周四
-     */
-    public int updateByPrimaryKey(Notice notice) {
-        return this.getSqlSession().update(NoticeDao.NAMESPACE + ".updateByPrimaryKey", notice);
-    }
-
-    /**
-     * 按主键删除
-     * @param notice 参数
-     * @return 数据列表
-     * @author fanggang7
-     * @date 2020-07-02 16:27:55 周四
-     */
-    public int deleteByPrimaryKey(Notice notice) {
-        return this.getSqlSession().update(NoticeDao.NAMESPACE + ".deleteByPrimaryKey", notice);
     }
 
 }
--- bluedragon-distribution-core/src/main/java/com/jd/bluedragon/distribution/notice/service/NoticeService.java
+++ bluedragon-distribution-core/src/main/java/com/jd/bluedragon/distribution/notice/service/NoticeService.java
@@ -1,10 +1,7 @@
 package com.jd.bluedragon.distribution.notice.service;
 
-import com.jd.bluedragon.distribution.api.Response;
 import com.jd.bluedragon.distribution.api.request.NoticeRequest;
 import com.jd.bluedragon.distribution.notice.domain.Notice;
-import com.jd.bluedragon.distribution.notice.request.NoticeQuery;
-import com.jd.ql.dms.common.web.mvc.api.PageDto;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
@@ -17,15 +14,6 @@ import java.util.List;
  * @date 2019/4/16
  */
 public interface NoticeService {
-
-    /**
-     * 分页查询通知数据
-     * @param query 查询参数
-     * @return 分页数据结果
-     * @author fanggang7
-     * @date 2020-07-02 16:41:33 周四
-     */
-    PageDto<Notice> queryPageList(NoticeQuery query);
 
     /**
      * 查询所有的通知信息
@@ -51,40 +39,11 @@ public interface NoticeService {
     void addAndUploadFile(List<MultipartFile> files, NoticeRequest request, String userErp) throws IOException;
 
     /**
-     * 按主键查询
-     * @param id 主键ID
-     * @return Notice 通知详情
-     * @author fanggang7
-     * @date 2020-07-07 19:52:00 周二
-     */
-    Response<Notice> getByPrimaryKey(Long id);
-
-    /**
      * 根据id列表删除
      *
      * @param ids
      * @param userErp
      */
     Integer deleteByIds(List<Long> ids, String userErp);
-
-    /**
-     * 按主键更新
-     *
-     * @param notice Notice
-     * @return Response 更新条数
-     * @author fanggang7
-     * @date 2020-07-01 20:00:46 周三
-     */
-    Response<Boolean> updateByPrimaryKey(Notice notice);
-
-    /**
-     * 按主键伪删除
-     *
-     * @param notice Notice
-     * @return Response 更新条数
-     * @author fanggang7
-     * @date 2020-07-01 20:00:46 周三
-     */
-    Response<Boolean> deleteByPrimaryKey(Notice notice);
 
 }
