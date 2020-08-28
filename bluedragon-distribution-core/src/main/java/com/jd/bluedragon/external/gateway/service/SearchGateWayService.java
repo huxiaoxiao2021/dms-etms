dbe2553fbba3d3da8ad48a79ca5a077acc2ce2b3
biyubo <biyubo@jd.com>
2020-08-28 17:53:07 +0800

    查询-增加接口

--- bluedragon-distribution-core/src/main/java/com/jd/bluedragon/external/gateway/service/SearchGateWayService.java
+++ bluedragon-distribution-core/src/main/java/com/jd/bluedragon/external/gateway/service/SearchGateWayService.java
@@ -2,7 +2,10 @@ package com.jd.bluedragon.external.gateway.service;
 
 import com.jd.bluedragon.common.dto.base.response.JdCResponse;
 import com.jd.bluedragon.common.dto.search.request.PackBoxRequest;
+import com.jd.bluedragon.common.dto.search.response.BoxInfoResponse;
 import com.jd.bluedragon.common.dto.search.response.PackBoxResponse;
+import com.jd.bluedragon.common.dto.search.response.PackageDifferentialResponse;
+import com.jd.bluedragon.common.dto.send.request.DeliveryRequest;
 
 import java.util.List;
 
@@ -17,5 +20,19 @@ public interface SearchGateWayService {
      * @return
      */
     JdCResponse<List<PackBoxResponse>> getBoxPackList(PackBoxRequest request);
+
+    /**
+     * 根据箱号，获取箱内包裹数
+     * @param request
+     * @return
+     */
+    JdCResponse<BoxInfoResponse> getPackSortByBoxCode(PackBoxRequest request);
+
+    /**
+     *包裹不全查询
+     * @param request
+     * @return
+     */
+    JdCResponse<List<PackageDifferentialResponse>> getPackageDifferential(DeliveryRequest request);
 
 }
--- bluedragon-distribution-web/src/main/java/com/jd/bluedragon/distribution/external/gateway/service/impl/SearchGateWayServiceImpl.java
+++ bluedragon-distribution-web/src/main/java/com/jd/bluedragon/distribution/external/gateway/service/impl/SearchGateWayServiceImpl.java
@@ -3,10 +3,17 @@ package com.jd.bluedragon.distribution.external.gateway.service.impl;
 import com.jd.bluedragon.Constants;
 import com.jd.bluedragon.common.dto.base.response.JdCResponse;
 import com.jd.bluedragon.common.dto.search.request.PackBoxRequest;
+import com.jd.bluedragon.common.dto.search.response.BoxInfoResponse;
 import com.jd.bluedragon.common.dto.search.response.PackBoxResponse;
+import com.jd.bluedragon.common.dto.search.response.PackageDifferentialResponse;
+import com.jd.bluedragon.common.dto.send.request.DeliveryRequest;
 import com.jd.bluedragon.distribution.api.JdResponse;
 import com.jd.bluedragon.distribution.api.response.BoxPackResponse;
+import com.jd.bluedragon.distribution.api.response.DeliveryResponse;
 import com.jd.bluedragon.distribution.rest.box.BoxPackResource;
+import com.jd.bluedragon.distribution.rest.send.DeliveryResource;
+import com.jd.bluedragon.distribution.send.domain.SendThreeDetail;
+import com.jd.bluedragon.distribution.send.domain.ThreeDeliveryResponse;
 import com.jd.bluedragon.external.gateway.service.SearchGateWayService;
 import com.jd.bluedragon.utils.JsonHelper;
 import com.jd.ump.annotation.JProEnum;
@@ -16,6 +23,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -27,6 +35,9 @@ public class SearchGateWayServiceImpl implements SearchGateWayService {
 
     @Autowired
     BoxPackResource boxPackResource;
+
+    @Autowired
+    DeliveryResource deliveryResource;
 
     /**
      * 根据运单号或包裹号查询箱号包裹信息
@@ -66,6 +77,111 @@ public class SearchGateWayServiceImpl implements SearchGateWayService {
             res.setData(JsonHelper.jsonToList(datastr,PackBoxResponse.class));
         }else {
             res.toFail(response.getMessage());
+        }
+
+        return res;
+    }
+
+    /**
+     * 根据箱号，获取箱内包裹数
+     * @param request
+     * @return
+     */
+    @Override
+    @JProfiler(jKey = "DMSWEB.SearchGateWayServiceImpl.getPackSortByBoxCode",jAppName = Constants.UMP_APP_NAME_DMSWEB, mState = {JProEnum.TP, JProEnum.FunctionError})
+    public JdCResponse<BoxInfoResponse> getPackSortByBoxCode(PackBoxRequest request){
+        JdCResponse<BoxInfoResponse> res=new JdCResponse<>();
+        res.toSucceed();
+
+        if(null==request){
+            res.toFail("入参为空");
+            return res;
+        }
+
+        if(null==request.getCreateSiteCode()){
+            res.toFail("操作站点ID为空");
+            return res;
+        }
+
+        if(StringUtils.isBlank(request.getWaybillNoOrPackNo())){
+            res.toFail("箱号为空");
+            return res;
+        }
+
+        BoxPackResponse response=boxPackResource.getBoxPack(request.getCreateSiteCode(),request.getWaybillNoOrPackNo());
+        if(JdResponse.CODE_OK.equals(response.getCode())){
+            res.toSucceed(response.getMessage());
+            BoxInfoResponse data=new BoxInfoResponse();
+            data.setTotalPack(response.getTotalPack());
+            data.setReceiveSiteCode(response.getReceiveSiteCode());
+            data.setReceiveSiteName(response.getReceiveSiteName());
+            res.setData(data);
+        }else {
+            res.toFail(response.getMessage());
+        }
+
+        return res;
+    }
+
+    @Override
+    @JProfiler(jKey = "DMSWEB.SearchGateWayServiceImpl.getPackageDifferential",jAppName = Constants.UMP_APP_NAME_DMSWEB, mState = {JProEnum.TP, JProEnum.FunctionError})
+    public JdCResponse<List<PackageDifferentialResponse>> getPackageDifferential(DeliveryRequest request){
+        JdCResponse<List<PackageDifferentialResponse>> res=new JdCResponse<>();
+        res.toSucceed();
+
+        if(null==request){
+            res.toFail("入参为空");
+            return res;
+        }
+
+        if(StringUtils.isBlank(request.getBoxCode())){
+            res.toFail("箱号为空");
+            return res;
+        }
+
+        if(null==request.getCurrentOperate() || request.getCurrentOperate().getSiteCode()<=0){
+            res.toFail("操作站点ID为空");
+            return res;
+        }
+
+        if(null==request.getReceiveSiteCode()){
+            res.toFail("目的地站点ID为空");
+            return res;
+        }
+
+        com.jd.bluedragon.distribution.api.request.DeliveryRequest sourceRequest=new com.jd.bluedragon.distribution.api.request.DeliveryRequest();
+        sourceRequest.setBoxCode(request.getBoxCode());
+        sourceRequest.setSiteCode(request.getCurrentOperate().getSiteCode());
+        sourceRequest.setReceiveSiteCode(request.getReceiveSiteCode());
+
+        ThreeDeliveryResponse response=deliveryResource.checkSortingDiff(sourceRequest);
+        if(JdResponse.CODE_OK.equals(response.getCode()) || DeliveryResponse.CODE_Delivery_SORTING_DIFF.equals(response.getCode())         ){
+            res.setData(dataSourceToGateWay(response));
+        }else {
+            res.toFail(response.getMessage());
+        }
+
+        return res;
+    }
+
+    /**
+     * 数据转换
+     * @param data
+     * @return
+     */
+    private List<PackageDifferentialResponse> dataSourceToGateWay(ThreeDeliveryResponse data){
+        List<PackageDifferentialResponse> res=new ArrayList<>();
+        if(null==data || null==data.getData() || data.getData().size()<=0){
+            return res;
+        }
+        for (SendThreeDetail itme : data.getData()) {
+            PackageDifferentialResponse info=new PackageDifferentialResponse();
+            info.setBoxCode(itme.getBoxCode());
+            info.setPackageBarcode(itme.getPackageBarcode());
+            info.setScanStatusStr(itme.getMark());
+            info.setIsWaybillFull(itme.getIsWaybillFull());
+
+            res.add(info);
         }
 
         return res;
