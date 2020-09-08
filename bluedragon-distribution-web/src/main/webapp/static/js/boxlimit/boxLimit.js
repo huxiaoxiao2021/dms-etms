4fa7ffec582f8a2633d568e5a7386ca6ae4af79a
magang33 <magang33@jd.com>
2020-09-08 08:47:25 +0800

    按钮调整

--- bluedragon-distribution-web/src/main/webapp/static/js/boxlimit/boxLimit.js
+++ bluedragon-distribution-web/src/main/webapp/static/js/boxlimit/boxLimit.js
@@ -18,7 +18,8 @@ $(function () {
                 pagination: true, // 是否显示分页（*）
                 pageNumber: 1, // 初始化加载第一页，默认第一页
                 pageSize: 20, // 每页的记录行数（*）
-                pageList: [50, 100, 500], // 可供选择的每页的行数（*）
+                pageList: [20, 50, 100, 500], // 可供选择的每页的行数（*）
+                smartDisplay:false,
                 cache: false, // 是否使用缓存，默认为true，所以一般情况下需要设置一下这个属性（*）
                 sidePagination: "server", // 分页方式：client客户端分页，server服务端分页（*）
                 striped: true, // 是否显示行间隔色
