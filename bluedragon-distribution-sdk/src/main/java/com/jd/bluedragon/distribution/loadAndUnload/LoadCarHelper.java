647b0f3202490554298d7f900213f543333a7b96
wuming103 <wuming103@jd.com>
2020-10-15 22:22:08 +0800

    "装车任务车牌枚举、dao 代码部分提交"

---
+++ bluedragon-distribution-sdk/src/main/java/com/jd/bluedragon/distribution/loadAndUnload/LoadCarHelper.java
@@ -0,0 +1,108 @@
+package com.jd.bluedragon.distribution.loadAndUnload;
+
+import com.jd.ql.dms.common.web.mvc.api.DbEntity;
+
+
+/**
+ * @program: bluedragon-distribution
+ * @description:装车任务协助人
+ * @author: wuming
+ * @create: 2020-10-15 20:35
+ */
+public class LoadCarHelper extends DbEntity {
+
+
+    /**
+     * 任务Id关联load_car表id
+     */
+    private Long taskId;
+
+    /**
+     * 协助人姓名
+     */
+    private String  helperName;
+
+    /**
+     * 协助人erp
+     */
+    private String  helperErp;
+
+    /**
+     * 创建人erp
+     */
+    private String createUserErp;
+
+    /**
+     * 创建人姓名
+     */
+    private String createUserName;
+
+    /**
+     * 是否删除：1-有效，0-删除
+     */
+    private Integer yn;
+
+    /**
+     * 扩展字段
+     */
+    private String exp;
+
+    public LoadCarHelper() {
+    }
+
+    public Long getTaskId() {
+        return taskId;
+    }
+
+    public void setTaskId(Long taskId) {
+        this.taskId = taskId;
+    }
+
+    public String getHelperName() {
+        return helperName;
+    }
+
+    public void setHelperName(String helperName) {
+        this.helperName = helperName;
+    }
+
+    public String getHelperErp() {
+        return helperErp;
+    }
+
+    public void setHelperErp(String helperErp) {
+        this.helperErp = helperErp;
+    }
+
+    public String getCreateUserErp() {
+        return createUserErp;
+    }
+
+    public void setCreateUserErp(String createUserErp) {
+        this.createUserErp = createUserErp;
+    }
+
+    public String getCreateUserName() {
+        return createUserName;
+    }
+
+    public void setCreateUserName(String createUserName) {
+        this.createUserName = createUserName;
+    }
+
+    public Integer getYn() {
+        return yn;
+    }
+
+    public void setYn(Integer yn) {
+        this.yn = yn;
+    }
+
+    public String getExp() {
+        return exp;
+    }
+
+    public void setExp(String exp) {
+        this.exp = exp;
+    }
+}
