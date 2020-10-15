1f9ed145c1a173fd89027e2c8cbf65f47ad8a516
zhengchengfa <zhengchengfa@jd.com>
2020-10-15 12:33:37 +0800

    "【任务描述】货物装车发货取消无接口定义"

--- bluedragon-distribution-common-android/src/main/java/com/jd/bluedragon/common/dto/goodsLoadingScanning/request/GoodsLoadingReq.java
+++ bluedragon-distribution-common-android/src/main/java/com/jd/bluedragon/common/dto/goodsLoadingScanning/request/GoodsLoadingReq.java
@@ -1,5 +1,7 @@
 package com.jd.bluedragon.common.dto.goodsLoadingScanning.request;
 
+import java.util.Date;
+
 /**
  * 装货完成发货请求入参
  */
@@ -10,4 +12,54 @@ public class GoodsLoadingReq {
      * 业务来源
      */
     private Integer bizSource;
+
+    /**
+     * 任务号
+     */
+    private String taskId;
+
+    /**
+     * 操作人
+     */
+    private String createUser;
+
+    /**
+     * 操作人编码
+     */
+    private Integer createUserCode;
+
+    /**
+     * 发货交接单类型(1 退货 2 转站 3 第三方)
+     */
+    private Integer sendType;
+
+    /**
+     * 发货单位编码
+     */
+    private Integer createSiteCode;
+
+    /**
+     * 发货交接单号-发货批次号
+     */
+    private String sendCode;
+
+    /**
+     * 收货单位编码
+     */
+    private Integer receiveSiteCode;
+
+    /**
+     * 创建时间
+     */
+    private Date createTime;
+
+    /**
+     * 操作时间
+     */
+    private Date operateTime;
+
+    /**
+     * 是否删除 '0' 删除 '1' 使用
+     */
+    private Integer yn;
 }
