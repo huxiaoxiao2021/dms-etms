d39baabda0b9e6de291661153f4ca498a6a63840
zhengchengfa <zhengchengfa@jd.com>
2020-10-19 20:14:49 +0800

    "【任务描述】测试类编写"

---
+++ bluedragon-distribution-web/src/test/java/com/jd/bluedragon/distribution/external/gateway/service/impl/GoodsLoadingScanningServiceImplTest.java
@@ -0,0 +1,31 @@
+package com.jd.bluedragon.distribution.external.gateway.service.impl;
+
+import com.jd.bluedragon.common.dto.base.response.JdCResponse;
+import com.jd.bluedragon.common.dto.goodsLoadingScanning.request.GoodsExceptionScanningReq;
+import com.jd.bluedragon.common.dto.goodsLoadingScanning.response.GoodsExceptionScanningDto;
+import com.jd.bluedragon.external.gateway.service.GoodsLoadingScanningService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.List;
+
+//@ContextConfiguration( {"classpath:distribution-web-context-test.xml"})
+@RunWith(SpringJUnit4ClassRunner.class)
+public class GoodsLoadingScanningServiceImplTest {
+
+    @Autowired
+    private GoodsLoadingScanningService goodsLoadingScanningService;
+
+    @Test
+    public void testFindExceptionGoodsLoading() {
+        GoodsExceptionScanningReq param = new GoodsExceptionScanningReq();
+        param.setTaskId(123L);
+        JdCResponse<List<GoodsExceptionScanningDto>> res =  goodsLoadingScanningService.findExceptionGoodsLoading(param);
+
+        System.out.println("------------------");
+    }
+
+
+}
