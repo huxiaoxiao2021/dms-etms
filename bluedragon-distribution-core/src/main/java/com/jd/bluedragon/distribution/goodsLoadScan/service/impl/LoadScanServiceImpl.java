2b06d4d89855565edf4b118371aff845a9338c2e
lvyuan21 <lvyuan21@jd.com>
2020-10-20 12:37:19 +0800

    (1)把网关jsf接口再下沉一层

--- bluedragon-distribution-core/src/main/java/com/jd/bluedragon/distribution/goodsLoadScan/service/impl/LoadScanServiceImpl.java
+++ bluedragon-distribution-core/src/main/java/com/jd/bluedragon/distribution/goodsLoadScan/service/impl/LoadScanServiceImpl.java
@@ -46,9 +46,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
 
 import java.util.*;
 
+@Service("loadScanService")
 public class LoadScanServiceImpl implements LoadScanService {
     private final static Logger log = LoggerFactory.getLogger(LoadScanServiceImpl.class);
 
