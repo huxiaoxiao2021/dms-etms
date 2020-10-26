2801a9d337253103dab03dc67c5f73c0ae553aa4
zhengchengfa <zhengchengfa@jd.com>
2020-10-26 18:23:18 +0800

    "【任务描述】: "

--- bluedragon-distribution-core/src/main/java/com/jd/bluedragon/distribution/goodsLoadScan/service/impl/ExceptionScanServiceImpl.java
+++ bluedragon-distribution-core/src/main/java/com/jd/bluedragon/distribution/goodsLoadScan/service/impl/ExceptionScanServiceImpl.java
@@ -201,7 +201,7 @@ public class ExceptionScanServiceImpl implements ExceptionScanService {
         }
 
         if(log.isInfoEnabled()) {
-            log.debug("根据任务号【{}】查询不齐异常数据 --end-- 返回异常【{}】", taskId, JsonHelper.toJson(res));
+            log.debug("根据任务号【{}】查询不齐异常数据 --end-- 返回不齐数据【{}】", taskId, JsonHelper.toJson(res));
         }
         return res;
     }
