91bb5d2eec36fb5745e5f3478a345ebd61a4e14e
wuzuxiang <wuzuxiang@jd.com>
2020-11-27 11:44:17 +0800

    修改物流网关外部服务域的调用 使用SDK调用，取代以前的组件平台，组件平台下线

--- bluedragon-distribution-core/src/main/java/com/jd/bluedragon/core/crossbow/DMSCrossbowClient.java
+++ bluedragon-distribution-core/src/main/java/com/jd/bluedragon/core/crossbow/DMSCrossbowClient.java
@@ -3,16 +3,13 @@ package com.jd.bluedragon.core.crossbow;
 import com.jd.bluedragon.Constants;
 import com.jd.bluedragon.core.crossbow.security.CrossbowSecurityProcessorSelector;
 import com.jd.bluedragon.utils.JsonHelper;
-import com.jd.bluedragon.utils.SpringHelper;
 import com.jd.lop.crossbow.dto.LopRequest;
 import com.jd.lop.crossbow.dto.LopResponse;
-import com.jd.lsb.component.exception.ExecErrorException;
-import com.jd.lsb.component.service.CrossbowService;
+import com.jd.lop.crossbow.sdk.LopClient;
 import com.jd.ump.annotation.JProEnum;
 import com.jd.ump.annotation.JProfiler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Component;
 
 import javax.validation.constraints.NotNull;
 import java.util.Map;
@@ -23,16 +20,28 @@ import java.util.Map;
  *     用到的是架构部的crossBow组件：该组件实现了对三方公司接口的统一调用，屏蔽了内部因三方差异二带来的接口协议和网络权限的差异。
  *
  * @see com.jd.bluedragon.external.crossbow.AbstractCrossbowManager 内部对crossbow组件的统一调用封装
- * @link http://lft.jd.com/docCenter?docId=2978 组件化接入文档
+ * @link http://lcp.jdwl.com/#/docSoftwareSystem/17/44780 crossbow的外部服务域接入文档
  * @author wuzuxiang
  * @since 2019/10/17
  **/
-@Component("dmsCrossbowClient")
 public class DMSCrossbowClient {
 
     private static final Logger log = LoggerFactory.getLogger(DMSCrossbowClient.class);
 
-    private CrossbowService crossbowService;
+    /* crossbow调用地址 （不同环境使用的url不同,规则: 网关调用地址/execute。示例为测试环境地址） */
+    private String serverUrl;
+
+    /* 超时时间 单位毫秒 */
+    private Integer timeout;
+
+    /* 服务调用编码 （在网关“接入方维护”功能中配置） */
+    private String partnerId;
+
+    /* 接入方密钥 （在网关“接入方维护”功能中配置） */
+    private String partnerSecret;
+
+    /* 调用物流网关的客户端 */
+    private LopClient client;
 
     /***
      * 调用crossbow组件客户端的执行方法 目前不涉及加密，如果有需要对接加密的请完善该接口
@@ -53,34 +62,39 @@ public class DMSCrossbowClient {
         request.addUrlArgs(urlArgs);
         request.setAppKey(crossBowConfig.getAppKey());
         request.setBody(parameterStr);
-        try {
-            /* 2. 设置安全插件对应的内容 */
-            request = CrossbowSecurityProcessorSelector.getProcessor(crossBowConfig.getSecurityEnum())
-                    .handleSecurityContent(request, crossBowConfig);
 
-            /* 3. 请求物流网关代理，调用外部接口 */
-            crossbowService = getCrossbowService();
-            LopResponse response = crossbowService.execute(request);
-            if (response.getStatusCode() != 200) {
-                log.warn("调用物流网关crossBow组件失败，参数为：{}, 返回值为: {}", JsonHelper.toJson(request), JsonHelper.toJson(response));
-                throw new RuntimeException("调用物流网关crossBow组件失败,返回值：" + JsonHelper.toJson(response));
-            }
-            return response.getBody();
-        } catch (ExecErrorException e) {
-            log.error("调用物流网关的接口异常，请求参数: {}" , JsonHelper.toJson(parameterStr), e);
-            throw new RuntimeException("物流网关crossBow组件调用异常", e);
+        /* 2. 设置安全插件对应的内容 */
+        request = CrossbowSecurityProcessorSelector.getProcessor(crossBowConfig.getSecurityEnum())
+                .handleSecurityContent(request, crossBowConfig);
+
+        /* 3. 请求物流网关代理，调用外部接口 */
+        LopResponse response = client.execute(request);
+        if (response.getStatusCode() != 200) {
+            log.warn("调用物流网关crossBow组件失败，参数为：{}, 返回值为: {}", JsonHelper.toJson(request), JsonHelper.toJson(response));
+            throw new RuntimeException("调用物流网关crossBow组件失败,返回值：" + JsonHelper.toJson(response));
         }
+        return response.getBody();
     }
 
     /**
-     * 从Spring的容器中获取组件“crossbow”，从crossbow组件中获取原子组件crossbowService
-     * @return crossbowService组件的实例
+     * 默认的构造器
      */
-    private CrossbowService getCrossbowService() {
-        if (crossbowService == null) {
-            com.jd.lsb.component.api.component.Component component  = (com.jd.lsb.component.api.component.Component) SpringHelper.getBean("crossbow");
-            crossbowService= (CrossbowService)component.getServiceImpl("crossbowService");
-        }
-        return crossbowService;
+    public DMSCrossbowClient() {}
+
+    /**
+     * 构造器
+     */
+    public DMSCrossbowClient(String serverUrl, Integer timeout, String partnerId, String partnerSecret) {
+        this.serverUrl = serverUrl;
+        this.timeout = timeout;
+        this.partnerId = partnerId;
+        this.partnerSecret = partnerSecret;
+    }
+
+    /**
+     * 初始化 LopClient 的实例
+     */
+    public void initClient() {
+        client = LopClient.getLopClient(serverUrl, timeout, partnerId, partnerSecret);
     }
 }
