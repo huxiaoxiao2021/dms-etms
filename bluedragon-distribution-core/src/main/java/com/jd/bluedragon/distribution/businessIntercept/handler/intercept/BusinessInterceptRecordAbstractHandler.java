b629142ac2d5ac4471dd1c52d0473c882d99f1a1
fanggang7 <fanggang7@jd.com>
2021-01-11 18:41:27 +0800

    [fix] 拦截报表 修正查询取消运单时间

--- bluedragon-distribution-core/src/main/java/com/jd/bluedragon/distribution/businessIntercept/handler/intercept/BusinessInterceptRecordAbstractHandler.java
+++ bluedragon-distribution-core/src/main/java/com/jd/bluedragon/distribution/businessIntercept/handler/intercept/BusinessInterceptRecordAbstractHandler.java
@@ -1,10 +1,16 @@
 package com.jd.bluedragon.distribution.businessIntercept.handler.intercept;
 
+import com.jd.bluedragon.core.base.WaybillQueryManager;
 import com.jd.bluedragon.core.jmq.producer.DefaultJMQProducer;
 import com.jd.bluedragon.distribution.api.Response;
 import com.jd.bluedragon.distribution.businessIntercept.dto.SaveInterceptMsgDto;
 import com.jd.bluedragon.distribution.waybill.domain.CancelWaybill;
 import com.jd.bluedragon.distribution.waybill.service.WaybillCancelService;
+import com.jd.etms.cache.util.EnumBusiCode;
+import com.jd.etms.waybill.domain.BaseEntity;
+import com.jd.etms.waybill.domain.Waybill;
+import com.jd.etms.waybill.dto.BigWaybillDto;
+import com.jd.etms.waybill.dto.WChoice;
 import com.jd.ql.basic.util.DateUtil;
 import org.apache.commons.collections4.CollectionUtils;
 import org.slf4j.Logger;
@@ -35,6 +41,9 @@ public abstract class BusinessInterceptRecordAbstractHandler implements IBusines
 
     @Autowired
     private WaybillCancelService waybillCancelService;
+
+    @Autowired
+    private WaybillQueryManager waybillQueryManager;
 
     /**
      * 处理拦截提交数据
@@ -96,7 +105,7 @@ public abstract class BusinessInterceptRecordAbstractHandler implements IBusines
         List<String> automaticWaybillCancelInterceptCodeList = Arrays.asList(this.automaticInterceptCodeWaybillCancel.split(","));
         // 如果是取消类型的code，就查询一下运单取消记录
         if((Objects.equals(interceptOperateDeviceTypePda, msgDto.getDeviceType()) && pdaWaybillCancelInterceptCodeList.contains(String.valueOf(msgDto.getInterceptCode())))
-                && (Objects.equals(deviceTypeAutomatic, msgDto.getDeviceType()) && automaticWaybillCancelInterceptCodeList.contains(String.valueOf(msgDto.getInterceptCode())))
+                || (Objects.equals(deviceTypeAutomatic, msgDto.getDeviceType()) && automaticWaybillCancelInterceptCodeList.contains(String.valueOf(msgDto.getInterceptCode())))
         ){
             Response<Boolean> cancelTimeResult = this.getAndSetWaybillCancelTime(msgDto);
             result.setData(cancelTimeResult.getData());
@@ -124,6 +133,39 @@ public abstract class BusinessInterceptRecordAbstractHandler implements IBusines
                 CancelWaybill cancelWaybill = waybillCancelList.get(0);
                 msgDto.setInterceptEffectTime(DateUtil.parse(cancelWaybill.getCreateTime(), DateUtil.FORMAT_DATE_TIME).getTime());
             }
+        } catch (Exception e) {
+            log.error("getWaybillCancelTime exception");
+            result.toError("获取运单取消时间失败");
+            result.setData(false);
+        }
+
+        return result;
+    }
+
+    /**
+     * 获取取消拦截生效时间
+     * @param msgDto 消息内容
+     * @return 处理结果
+     * @author fanggang7
+     * @time 2021-01-10 18:55:51 周日
+     */
+    protected Response<Boolean> getAndSetWaybillCreateTime(SaveInterceptMsgDto msgDto){
+        Response<Boolean> result = new Response<>();
+        result.toSucceed();
+        result.setData(true);
+
+        try {
+            WChoice wChoice = new WChoice();
+            wChoice.setQueryWaybillC(true);
+            BaseEntity<BigWaybillDto> bigWaybillDtoBaseEntity = waybillQueryManager.getDataByChoice(msgDto.getWaybillCode(), wChoice);
+            if(EnumBusiCode.BUSI_SUCCESS.getCode() == bigWaybillDtoBaseEntity.getResultCode()){
+                BigWaybillDto bigWaybillDto = bigWaybillDtoBaseEntity.getData();
+                Waybill waybill = bigWaybillDto.getWaybill();
+                if(waybill != null && waybill.getCreateTime() != null){
+                    msgDto.setInterceptEffectTime(waybill.getCreateTime().getTime());
+                }
+            }
+
         } catch (Exception e) {
             log.error("getWaybillCancelTime exception");
             result.toError("获取运单取消时间失败");
