8f5d84ae7fd528e04ed387802fdf2d49e1585fa1
biyubo <biyubo@jd.com>
2021-01-21 14:53:51 +0800

    用户ERP获取站点改为由用户ID获取

--- bluedragon-distribution-core/src/main/java/com/jd/bluedragon/distribution/print/waybill/handler/C2cInterceptHandler.java
+++ bluedragon-distribution-core/src/main/java/com/jd/bluedragon/distribution/print/waybill/handler/C2cInterceptHandler.java
@@ -78,7 +78,7 @@ public class C2cInterceptHandler extends NeedPrepareDataInterceptHandler<Waybill
         }
 
         //校验操作人所属场地是否为分拣中心
-        if(null!=param.getRequest().getUserERP()){
+        if(null!=param.getRequest().getUserCode()){
             BaseStaffSiteOrgDto baseStaffByErpNoCache = baseMajorManager.getBaseStaffByStaffId(param.getRequest().getUserCode());
             if(null!=baseStaffByErpNoCache && Integer.valueOf(64).equals(baseStaffByErpNoCache.getSiteType())){
                 param.setDmsCenter(Boolean.TRUE);
