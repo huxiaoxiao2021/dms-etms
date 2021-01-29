141be866684ec782a0f8c20625fe106e7c6c8020
wuming103 <wuming103@jd.com>
2021-01-29 17:39:34 +0800

    "提交解决报错"

--- bluedragon-distribution-core/src/main/java/com/jd/bluedragon/core/base/WaybillStagingCheckManagerImpl.java
+++ bluedragon-distribution-core/src/main/java/com/jd/bluedragon/core/base/WaybillStagingCheckManagerImpl.java
@@ -15,28 +15,28 @@ import javax.annotation.Resource;
  * @author: wuming
  * @create: 2021-01-25 11:01
  */
-@Service("waybillStagingCheckManager")
-public class WaybillStagingCheckManagerImpl implements WaybillStagingCheckManager {
-
-    private final Logger log = LoggerFactory.getLogger(WaybillStagingCheckManagerImpl.class);
-
-    @Resource
-//    private StagingServiceWS stagingServiceWS;
-
-    @JProfiler(jKey = "DMSCORE.WaybillStagingCheckManagerImpl.stagingCheck", jAppName = Constants.UMP_APP_NAME_DMSWORKER, mState = {JProEnum.TP, JProEnum.FunctionError})
-    @Override
-    public boolean stagingCheck(String packageCode, Integer operateSiteCode) {
-        try {
-//            StagingCheckReq req = new StagingCheckReq();
-//            req.setCurrentSiteCode(operateSiteCode);
-//            req.setPackageCode(packageCode);
-//            Result<Boolean> result = stagingServiceWS.stagingCheck(req);
-//            if (null != result && Constants.STAGING_CHECK_SUCCESS_CODE.equals(result.getCode())) {
-//                return true;
-//            }
-        } catch (Exception e) {
-            log.error("WaybillStagingCheckManagerImpl.stagingCheck.error-packageCode={}", packageCode, e);
-        }
-        return false;
-    }
-}
+//@Service("waybillStagingCheckManager")
+//public class WaybillStagingCheckManagerImpl implements WaybillStagingCheckManager {
+//
+//    private final Logger log = LoggerFactory.getLogger(WaybillStagingCheckManagerImpl.class);
+//
+//    @Resource
+////    private StagingServiceWS stagingServiceWS;
+//
+//    @JProfiler(jKey = "DMSCORE.WaybillStagingCheckManagerImpl.stagingCheck", jAppName = Constants.UMP_APP_NAME_DMSWORKER, mState = {JProEnum.TP, JProEnum.FunctionError})
+//    @Override
+//    public boolean stagingCheck(String packageCode, Integer operateSiteCode) {
+//        try {
+////            StagingCheckReq req = new StagingCheckReq();
+////            req.setCurrentSiteCode(operateSiteCode);
+////            req.setPackageCode(packageCode);
+////            Result<Boolean> result = stagingServiceWS.stagingCheck(req);
+////            if (null != result && Constants.STAGING_CHECK_SUCCESS_CODE.equals(result.getCode())) {
+////                return true;
+////            }
+//        } catch (Exception e) {
+//            log.error("WaybillStagingCheckManagerImpl.stagingCheck.error-packageCode={}", packageCode, e);
+//        }
+//        return false;
+//    }
+//}
