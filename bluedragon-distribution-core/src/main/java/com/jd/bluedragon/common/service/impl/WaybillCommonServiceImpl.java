f005c1863ea308a6d10c171fbf7d04b4c57ff633
wangzhen580 <wangzhen580@jd.com>
2021-02-02 10:48:54 +0800

    收件人地址寄递码(删除打印日志)

--- bluedragon-distribution-core/src/main/java/com/jd/bluedragon/common/service/impl/WaybillCommonServiceImpl.java
+++ bluedragon-distribution-core/src/main/java/com/jd/bluedragon/common/service/impl/WaybillCommonServiceImpl.java
@@ -689,17 +689,11 @@ public class WaybillCommonServiceImpl implements WaybillCommonService {
 
             // 如果获取到收件人地址寄递码(receiveAdderesssCode存在)，则转换为二维码替换左下角二维码链接内容，同时将原二维码文本“扫码寄快递”设置为空。
             if (waybillExt != null) {
-                this.log.info("根据waybill获取waybillExt，运单号：【{}】", waybill.getWaybillCode());
                 String receiveAddressCode = waybillExt.getReceiveAddressCode();
                 if (StringUtils.isNotBlank(receiveAddressCode)) {
-                    this.log.info("根据waybillExt获取寄递码receiveAddressCode：{}，运单号：【{}】", receiveAddressCode, waybill.getWaybillCode());
                     target.setPopularizeMatrixCode(receiveAddressCode);
                     target.setPopularizeMatrixCodeDesc("");
-                } else {
-                    this.log.warn("未获取到寄递码receiveAddressCode，运单号：【{}】", waybill.getWaybillCode());
                 }
-            } else {
-                this.log.warn("未获取到waybillExt，运单号：【{}】", waybill.getWaybillCode());
             }
 
             //包裹有话说
