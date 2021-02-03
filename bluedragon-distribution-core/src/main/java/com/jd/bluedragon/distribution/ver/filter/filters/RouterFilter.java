dcfb38e792b3b164b79eb425590e32f252b0005e
xumigen <xumigen@jd.com>
2021-02-03 15:19:58 +0800

    问题优化 路由下一网点 匹配，ABA问题 从后面开始匹配

--- bluedragon-distribution-core/src/main/java/com/jd/bluedragon/distribution/ver/filter/filters/RouterFilter.java
+++ bluedragon-distribution-core/src/main/java/com/jd/bluedragon/distribution/ver/filter/filters/RouterFilter.java
@@ -3,6 +3,8 @@ package com.jd.bluedragon.distribution.ver.filter.filters;
 import com.jd.bluedragon.Constants;
 import com.jd.bluedragon.distribution.api.response.SortingResponse;
 import com.jd.bluedragon.distribution.base.service.SiteService;
+import com.jd.bluedragon.distribution.router.RouterService;
+import com.jd.bluedragon.distribution.router.domain.dto.RouteNextDto;
 import com.jd.bluedragon.distribution.rule.domain.Rule;
 import com.jd.bluedragon.distribution.ver.domain.FilterContext;
 import com.jd.bluedragon.distribution.ver.exception.SortingCheckException;
@@ -11,12 +13,14 @@ import com.jd.bluedragon.distribution.ver.filter.FilterChain;
 import com.jd.bluedragon.distribution.waybill.service.WaybillCacheService;
 import com.jd.bluedragon.utils.StringHelper;
 import com.jd.bluedragon.utils.WaybillCacheHelper;
+import org.apache.commons.collections.CollectionUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 /**
  * Created by xumei3 on 2018/3/21.
@@ -33,6 +37,9 @@ public class RouterFilter implements Filter {
 
     @Autowired
     private WaybillCacheService waybillCacheService;
+
+    @Autowired
+    private RouterService routerService;
 
     @Override
     public void doFilter(FilterContext request, FilterChain chain) throws Exception {
@@ -60,50 +67,34 @@ public class RouterFilter implements Filter {
             if(StringHelper.isEmpty(waybillCode) && request.getWaybillCache()!=null){
                 waybillCode = request.getWaybillCache().getWaybillCode();
             }
+            RouteNextDto routeNextDto = routerService.matchRouterNextNode(createSiteCode,waybillCode);
 
-            //根据waybillCode查库获取路由信息
-            String router = waybillCacheService.getRouterByWaybillCode(waybillCode);
-            boolean verifyPass = false;
-            List<Integer> routerShow = new ArrayList<>();
-
-            boolean getCurNodeFlag = false;
-
-            logger.info("RouterFilter根据运单号获取运单路由，运单号:" + waybillCode + "，路由：" + router);
-            if (StringHelper.isNotEmpty(router)) {
-                //路由校验逻辑
-                String[] routerNodes = router.split(WAYBILL_ROUTER_SPLITER);
-                for (int i = 0; i < routerNodes.length - 1; i++) {
-                    int curNode = Integer.parseInt(routerNodes[i]);
-                    int nexNode = Integer.parseInt(routerNodes[i + 1]);
-                    if(curNode == createSiteCode){
-                        getCurNodeFlag = true;
-                        routerShow.add(nexNode);
-                        if(nexNode == receiveSiteCode){
-                            verifyPass = true;
-                            break;
-                        }
+            logger.info("RouterFilter根据运单号获取运单路由，运单号[{}]routExistCurrentSite[{}]firstNextSiteId[{}]",
+                    waybillCode,routeNextDto.isRoutExistCurrentSite(),routeNextDto.getFirstNextSiteId());
+            if(routeNextDto.isRoutExistCurrentSite() &&
+                    !isRightReceiveSite(receiveSiteCode, routeNextDto)) {
+                //将下一站由编码转换成名称，并进行截取，供pda提示
+                StringBuilder routerShortNames= new StringBuilder();
+                for(Integer dmsCode : routeNextDto.getNextSiteIdList()){
+                    if(StringHelper.isEmpty(siteService.getDmsShortNameByCode(dmsCode))){
+                        continue;
                     }
+                    routerShortNames.append(siteService.getDmsShortNameByCode(dmsCode)).append(Constants.SEPARATOR_COMMA);
                 }
-
-                //路由中包括当前操作的分拣中心并且没有通过校验
-                if(getCurNodeFlag && !verifyPass) {
-                    //将下一站由编码转换成名称，并进行截取，供pda提示
-                    StringBuilder routerShortNames= new StringBuilder();
-                    for(Integer dmsCode : routerShow){
-                        if(StringHelper.isEmpty(siteService.getDmsShortNameByCode(dmsCode))){
-                            continue;
-                        }
-                        routerShortNames.append(siteService.getDmsShortNameByCode(dmsCode)).append(Constants.SEPARATOR_COMMA);
-                    }
-                    if(StringHelper.isNotEmpty(routerShortNames.toString())){
-                        routerShortNames = new StringBuilder(routerShortNames.substring(0, routerShortNames.length() - 1));
-                    }
-                    throw new SortingCheckException(SortingResponse.CODE_CROUTER_ERROR,
-                            SortingResponse.MESSAGE_CROUTER_ERROR + "路由下一站：" + routerShortNames);
+                if(StringHelper.isNotEmpty(routerShortNames.toString())){
+                    routerShortNames = new StringBuilder(routerShortNames.substring(0, routerShortNames.length() - 1));
                 }
+                throw new SortingCheckException(SortingResponse.CODE_CROUTER_ERROR,
+                        SortingResponse.MESSAGE_CROUTER_ERROR + "路由下一站：" + routerShortNames);
             }
+
         }
 
         chain.doFilter(request, chain);
+    }
+
+    private boolean isRightReceiveSite(Integer receiveSiteCode, RouteNextDto routeNextDto) {
+        return CollectionUtils.isNotEmpty(routeNextDto.getNextSiteIdList())
+                && routeNextDto.getNextSiteIdList().contains(receiveSiteCode);
     }
 }
