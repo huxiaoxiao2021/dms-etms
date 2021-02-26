1e637631736a75bfb497086d98656e68d03e8c66
biyubo <biyubo@jd.com>
2021-02-26 18:46:16 +0800

    包裹回流扫描-修改

--- bluedragon-distribution-web/src/main/java/com/jd/bluedragon/distribution/external/gateway/service/impl/ReflowPackageGatewayServiceImpl.java
+++ bluedragon-distribution-web/src/main/java/com/jd/bluedragon/distribution/external/gateway/service/impl/ReflowPackageGatewayServiceImpl.java
@@ -12,7 +12,6 @@ import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-@Service("reflowPackageGatewayService")
 @Slf4j
 public class ReflowPackageGatewayServiceImpl implements ReflowPackageGatewayService {
 
