4e9a028bbfaa8be32e5fb854cc0ded15029bca5a
magang33 <magang33@jd.com>
2021-03-03 20:58:07 +0800

    C网异常重量拦截器拦截器

--- bluedragon-distribution-core/src/main/java/com/jd/bluedragon/distribution/print/waybill/handler/AbnormalWeightForCHandler.java
+++ bluedragon-distribution-core/src/main/java/com/jd/bluedragon/distribution/print/waybill/handler/AbnormalWeightForCHandler.java
@@ -6,9 +6,13 @@ import com.jd.bluedragon.distribution.api.domain.WeightOperFlow;
 import com.jd.bluedragon.distribution.api.request.WaybillPrintRequest;
 import com.jd.bluedragon.distribution.command.JdResult;
 import com.jd.bluedragon.distribution.handler.Handler;
+import com.jd.bluedragon.distribution.handler.InterceptHandler;
+import com.jd.bluedragon.distribution.handler.InterceptResult;
 import com.jd.bluedragon.distribution.print.domain.BasePrintWaybill;
+import com.jd.bluedragon.dms.utils.BusinessUtil;
 import com.jd.bluedragon.utils.StringHelper;
 import com.jd.ql.basic.dto.BaseSiteInfoDto;
+import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,23 +31,27 @@ import java.util.Objects;
  * 2.当包裹重量＞1000kg时，提示“重量错误，请重新称重！”。系统不记录。
  */
 @Service("abnormalWeightForCHandler")
-public class AbnormalWeightForCHandler implements Handler<WaybillPrintContext,JdResult<String>>{
+public class AbnormalWeightForCHandler implements InterceptHandler<WaybillPrintContext,String> {
 	private static final String splitRegex = ",";
 
 	// 拦截的 操作类型
-	@Value("print.abnormal.weight.printOperateTypes:'100101,100102,100105,100107'")
+	@Value("${print.abnormal.weight.printOperateTypes:100101,100102,100105,100107}")
 	private String printOperateTypes;
 
 	// 重量的阈值范围
-	@Value("print.abnormal.weight.weightThresholds:'60,1000'")
+	@Value("${print.abnormal.weight.weightThresholds:60,1000}")
 	private String weightThresholds;
 
 	@Override
-	public JdResult<String> handle(WaybillPrintContext context) {
+	public InterceptResult<String> handle(WaybillPrintContext context) {
 
 		WaybillPrintRequest request = context.getRequest();
 		if (request != null) {
 			if (checkOperateType(request.getOperateType())) {
+				// 判断waybillSign是否 C 网
+				if (!BusinessUtil.isCInternet(context.getWaybillSign())) {
+					return context.getResult();
+				}
 				if (request.hasWeighted()) {
 					// 重量异常 阈值
 					String[] split = weightThresholds.split(splitRegex);
@@ -53,10 +61,10 @@ public class AbnormalWeightForCHandler implements Handler<WaybillPrintContext,Jd
 					double thresholdLow = Double.parseDouble(split[0]);
 					double thresholdHigh = Double.parseDouble(split[1]);
 
-					// 弱拦截状态，提示确认重量
+					// 强拦截状态，提示确认重量
 					if (weight > thresholdLow && weight <= thresholdHigh) {
 						if (!Objects.equals(Boolean.TRUE, request.getConfirm())) {
-							context.getResult().toWeakSuccess(JdResponse.CODE_PRINT_WEIGHT_WARNING, JdResponse.MESSAGE_CODE_PRINT_WEIGHT_WARNING);
+							context.getResult().toNeedConfirmStatus(JdResponse.CODE_PRINT_WEIGHT_WARNING, JdResponse.MESSAGE_CODE_PRINT_WEIGHT_WARNING);
 						}
 					}
 					// 强制拦截状态，不通过
