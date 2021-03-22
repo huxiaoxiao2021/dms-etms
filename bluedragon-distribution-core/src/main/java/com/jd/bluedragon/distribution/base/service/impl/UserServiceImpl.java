bf92b7661e732c46d7d1a1729fb276a7d113f4f6
fanggang7 <fanggang7@jd.com>
2021-03-22 09:53:46 +0800

    [opt] PDA消息通知token验证接口

--- bluedragon-distribution-core/src/main/java/com/jd/bluedragon/distribution/base/service/impl/UserServiceImpl.java
+++ bluedragon-distribution-core/src/main/java/com/jd/bluedragon/distribution/base/service/impl/UserServiceImpl.java
@@ -166,18 +166,15 @@ public class UserServiceImpl extends AbstractBaseUserService implements UserServ
     /**
      * 客户端登录token验证
      *
-     * @param loginWithTokenVerifyRequest
      * @return
      * @author fanggang7
      * @time 2021-03-09 19:32:02 周二
      */
     @Override
-    public JdResult<Boolean> verifyClientLoginToken(LoginWithTokenVerifyRequest loginWithTokenVerifyRequest) {
+    public JdResult<Boolean> verifyClientLoginToken(String deviceId, String token) {
         JdResult<Boolean> result = new JdResult<>();
         result.toSuccess("success");
         try {
-            String deviceId = loginWithTokenVerifyRequest.getDeviceId();
-            String token = loginWithTokenVerifyRequest.getToken();
             if(StringUtils.isBlank(deviceId) || StringUtils.isBlank(token)){
                 result.setCode(JdResponse.CODE_PARAM_ERROR);
                 result.setMessage(JdResponse.MESSAGE_PARAM_ERROR);
--- bluedragon-distribution-web/src/main/java/com/jd/bluedragon/distribution/external/service/impl/DmsBaseServiceImpl.java
+++ bluedragon-distribution-web/src/main/java/com/jd/bluedragon/distribution/external/service/impl/DmsBaseServiceImpl.java
@@ -4,7 +4,6 @@ import com.jd.bluedragon.Constants;
 import com.jd.bluedragon.configuration.ucc.UccPropertyConfiguration;
 import com.jd.bluedragon.distribution.api.JdResponse;
 import com.jd.bluedragon.distribution.api.request.LoginRequest;
-import com.jd.bluedragon.distribution.api.request.LoginWithTokenVerifyRequest;
 import com.jd.bluedragon.distribution.api.response.BaseResponse;
 import com.jd.bluedragon.distribution.api.response.LoginUserResponse;
 import com.jd.bluedragon.distribution.base.service.UserService;
@@ -18,7 +17,6 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.UUID;
 
 /**
  * <p>
@@ -76,15 +74,14 @@ public class DmsBaseServiceImpl implements DmsBaseService {
     /**
      * 客户端登录token验证
      *
-     * @param loginWithTokenVerifyRequest
      * @return
      * @author fanggang7
      * @time 2021-03-09 19:32:02 周二
      */
     @Override
     @JProfiler(jKey = "DMSWEB.DmsBaseServiceImpl.verifyClientLoginToken", mState = {JProEnum.TP, JProEnum.FunctionError}, jAppName = Constants.UMP_APP_NAME_DMSWEB)
-    public JdResult verifyClientLoginToken(LoginWithTokenVerifyRequest loginWithTokenVerifyRequest) {
-        return userService.verifyClientLoginToken(loginWithTokenVerifyRequest);
+    public JdResult verifyClientLoginToken(String deviceId, String token) {
+        return userService.verifyClientLoginToken(deviceId, token);
     }
 
     @Override
