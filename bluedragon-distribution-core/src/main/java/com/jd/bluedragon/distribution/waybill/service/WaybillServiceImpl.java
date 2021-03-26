f0eb2310d07a0ef32aff4c3ffee03794f5aecf5e
liming522 <liming522@jd.com>
2021-03-26 15:00:50 +0800

    已退款禁止使用现场预分拣

--- bluedragon-distribution-core/src/main/java/com/jd/bluedragon/distribution/waybill/service/WaybillServiceImpl.java
+++ bluedragon-distribution-core/src/main/java/com/jd/bluedragon/distribution/waybill/service/WaybillServiceImpl.java
@@ -1,9 +1,11 @@
 package com.jd.bluedragon.distribution.waybill.service;
 
 import com.jd.bluedragon.Constants;
+import com.jd.bluedragon.common.dto.base.response.JdCResponse;
 import com.jd.bluedragon.configuration.ucc.UccPropertyConfiguration;
 import com.jd.bluedragon.core.base.BaseMajorManager;
 import com.jd.bluedragon.core.base.WaybillQueryManager;
+import com.jd.bluedragon.core.jsf.dms.BlockerQueryWSJsfManager;
 import com.jd.bluedragon.core.jsf.dms.CancelWaybillJsfManager;
 import com.jd.bluedragon.distribution.abnormalwaybill.domain.AbnormalWayBill;
 import com.jd.bluedragon.distribution.abnormalwaybill.service.AbnormalWayBillService;
@@ -115,6 +117,9 @@ public class WaybillServiceImpl implements WaybillService {
 
     @Autowired
     private CancelWaybillJsfManager cancelWaybillJsfManager;
+
+    @Autowired
+    private BlockerQueryWSJsfManager blockerQueryWSJsfManager;
 
     /**
      * 普通运单类型（非移动仓内配）
@@ -1067,6 +1072,12 @@ public class WaybillServiceImpl implements WaybillService {
             JsfResponse<WaybillCancelJsfResponse> cancelJsfResponseJsfResponse = cancelWaybillJsfManager.dealCancelWaybill(waybillForPreSortOnSiteRequest.getWaybill());
             if (!cancelJsfResponseJsfResponse.getCode().equals(JsfResponse.SUCCESS_CODE)){
                 result.customMessage(InvokeResult.RESULT_INTERCEPT_CODE,cancelJsfResponseJsfResponse.getMessage());
+                return result;
+            }
+            //规则4-已退款的禁止 操作现场预分拣
+            JdCResponse jdCResponse = blockerQueryWSJsfManager.queryExceptionOrders(waybill.getWaybillCode());
+            if(!jdCResponse.getCode().equals(JdCResponse.CODE_SUCCESS)){
+                result.customMessage(InvokeResult.RESULT_INTERCEPT_CODE,jdCResponse.getMessage());
                 return result;
             }
 
