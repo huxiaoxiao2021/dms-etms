88cdfec92befaf0ff6f8f5db4091ce0c839dadca
biyubo <biyubo@jd.com>
2021-04-06 21:05:49 +0800

    安卓弃件暂存

--- bluedragon-distribution-core/src/main/java/com/jd/bluedragon/external/gateway/service/ReflowPackageGatewayService.java
+++ bluedragon-distribution-core/src/main/java/com/jd/bluedragon/external/gateway/service/ReflowPackageGatewayService.java
@@ -2,6 +2,7 @@ package com.jd.bluedragon.external.gateway.service;
 
 import com.jd.bluedragon.common.dto.base.response.JdCResponse;
 import com.jd.bluedragon.common.dto.recyclematerial.request.ReflowPackageRequest;
+import com.jd.bluedragon.common.dto.wastepackagestorage.request.WastePackageJSFRequest;
 
 public interface ReflowPackageGatewayService {
 
@@ -12,6 +13,10 @@ public interface ReflowPackageGatewayService {
      */
     JdCResponse<Boolean> reflowPackageSubmit(ReflowPackageRequest request);
 
-
-
+    /**
+     * 弃件暂存
+     * @param request
+     * @return
+     */
+    JdCResponse<Boolean> wastepackagestorage(WastePackageJSFRequest request);
 }
