242ed382a13f11bcdd85553f03639157638dcc96
fanggang7 <fanggang7@jd.com>
2021-05-06 16:50:01 +0800

    [fix] 返回字段getter及setter

--- bluedragon-distribution-sdk/src/main/java/com/jd/bluedragon/distribution/sealVehicle/domain/SealCarNotCollectedDto.java
+++ bluedragon-distribution-sdk/src/main/java/com/jd/bluedragon/distribution/sealVehicle/domain/SealCarNotCollectedDto.java
@@ -3,11 +3,11 @@ package com.jd.bluedragon.distribution.sealVehicle.domain;
 import java.io.Serializable;
 
 /**
- * description
+ * 封车集齐监控报表返回参数
  *
  * @author fanggang7
  * @copyright jd.com 京东物流JDL
- * @time
+ * @time 2021-04-29 14:22:48 周日
  */
 public class SealCarNotCollectedDto implements Serializable {
 
@@ -42,4 +42,58 @@ public class SealCarNotCollectedDto implements Serializable {
      * 封车包裹数
      */
     private Integer packageNumSeal;
+
+    public Long getSiteId() {
+        return siteId;
+    }
+
+    public SealCarNotCollectedDto setSiteId(Long siteId) {
+        this.siteId = siteId;
+        return this;
+    }
+
+    public String getSealCarCode() {
+        return sealCarCode;
+    }
+
+    public SealCarNotCollectedDto setSealCarCode(String sealCarCode) {
+        this.sealCarCode = sealCarCode;
+        return this;
+    }
+
+    public String getVehicleNumber() {
+        return vehicleNumber;
+    }
+
+    public SealCarNotCollectedDto setVehicleNumber(String vehicleNumber) {
+        this.vehicleNumber = vehicleNumber;
+        return this;
+    }
+
+    public String getWaybillCode() {
+        return waybillCode;
+    }
+
+    public SealCarNotCollectedDto setWaybillCode(String waybillCode) {
+        this.waybillCode = waybillCode;
+        return this;
+    }
+
+    public Integer getPackageNumSys() {
+        return packageNumSys;
+    }
+
+    public SealCarNotCollectedDto setPackageNumSys(Integer packageNumSys) {
+        this.packageNumSys = packageNumSys;
+        return this;
+    }
+
+    public Integer getPackageNumSeal() {
+        return packageNumSeal;
+    }
+
+    public SealCarNotCollectedDto setPackageNumSeal(Integer packageNumSeal) {
+        this.packageNumSeal = packageNumSeal;
+        return this;
+    }
 }
