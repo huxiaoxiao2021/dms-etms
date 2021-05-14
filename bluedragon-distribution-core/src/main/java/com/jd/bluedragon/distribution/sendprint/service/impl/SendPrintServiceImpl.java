0bde8e43af3929b1cab539c5473196384758c3ec
hujiping1 <hujiping@jd.com>
2021-05-14 15:19:58 +0800

    0.0问题修改

--- bluedragon-distribution-core/src/main/java/com/jd/bluedragon/distribution/sendprint/service/impl/SendPrintServiceImpl.java
+++ bluedragon-distribution-core/src/main/java/com/jd/bluedragon/distribution/sendprint/service/impl/SendPrintServiceImpl.java
@@ -1544,7 +1544,7 @@ public class SendPrintServiceImpl implements SendPrintService {
         printHandoverListDto.setSendUser(sendM.getCreateUser());
         printHandoverListDto.setSendUserCode(sendM.getCreateUserCode());
 
-        printHandoverListDto.setIsCancel((Byte.valueOf(String.valueOf(Constants.DOUBLE_ZERO))));
+        printHandoverListDto.setIsCancel((Byte.valueOf(String.valueOf(Constants.NUMBER_ZERO))));
         printHandoverListDto.setIsNew(Byte.valueOf(String.valueOf(Constants.NUMBER_ZERO)));
 
         printHandoverListDto.setSendTime(sendM.getOperateTime() == null ? null : sendM.getOperateTime().getTime());
