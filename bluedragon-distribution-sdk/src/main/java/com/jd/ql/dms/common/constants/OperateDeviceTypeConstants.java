236808e820369dfdeb8faa354d8e934633d48395
fanggang7 <fanggang7@jd.com>
2021-06-09 15:46:49 +0800

    [fix] 拦截报表 称重处理节点值修复

---
+++ bluedragon-distribution-sdk/src/main/java/com/jd/ql/dms/common/constants/OperateDeviceTypeConstants.java
@@ -0,0 +1,36 @@
+package com.jd.ql.dms.common.constants;
+
+/**
+ * 操作设备类型常量
+ *
+ * @author fanggang7
+ * @copyright jd.com 京东物流JDL
+ * @time 2021-05-12 16:38:40 周三
+ */
+public class OperateDeviceTypeConstants {
+
+    /**
+     * PDA
+     */
+    public static final int PDA = 1;
+
+    /**
+     * 自动化设备
+     */
+    public static final int AUTOMATIC = 2;
+
+    /**
+     * DWS设备
+     */
+    public static final int MACHINE_DWS = 3;
+
+    /**
+     * 打印客户端
+     */
+    public static final int PRINT_CLIENT = 4;
+
+    /**
+     * 龙门架
+     */
+    public static final int DRAGON_DOOR_FRAME = 4;
+}
