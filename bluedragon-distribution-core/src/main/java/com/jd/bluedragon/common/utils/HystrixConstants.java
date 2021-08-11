95c791c5bd6cf929493b148c100b6877d5fd2606
xumigen <xumigen@jd.com>
2021-08-11 18:05:15 +0800

    hystrix 提取常量

---
+++ bluedragon-distribution-core/src/main/java/com/jd/bluedragon/common/utils/HystrixConstants.java
@@ -0,0 +1,15 @@
+package com.jd.bluedragon.common.utils;
+
+import com.netflix.hystrix.HystrixCommandGroupKey;
+
+/**
+ * @program: bluedragon-distribution
+ * @description: hystrix 常量
+ * @author: xumigen
+ * @create: 2021-08-11 18:03
+ **/
+public class HystrixConstants {
+
+    private String HystrixCommandGroupKey;
+
+}
