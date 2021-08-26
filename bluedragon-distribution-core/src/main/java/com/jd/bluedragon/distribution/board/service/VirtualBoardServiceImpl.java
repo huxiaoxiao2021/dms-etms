87fb4964e70bf42b7ef468dfa1ebc8fdd58483c6
fanggang7 <fanggang7@jd.com>
2021-08-26 10:10:05 +0800

    [feature] 组板发货功能 组板与取消组板校验发货与封车

--- bluedragon-distribution-core/src/main/java/com/jd/bluedragon/distribution/board/service/VirtualBoardServiceImpl.java
+++ bluedragon-distribution-core/src/main/java/com/jd/bluedragon/distribution/board/service/VirtualBoardServiceImpl.java
@@ -6,8 +6,8 @@ import com.jd.bluedragon.common.dto.base.request.OperatorInfo;
 import com.jd.bluedragon.common.dto.base.request.User;
 import com.jd.bluedragon.common.dto.base.response.JdCResponse;
 import com.jd.bluedragon.common.dto.board.request.*;
-import com.jd.bluedragon.common.dto.board.response.VirtualBoardResultDto;
 import com.jd.bluedragon.common.dto.board.response.UnbindVirtualBoardResultDto;
+import com.jd.bluedragon.common.dto.board.response.VirtualBoardResultDto;
 import com.jd.bluedragon.common.utils.CacheKeyConstants;
 import com.jd.bluedragon.configuration.ucc.UccPropertyConfiguration;
 import com.jd.bluedragon.core.base.BaseMajorManager;
@@ -19,6 +19,9 @@ import com.jd.bluedragon.distribution.box.service.BoxService;
 import com.jd.bluedragon.distribution.businessIntercept.enums.BusinessInterceptOnlineStatusEnum;
 import com.jd.bluedragon.distribution.client.domain.PdaOperateRequest;
 import com.jd.bluedragon.distribution.jsf.domain.BoardCombinationJsfResponse;
+import com.jd.bluedragon.distribution.seal.service.NewSealVehicleService;
+import com.jd.bluedragon.distribution.send.domain.SendM;
+import com.jd.bluedragon.distribution.send.service.SendMService;
 import com.jd.bluedragon.distribution.task.domain.Task;
 import com.jd.bluedragon.distribution.task.service.TaskService;
 import com.jd.bluedragon.distribution.ver.service.SortingCheckService;
@@ -86,6 +89,12 @@ public class VirtualBoardServiceImpl implements VirtualBoardService {
 
     @Autowired
     private UccPropertyConfiguration uccPropertyConfiguration;
+
+    @Autowired
+    private SendMService sendMService;
+
+    @Autowired
+    private NewSealVehicleService newSealVehicleService;
 
     @Autowired
     private TaskService taskService;
@@ -367,6 +376,12 @@ public class VirtualBoardServiceImpl implements VirtualBoardService {
                     }
                     destinationId = boxExist.getReceiveSiteCode();
                 }
+                // 已在同场地发货，不可再组板
+                final SendM recentSendMByParam = getRecentSendMByParam(bindToVirtualBoardPo.getBarCode(), operatorInfo.getSiteCode(), null, null);
+                if (recentSendMByParam != null) {
+                    result.toFail("该包裹已发货");
+                    return result;
+                }
                 // 调板号服务绑定到板号
                 bindToVirtualBoardPo.setMaxItemCount(uccPropertyConfiguration.getVirtualBoardMaxItemCount());
                 final com.jd.transboard.api.dto.BindToVirtualBoardPo convertToTcParam = this.getConvertToTcParam(bindToVirtualBoardPo);
@@ -401,6 +416,33 @@ public class VirtualBoardServiceImpl implements VirtualBoardService {
         } finally {
         }
         return result;
+    }
+
+    /**
+     * 获取最近一次的发货信息
+     *
+     * @param boxCode
+     * @param createSiteCode
+     * @param receiveSiteCode
+     * @return
+     */
+    private SendM getRecentSendMByParam(String boxCode, Integer createSiteCode, Integer receiveSiteCode, Date operateTime) {
+        //查询箱子发货记录
+        /* 不直接使用domain的原因，SELECT语句有[test="createUserId!=null"]等其它 */
+        SendM queryPara = new SendM();
+        queryPara.setBoxCode(boxCode);
+        queryPara.setCreateSiteCode(createSiteCode);
+        if (receiveSiteCode != null) {
+            queryPara.setReceiveSiteCode(receiveSiteCode);
+        }
+        if (operateTime != null){
+            queryPara.setUpdateTime(operateTime);
+        }
+        List<SendM> sendMList = sendMService.findByParams(queryPara);
+        if (null != sendMList && sendMList.size() > 0) {
+            return sendMList.get(0);
+        }
+        return null;
     }
 
     private com.jd.transboard.api.dto.BindToVirtualBoardPo getConvertToTcParam(BindToVirtualBoardPo bindToVirtualBoardPo) {
@@ -654,6 +696,16 @@ public class VirtualBoardServiceImpl implements VirtualBoardService {
                 result.setMessage(baseCheckResult.getMessage());
                 return result;
             }
+            final OperatorInfo operatorInfo = unbindToVirtualBoardPo.getOperatorInfo();
+            // 校验是否已封车，已封车不可取消
+            final SendM recentSendM = getRecentSendMByParam(unbindToVirtualBoardPo.getBarCode(), operatorInfo.getSiteCode(), null, null);
+            if (recentSendM != null) {
+                if (newSealVehicleService.checkSendCodeIsSealed(recentSendM.getSendCode())) {
+                    result.toFail("该箱/包裹已封车，不可取消组板");
+                    return result;
+                }
+            }
+
             final Response<com.jd.transboard.api.dto.UnbindVirtualBoardResultDto> handleResult = virtualBoardJsfManager.unbindToBoard(this.getConvertToTcParam(unbindToVirtualBoardPo));
             if(!Objects.equals(handleResult.getCode(), ResponseEnum.SUCCESS.getIndex())){
                 log.error("VirtualBoardServiceImpl.unbindToBoard--fail-- param {} result {}", JsonHelper.toJson(unbindToVirtualBoardPo), JsonHelper.toJson(handleResult));
