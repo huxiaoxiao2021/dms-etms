8ae794abfb2d42306bcdba0430d48d591528951e
wuyoude <wuyoude@jd.com>
2021-11-13 08:26:25 +0800

    枚举类型修改

--- bluedragon-distribution-sdk/src/main/java/com/jd/bluedragon/distribution/discardedPackageStorageTemp/enums/WasteOperateType.java
+++ bluedragon-distribution-sdk/src/main/java/com/jd/bluedragon/distribution/discardedPackageStorageTemp/enums/WasteOperateType.java
@@ -1,4 +1,7 @@
-package com.jd.bluedragon.dms.utils;
+package com.jd.bluedragon.distribution.discardedPackageStorageTemp.enums;
+
+import java.util.List;
+import java.util.Map;
 
 /**
  * 弃件操作类型
@@ -11,12 +14,19 @@ public enum WasteOperateType {
 
     private Integer code;
     private String name;
-
+    
     WasteOperateType(Integer code, String name) {
         this.code = code;
         this.name = name;
     }
-
+    public static String getNameByCode(Integer code) {
+		for(WasteOperateType enumData:WasteOperateType.values()) {
+			if(enumData.getCode().equals(code)) {
+				return enumData.getName();
+			}
+		}
+        return "";
+    }
     public Integer getCode() {
         return code;
     }
