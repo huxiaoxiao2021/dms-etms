f87155b3626ba1bdc9acf4b2f50c6f6fdd4af2f8
liuduo8 <liuduo3@jd.com>
2021-11-29 16:37:26 +0800

    换单理赔话术空指针修复

--- bluedragon-distribution-core/src/main/java/com/jd/bluedragon/distribution/print/waybill/handler/ReverseChangeInterceptHandler.java
+++ bluedragon-distribution-core/src/main/java/com/jd/bluedragon/distribution/print/waybill/handler/ReverseChangeInterceptHandler.java
@@ -86,7 +86,7 @@ public class ReverseChangeInterceptHandler implements InterceptHandler<WaybillPr
 
         /* 获取新单的信息 */
         InvokeResult<RepeatPrint> newWaybillResult = reversePrintService.getNewWaybillCode1(oldWaybillCode, true);
-        if (newWaybillResult.getData() != null && newWaybillResult.getData().getIsLPFlag()){
+        if (null != newWaybillResult && newWaybillResult.getData() != null && newWaybillResult.getData().getIsLPFlag()){
             LOGGER.warn("ReverseChangeInterceptHandler.handle-->理赔中{}",oldWaybillCode);
             result.toError(JdResponse.CODE_REVERSE_CHANGE_PRINT_WAYBILL_LP,
                     JdResponse.MESSAGE_REVERSE_CHANGE_PRINT_WAYBILL_LP);
