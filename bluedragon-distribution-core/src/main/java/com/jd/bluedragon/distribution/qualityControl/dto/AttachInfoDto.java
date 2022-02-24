8e371a46b0444b959f5c08fb8bc36e5a34cbb18b
fanggang7 <fanggang7@jd.com>
2022-02-24 21:47:07 +0800

    feat: 对接质控系统 mq消费 fix

--- bluedragon-distribution-core/src/main/java/com/jd/bluedragon/distribution/qualityControl/dto/AttachInfoDto.java
+++ bluedragon-distribution-core/src/main/java/com/jd/bluedragon/distribution/qualityControl/dto/AttachInfoDto.java
@@ -1,5 +1,7 @@
 package com.jd.bluedragon.distribution.qualityControl.dto;
 
+import java.io.Serializable;
+
 /**
  * 附件信息
  *
@@ -7,7 +9,9 @@ package com.jd.bluedragon.distribution.qualityControl.dto;
  * @copyright jd.com 京东物流JDL
  * @time 2022-02-18 10:25:19 周五
  */
-public class AttachInfoDto {
+public class AttachInfoDto implements Serializable {
+
+    private static final long serialVersionUID = -394822698294035461L;
 
     /**
      * 名称：
@@ -32,4 +36,37 @@ public class AttachInfoDto {
      * character：文本框（电报号，车牌号等）
      */
     private String attachType;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public String getAttachType() {
+        return attachType;
+    }
+
+    public void setAttachType(String attachType) {
+        this.attachType = attachType;
+    }
+
+    @Override
+    public String toString() {
+        return "AttachInfoDto{" +
+                "name='" + name + '\'' +
+                ", value='" + value + '\'' +
+                ", attachType='" + attachType + '\'' +
+                '}';
+    }
 }
