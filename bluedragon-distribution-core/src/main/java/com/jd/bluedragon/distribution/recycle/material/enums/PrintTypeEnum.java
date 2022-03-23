8f3e89c61290774e3b9e51223ff26cf5b3cbecab
jinjingcheng <jinjingcheng@jd.com>
2022-03-23 21:09:10 +0800

    周转筐打印

---
+++ bluedragon-distribution-core/src/main/java/com/jd/bluedragon/distribution/recycle/material/enums/PrintTypeEnum.java
@@ -0,0 +1,29 @@
+package com.jd.bluedragon.distribution.recycle.material.enums;
+
+public enum PrintTypeEnum {
+    PRINT(1,"打印"),REPRINT(2,"补打印");
+    private int code;
+
+    private String name;
+
+    PrintTypeEnum(int code, String name) {
+        this.code = code;
+        this.name = name;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
