adb2374573c6d5ee9ba8a6d03cba12fa89fd5bb0
wangyinhui3 <wangyinhui3@jd.com>
2022-03-25 00:41:30 +0800

    修改解封车任务hystrix配置

--- bluedragon-distribution-core/src/main/java/com/jd/bluedragon/distribution/seal/hystrix/HystrixPropertiesAutoInjection.java
+++ bluedragon-distribution-core/src/main/java/com/jd/bluedragon/distribution/seal/hystrix/HystrixPropertiesAutoInjection.java
@@ -11,6 +11,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -24,25 +25,53 @@ public class HystrixPropertiesAutoInjection implements PolledConfigurationSource
 
     private static final Logger log = LoggerFactory.getLogger(HystrixPropertiesAutoInjection.class);
 
+    private static final String DEFAULT_PROPS_KEY = "defaultProperties";
+    private static final String COMMAND_PROPS_KEY = "commandProperties";
+    private static final String THREAD_POOL_PROPS_KEY = "threadPoolProperties";
+
+
+    private static final Map<String, Object> DEFAULT_PROPS = new HashMap<>();
+
+    static {
+        DEFAULT_PROPS.put("hystrix.command.default.circuitBreaker.enabled", false); //关闭断路由器
+        DEFAULT_PROPS.put("hystrix.command.default.execution.timeout.enabled", false); //关闭超时降级开关
+        DEFAULT_PROPS.put("hystrix.command.default.fallback.enabled", false); //关闭回退开关
+    }
+
     @Autowired
     private UccPropertyConfiguration uccConfig;
 
     @Override
     public PollResult poll(boolean initial, Object checkPoint) throws Exception {
-        Map<String, Object> propMap = null;
+        Map<String, Object> allPropMap;
+
         String hystrixProps = uccConfig.getSealTaskHystrixProps();
         if (StringUtils.isNotBlank(hystrixProps)) {
-            propMap = JsonHelper.fromJson(hystrixProps, Map.class);
-            if (MapUtils.isEmpty(propMap)) {
-                log.error("从UCC获取动态解封车降级配置为空.");
+            allPropMap = JsonHelper.fromJson(hystrixProps, Map.class);
+            Map<String, Object> defaultPropMap = (Map<String, Object>) allPropMap.get(DEFAULT_PROPS_KEY);
+            if (MapUtils.isNotEmpty(defaultPropMap)) {
+                allPropMap.putAll(defaultPropMap);
+            }
+            Map<String, Object> cmdPropMap = (Map<String, Object>) allPropMap.get(COMMAND_PROPS_KEY);
+            if (MapUtils.isNotEmpty(cmdPropMap)) {
+                allPropMap.putAll(cmdPropMap);
+            }
+            Map<String, Object> threadPropMap = (Map<String, Object>) allPropMap.get(THREAD_POOL_PROPS_KEY);
+            if (MapUtils.isNotEmpty(threadPropMap)) {
+                allPropMap.putAll(threadPropMap);
             }
         }
+        else {
+            log.error("从UCC获取动态解封车降级配置为空.");
+            allPropMap = DEFAULT_PROPS;
+        }
 
-        PollResult pollResult = PollResult.createFull(propMap);
+        PollResult pollResult = PollResult.createFull(allPropMap);
 
         if (log.isInfoEnabled()) {
             log.info("拉取解封车降级配置:{}", JsonHelper.toJson(pollResult));
         }
+
         return pollResult;
     }
 }
