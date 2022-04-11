e9c67e7848906d9e4bd5f81a07bd7364059f23bc
liuduo8 <liuduo3@jd.com>
2022-04-11 21:01:29 +0800

    feat:tms_seal_car_status 单测

--- bluedragon-distribution-worker/src/test/java/ld/TmsConsumerTest.java
+++ bluedragon-distribution-worker/src/test/java/ld/TmsConsumerTest.java
@@ -35,10 +35,14 @@ public class TmsConsumerTest {
 
     @Test
     public void testTmsMq(){
-        String tmsSealCarStatusJson = "{\"sealCarCode\":\"SC22040419643118\",\"status\":10,\"operateUserCode\":\"chenyifei6\",\"operateUserName\":\"陈毅飞\",\"operateTime\":\"2022-04-04 19:10:54\",\"sealCarType\":30,\"batchCodes\":[\"R202204041106221342\",\"R1510931730171867136\",\"R202204041145461342\"],\"transBookCode\":\"TB22040430009175\",\"volume\":null,\"weight\":null,\"transWay\":2,\"vehicleNumber\":\"京AAJ7385\",\"operateSiteId\":1342,\"operateSiteCode\":\"010Y059\",\"operateSiteName\":\"北京上庄营业部\",\"warehouseCode\":null,\"largeCargoDetails\":null,\"pieceCount\":null,\"source\":2,\"sealCarInArea\":null}\n";
+        //封车
+        String tmsSealCarStatusJson = "{\"sealCarCode\":\"SC22041100014898\",\"status\":10,\"operateUserCode\":\"bjxings\",\"operateUserName\":\"邢松\",\"operateTime\":\"2022-04-11 15:57:41\",\"sealCarType\":10,\"batchCodes\":[\"910-39-01595202205127129\"],\"transBookCode\":null,\"volume\":22.3112,\"weight\":343.22,\"transWay\":3,\"vehicleNumber\":\"京AB0D87\",\"operateSiteId\":910,\"operateSiteCode\":\"010F002\",\"operateSiteName\":\"北京马驹桥分拣中心6\",\"warehouseCode\":null,\"largeCargoDetails\":null,\"pieceCount\":null,\"source\":1}";
+        //解封车
+        String tmsSealCarStatusJson2 = "{\"sealCarCode\":\"SC22041100014898\",\"status\":20,\"operateUserCode\":\"bjxings\",\"operateUserName\":\"邢松\",\"operateTime\":\"2022-04-11 15:57:41\",\"sealCarType\":10,\"batchCodes\":[\"910-39-01595202205127129\"],\"transBookCode\":null,\"volume\":22.3112,\"weight\":343.22,\"transWay\":3,\"vehicleNumber\":\"京AB0D87\",\"operateSiteId\":910,\"operateSiteCode\":\"010F002\",\"operateSiteName\":\"北京马驹桥分拣中心6\",\"warehouseCode\":null,\"largeCargoDetails\":null,\"pieceCount\":null,\"source\":1}";
+
 
         Message message = new Message();
-        message.setText(tmsSealCarStatusJson);
+        message.setText(tmsSealCarStatusJson2);
         try {
             tmsSealCarStatusConsumer.consume(message);
         } catch (Exception e) {
