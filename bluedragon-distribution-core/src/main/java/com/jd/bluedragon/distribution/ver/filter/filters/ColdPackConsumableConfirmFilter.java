a2c1a14a830421b2332b13b18a950d518ef3414e
caozhixing3 <caozhixing3@jd.com>
2022-04-12 10:32:59 +0800

    feat(医药包装耗材): 拦截码

--- bluedragon-distribution-core/src/main/java/com/jd/bluedragon/distribution/ver/filter/filters/ColdPackConsumableConfirmFilter.java
+++ bluedragon-distribution-core/src/main/java/com/jd/bluedragon/distribution/ver/filter/filters/ColdPackConsumableConfirmFilter.java
@@ -58,7 +58,7 @@ public class ColdPackConsumableConfirmFilter implements Filter {
                 Boolean needConfirmed = coldChainReverseManager.checkIsNeedConfirmed(request.getWaybillCode());
                 if (needConfirmed) {
                     //强制拦截
-                    throw new SortingCheckException(SortingResponse.CODE_29120,
+                    throw new SortingCheckException(SortingResponse.CODE_29322,
                             HintService.getHintWithFuncModule(HintCodeConstants.PACKING_CONSUMABLE_CONFIRM, request.getFuncModule()));
                 }
             }
