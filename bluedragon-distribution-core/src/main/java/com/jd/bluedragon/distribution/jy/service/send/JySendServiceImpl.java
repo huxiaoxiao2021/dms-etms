4d512018eaa9a30844ebe749a0c4a10fcfa9aec6
weixiaofeng12 <weixiaofeng12@jd.com>
2022-06-29 10:16:50 +0800

    任务迁移更新发货明细模型里面的任务批次关系

--- bluedragon-distribution-core/src/main/java/com/jd/bluedragon/distribution/jy/service/send/JySendServiceImpl.java
+++ bluedragon-distribution-core/src/main/java/com/jd/bluedragon/distribution/jy/service/send/JySendServiceImpl.java
@@ -2,9 +2,15 @@ package com.jd.bluedragon.distribution.jy.service.send;
 
 import com.jd.bluedragon.Constants;
 import com.jd.bluedragon.distribution.jy.dao.send.JySendDao;
+import com.jd.bluedragon.distribution.jy.dto.send.TransferDto;
+import com.jd.bluedragon.distribution.jy.dto.send.VehicleSendRelationDto;
 import com.jd.bluedragon.distribution.jy.send.JySendEntity;
+import com.jd.bluedragon.utils.BeanUtils;
+import com.jd.bluedragon.utils.ObjectHelper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
 
 /**
  * @ClassName JySendServiceImpl
@@ -40,5 +46,24 @@ public class JySendServiceImpl implements IJySendService{
     @Override
     public JySendEntity findByBizId(JySendEntity entity) {
         return sendDao.findByBizId(entity);
+    }
+
+    @Override
+    public int updateTransferProperBySendCode(VehicleSendRelationDto dto) {
+        Integer rs =0;
+        JySendEntity entity = new JySendEntity();
+        entity.setSendVehicleBizId(dto.getToSendVehicleBizId());
+        entity.setCreateSiteId(dto.getCreateSiteId());
+        entity.setUpdateUserErp(dto.getUpdateUserErp());
+        entity.setUpdateUserName(dto.getUpdateUserName());
+        entity.setUpdateTime(new Date());
+        if (ObjectHelper.isNotNull(dto.getNewSendCode())){
+            entity.setNewSendCode(dto.getNewSendCode());
+        }
+        for (String sendCode:dto.getSendCodes()){
+            entity.setSendCode(sendCode);
+            rs =rs+sendDao.updateTransferProperBySendCode(entity);
+        }
+        return rs;
     }
 }
