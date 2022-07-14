792ad320d4b455b18676b13e7f91037eaf444c3d
weixiaofeng12 <weixiaofeng12@jd.com>
2022-07-14 16:20:16 +0800

    合并分支

--- bluedragon-distribution-sdk/src/main/java/com/jd/bluedragon/distribution/base/domain/InvokeResult.java
+++ bluedragon-distribution-sdk/src/main/java/com/jd/bluedragon/distribution/base/domain/InvokeResult.java
@@ -59,6 +59,9 @@ public class InvokeResult<T> implements Serializable {
     public static final int RESULT_NO_GROUP_CODE = 307;
     public static final String RESULT_NO_GROUP_MESSAGE = "查询同组箱号异常";
 
+    public static final int RESULT_CONFIRM_CODE = 310;
+    public static final String RESULT_CONFIRM_MESSAGE = "调度站点无滑道信息，是否继续操作?";
+
 
     public static final int RESULT_EXPORT_CODE = 308;
     public static final String RESULT_EXPORT_MESSAGE = "导出执行异常";
