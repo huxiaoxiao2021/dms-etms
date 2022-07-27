f3aa231ba915e55ed1d455ff650a3205bd40683d
jinjingcheng <jinjingcheng@jd.com>
2022-07-27 15:27:38 +0800

    解绑集包袋和箱号关系

--- bluedragon-distribution-web/src/main/java/com/jd/bluedragon/distribution/cycleBox/service/CycleBoxJsfServiceImpl.java
+++ bluedragon-distribution-web/src/main/java/com/jd/bluedragon/distribution/cycleBox/service/CycleBoxJsfServiceImpl.java
@@ -4,6 +4,7 @@ import com.jd.bluedragon.Constants;
 import com.jd.bluedragon.distribution.cycleBox.domain.BoxMaterialRelationDto;
 import com.jd.bluedragon.distribution.cyclebox.domain.BoxMaterialRelation;
 import com.jd.bluedragon.distribution.cyclebox.service.BoxMaterialRelationService;
+import com.jd.bluedragon.distribution.jsf.domain.InvokeResult;
 import com.jd.ump.annotation.JProEnum;
 import com.jd.ump.annotation.JProfiler;
 import org.springframework.beans.BeanUtils;
@@ -27,5 +28,11 @@ public class CycleBoxJsfServiceImpl implements CycleBoxJsfService{
         BeanUtils.copyProperties(boxMaterialRelation,result);
         result.setOperatorERP(boxMaterialRelation.getOperatorErp());
         return result;
+    }
+
+    @Override
+    @JProfiler(jAppName = Constants.UMP_APP_NAME_DMSWEB, jKey = "DMS.WEB.CycleBoxJsfServiceImpl.unBindBoxMaterialRelation", mState = JProEnum.TP)
+    public InvokeResult<Boolean> unBindBoxMaterialRelation(String boxCode, String materialCode, Integer createSiteCode) {
+        return null;
     }
 }
