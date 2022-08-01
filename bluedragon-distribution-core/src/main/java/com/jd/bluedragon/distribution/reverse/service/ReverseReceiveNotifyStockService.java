d2ef876ef41ed23ffe84b377ef9f6c5d3dfa5270
xumigen <xumigen@jd.com>
2022-08-01 11:18:25 +0800

    供应链中台二期出管 改造5

--- bluedragon-distribution-core/src/main/java/com/jd/bluedragon/distribution/reverse/service/ReverseReceiveNotifyStockService.java
+++ bluedragon-distribution-core/src/main/java/com/jd/bluedragon/distribution/reverse/service/ReverseReceiveNotifyStockService.java
@@ -692,6 +692,9 @@ public class ReverseReceiveNotifyStockService {
         chuguanDetailVo.setProfitLossId(profitLossId);
         chuguanDetailVo.setDistriOrderIds(distriOrderIds);
         chuguanDetailVo.setPurchaseChannel(dimValue);
+        Map<String, String> paramExtMap = new HashMap<>();
+        paramExtMap.put("skuLevelData","1");
+        chuguanDetailVo.setParamExtMap(paramExtMap);
         return chuguanDetailVo;
     }
 
@@ -716,6 +719,9 @@ public class ReverseReceiveNotifyStockService {
             if(item.getProfitChannelId()!=null){
                 chuguanDetailVo.setProfitLossId(String.valueOf(item.getProfitChannelId()));
             }
+            Map<String, String> paramExtMap = new HashMap<>();
+            paramExtMap.put("skuLevelData","1");
+            chuguanDetailVo.setParamExtMap(paramExtMap);
             chuguanDetailVoList.add(chuguanDetailVo);
         }
         return chuguanDetailVoList;
