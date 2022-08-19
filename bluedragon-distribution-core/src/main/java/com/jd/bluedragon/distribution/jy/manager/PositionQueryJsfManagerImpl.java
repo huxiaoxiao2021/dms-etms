0ec41b3337452e3d5a7ae95e04aa5cb75da63b15
luqinglin3 <luqinglin3@jd.com>
2022-08-19 18:37:37 +0800

    自测

--- bluedragon-distribution-core/src/main/java/com/jd/bluedragon/distribution/jy/manager/PositionQueryJsfManagerImpl.java
+++ bluedragon-distribution-core/src/main/java/com/jd/bluedragon/distribution/jy/manager/PositionQueryJsfManagerImpl.java
@@ -14,12 +14,12 @@ import org.springframework.stereotype.Service;
 @Service
 public class PositionQueryJsfManagerImpl implements PositionQueryJsfManager {
     @Autowired
-    private PositionQueryJsfService positionQueryJsfService;
+    private PositionQueryJsfService positionQueryBasicJsfService;
 
     @Override
     @JProfiler(jAppName = Constants.UMP_APP_NAME_DMSWEB, jKey = "DMSWEB.PositionQueryJsfManagerImpl.queryOneByPositionCode", mState = {JProEnum.TP, JProEnum.FunctionError})
     @Cache(key = "BasicQueryWSManagerImpl.getDictList@args0@args1@args2", memoryEnable = true, memoryExpiredTime = 10 * 60 * 1000, redisEnable = true, redisExpiredTime = 20 * 60 * 1000)
     public Result<PositionDetailRecord> queryOneByPositionCode(String positionCode) {
-        return positionQueryJsfService.queryOneByPositionCode(positionCode);
+        return positionQueryBasicJsfService.queryOneByPositionCode(positionCode);
     }
 }
