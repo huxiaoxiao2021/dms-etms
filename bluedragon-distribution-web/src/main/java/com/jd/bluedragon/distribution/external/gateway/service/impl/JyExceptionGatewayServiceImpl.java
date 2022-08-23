e8607f9c5fd00623919cf1f29dbc591c0e748ec4
magang33 <magang33@jd.com>
2022-08-23 11:32:01 +0800

    异常岗-网关调用

--- bluedragon-distribution-web/src/main/java/com/jd/bluedragon/distribution/external/gateway/service/impl/JyExceptionGatewayServiceImpl.java
+++ bluedragon-distribution-web/src/main/java/com/jd/bluedragon/distribution/external/gateway/service/impl/JyExceptionGatewayServiceImpl.java
@@ -3,41 +3,48 @@ package com.jd.bluedragon.distribution.external.gateway.service.impl;
 import com.jd.bluedragon.common.dto.base.response.JdCResponse;
 import com.jd.bluedragon.common.dto.jyexpection.request.*;
 import com.jd.bluedragon.common.dto.jyexpection.response.*;
-import com.jd.bluedragon.common.dto.operation.workbench.enums.JyExpStatusEnum;
+import com.jd.bluedragon.distribution.jy.service.exception.JyExceptionService;
 import com.jd.bluedragon.external.gateway.service.JyExceptionGatewayService;
-import com.jd.fastjson.JSON;
+import org.springframework.beans.factory.annotation.Autowired;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 
 public class JyExceptionGatewayServiceImpl implements JyExceptionGatewayService {
+    @Autowired
+    private JyExceptionService jyExceptionService;
+
+
+    /**
+     * 通用异常上报入口-扫描
+     *
+     * @param req
+     */
     @Override
     public JdCResponse<Object> uploadScan(ExpUploadScanReq req) {
 
-        return JdCResponse.ok();
+        return jyExceptionService.uploadScan(req);
     }
 
+    /**
+     * 按取件状态统计
+     *
+     * @param req
+     */
     @Override
     public JdCResponse<List<StatisticsByStatusDto>> statisticsByStatus(ExpBaseReq req) {
 
-        List<StatisticsByStatusDto> list = new ArrayList<>();
-        for (JyExpStatusEnum value : JyExpStatusEnum.values()) {
-            StatisticsByStatusDto dto = new StatisticsByStatusDto();
-            dto.setStatus(value.getCode());
-            dto.setCount(new Random().nextInt(100));
-            list.add(dto);
-        }
-
-        return JdCResponse.ok(list);
+        return jyExceptionService.statisticsByStatus(req);
     }
 
+    /**
+     * 网格待取件列表统计接口
+     *
+     * @param req
+     */
     @Override
     public JdCResponse<List<StatisticsByGridDto>> getGridStatisticsPageList(StatisticsByGridReq req) {
-        String json = "[{\"gridNo\":\"10\",\"areaCode\":\"LDPSFQ-001\",\"areaName\":\"落地配送分区\",\"tags\":[{\"code\":3,\"name\":\"三无\",\"style\":\"\"},{\"code\":0,\"name\":\"客户改址\",\"style\":\"\"}],\"pendingNum\":1231,\"timeoutNum\":1231},{\"gridNo\":\"10\",\"areaName\":\"落地配送分区\",\"tags\":[{\"code\":3,\"name\":\"三无\",\"style\":\"\"},{\"code\":0,\"name\":\"客户改址\",\"style\":\"\"}],\"pendingNum\":1231,\"timeoutNum\":1231}]";
-        List<StatisticsByGridDto> list = JSON.parseArray(json, StatisticsByGridDto.class);
 
-        return JdCResponse.ok(list);
+        return jyExceptionService.getGridStatisticsPageList(req);
     }
 
     /**
@@ -47,7 +54,8 @@ public class JyExceptionGatewayServiceImpl implements JyExceptionGatewayService 
      */
     @Override
     public JdCResponse<List<ProcessingNumByGridDto>> getReceivingCount(StatisticsByGridReq req) {
-        return null;
+
+        return jyExceptionService.getReceivingCount(req);
     }
 
     /**
@@ -57,7 +65,8 @@ public class JyExceptionGatewayServiceImpl implements JyExceptionGatewayService 
      */
     @Override
     public JdCResponse<List<ExpTaskDto>> getExceptionTaskPageList(ExpTaskPageReq req) {
-        return null;
+
+        return jyExceptionService.getExceptionTaskPageList(req);
     }
 
     /**
@@ -67,7 +76,8 @@ public class JyExceptionGatewayServiceImpl implements JyExceptionGatewayService 
      */
     @Override
     public JdCResponse<Object> receive(ExpReceiveReq req) {
-        return null;
+
+        return jyExceptionService.receive(req);
     }
 
     /**
@@ -77,7 +87,8 @@ public class JyExceptionGatewayServiceImpl implements JyExceptionGatewayService 
      */
     @Override
     public JdCResponse<ExpTaskDetailDto> getTaskDetail(ExpTaskByIdReq req) {
-        return null;
+
+        return jyExceptionService.getTaskDetail(req);
     }
 
     /**
@@ -87,7 +98,8 @@ public class JyExceptionGatewayServiceImpl implements JyExceptionGatewayService 
      */
     @Override
     public JdCResponse<Object> processTask(ExpTaskDetailReq req) {
-        return null;
+
+        return jyExceptionService.processTask(req);
     }
 
 }
