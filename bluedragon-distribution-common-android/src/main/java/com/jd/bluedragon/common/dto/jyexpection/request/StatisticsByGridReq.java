e4143cf5357ad2c48a1145163bf49f553512f113
magang33 <magang33@jd.com>
2022-08-24 16:42:58 +0800

    三无异常岗:升级jar包

--- bluedragon-distribution-common-android/src/main/java/com/jd/bluedragon/common/dto/jyexpection/request/StatisticsByGridReq.java
+++ bluedragon-distribution-common-android/src/main/java/com/jd/bluedragon/common/dto/jyexpection/request/StatisticsByGridReq.java
@@ -48,7 +48,17 @@ public class StatisticsByGridReq extends ExpBaseReq {
     }
 
     public Integer getOffSet() {
+        if (pageNumber == null || pageSize == null) {
+            return 0;
+        }
         return (pageNumber - 1) * pageSize;
     }
 
+    public void setOffSet(Integer offSet) {
+        if (pageNumber == null || pageSize == null) {
+            this.offSet = 0;
+        }else {
+            this.offSet = (pageNumber - 1) * pageSize;
+        }
+    }
 }
