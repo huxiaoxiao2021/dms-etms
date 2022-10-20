b11ade7e223842a1b2f1357b2c4ae7de54196fcd
magang33 <magang33@jd.com>
2022-10-20 10:40:22 +0800

    三无异常岗：添加ump

--- bluedragon-distribution-web/src/main/java/com/jd/bluedragon/distribution/external/gateway/service/impl/JyExceptionGatewayServiceImpl.java
+++ bluedragon-distribution-web/src/main/java/com/jd/bluedragon/distribution/external/gateway/service/impl/JyExceptionGatewayServiceImpl.java
@@ -2,12 +2,15 @@ package com.jd.bluedragon.distribution.external.gateway.service.impl;
 
 import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
+import com.jd.bluedragon.Constants;
 import com.jd.bluedragon.common.dto.base.response.JdCResponse;
 import com.jd.bluedragon.common.dto.jyexpection.request.*;
 import com.jd.bluedragon.common.dto.jyexpection.response.*;
 import com.jd.bluedragon.distribution.barcode.service.BarcodeService;
 import com.jd.bluedragon.distribution.jy.service.exception.JyExceptionService;
 import com.jd.bluedragon.external.gateway.service.JyExceptionGatewayService;
+import com.jd.ump.annotation.JProEnum;
+import com.jd.ump.annotation.JProfiler;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,6 +32,7 @@ public class JyExceptionGatewayServiceImpl implements JyExceptionGatewayService 
      * @param req
      */
     @Override
+    @JProfiler(jAppName = Constants.UMP_APP_NAME_DMSWEB,jKey = "DMS.BASE.JyExceptionGatewayServiceImpl.uploadScan", mState = {JProEnum.TP})
     public JdCResponse<Object> uploadScan(ExpUploadScanReq req) {
 
         return jyExceptionService.uploadScan(req);
@@ -40,6 +44,7 @@ public class JyExceptionGatewayServiceImpl implements JyExceptionGatewayService 
      * @param req
      */
     @Override
+    @JProfiler(jAppName = Constants.UMP_APP_NAME_DMSWEB,jKey = "DMS.BASE.JyExceptionGatewayServiceImpl.statisticsByStatus", mState = {JProEnum.TP})
     public JdCResponse<List<StatisticsByStatusDto>> statisticsByStatus(ExpBaseReq req) {
 
         return jyExceptionService.statisticsByStatus(req);
@@ -51,6 +56,7 @@ public class JyExceptionGatewayServiceImpl implements JyExceptionGatewayService 
      * @param req
      */
     @Override
+    @JProfiler(jAppName = Constants.UMP_APP_NAME_DMSWEB,jKey = "DMS.BASE.JyExceptionGatewayServiceImpl.getGridStatisticsPageList", mState = {JProEnum.TP})
     public JdCResponse<List<StatisticsByGridDto>> getGridStatisticsPageList(StatisticsByGridReq req) {
 
         return jyExceptionService.getGridStatisticsPageList(req);
@@ -62,6 +68,7 @@ public class JyExceptionGatewayServiceImpl implements JyExceptionGatewayService 
      * @param req
      */
     @Override
+    @JProfiler(jAppName = Constants.UMP_APP_NAME_DMSWEB,jKey = "DMS.BASE.JyExceptionGatewayServiceImpl.getReceivingCount", mState = {JProEnum.TP})
     public JdCResponse<List<ProcessingNumByGridDto>> getReceivingCount(StatisticsByGridReq req) {
 
         return jyExceptionService.getReceivingCount(req);
@@ -73,6 +80,7 @@ public class JyExceptionGatewayServiceImpl implements JyExceptionGatewayService 
      * @param req
      */
     @Override
+    @JProfiler(jAppName = Constants.UMP_APP_NAME_DMSWEB,jKey = "DMS.BASE.JyExceptionGatewayServiceImpl.releaseReceivingCount", mState = {JProEnum.TP})
     public JdCResponse<Object> releaseReceivingCount(ExpTaskPageReq req) {
         return jyExceptionService.releaseReceivingCount(req);
     }
@@ -83,6 +91,7 @@ public class JyExceptionGatewayServiceImpl implements JyExceptionGatewayService 
      * @param req
      */
     @Override
+    @JProfiler(jAppName = Constants.UMP_APP_NAME_DMSWEB,jKey = "DMS.BASE.JyExceptionGatewayServiceImpl.getExceptionTaskPageList", mState = {JProEnum.TP})
     public JdCResponse<List<ExpTaskDto>> getExceptionTaskPageList(ExpTaskPageReq req) {
 
         return jyExceptionService.getExceptionTaskPageList(req);
@@ -94,6 +103,7 @@ public class JyExceptionGatewayServiceImpl implements JyExceptionGatewayService 
      * @param req
      */
     @Override
+    @JProfiler(jAppName = Constants.UMP_APP_NAME_DMSWEB,jKey = "DMS.BASE.JyExceptionGatewayServiceImpl.receive", mState = {JProEnum.TP})
     public JdCResponse<Object> receive(ExpReceiveReq req) {
 
         return jyExceptionService.receive(req);
@@ -104,6 +114,7 @@ public class JyExceptionGatewayServiceImpl implements JyExceptionGatewayService 
      *
      */
     @Override
+    @JProfiler(jAppName = Constants.UMP_APP_NAME_DMSWEB,jKey = "DMS.BASE.JyExceptionGatewayServiceImpl.queryByBarcode", mState = {JProEnum.TP})
     public JdCResponse<ExpTaskDto> queryByBarcode(ExpReceiveReq req) {
         return jyExceptionService.queryByBarcode(req.getBarCode());
     }
@@ -114,6 +125,7 @@ public class JyExceptionGatewayServiceImpl implements JyExceptionGatewayService 
      * @param req
      */
     @Override
+    @JProfiler(jAppName = Constants.UMP_APP_NAME_DMSWEB,jKey = "DMS.BASE.JyExceptionGatewayServiceImpl.getTaskDetail", mState = {JProEnum.TP})
     public JdCResponse<ExpTaskDetailDto> getTaskDetail(ExpTaskByIdReq req) {
 
         return jyExceptionService.getTaskDetail(req);
@@ -125,12 +137,14 @@ public class JyExceptionGatewayServiceImpl implements JyExceptionGatewayService 
      * @param req
      */
     @Override
+    @JProfiler(jAppName = Constants.UMP_APP_NAME_DMSWEB,jKey = "DMS.BASE.JyExceptionGatewayServiceImpl.processTask", mState = {JProEnum.TP})
     public JdCResponse<Object> processTask(ExpTaskDetailReq req) {
 
         return jyExceptionService.processTask(req);
     }
 
     @Override
+    @JProfiler(jAppName = Constants.UMP_APP_NAME_DMSWEB,jKey = "DMS.BASE.JyExceptionGatewayServiceImpl.queryProductName", mState = {JProEnum.TP})
     public JdCResponse<List<DmsBarCode>> queryProductName(String barCode) {
         if (StringUtils.isEmpty(barCode)){
             return JdCResponse.fail("缺少查询参数");
