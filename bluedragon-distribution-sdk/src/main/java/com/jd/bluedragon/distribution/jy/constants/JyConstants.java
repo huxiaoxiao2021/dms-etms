1fa0262aed807baa828606814ffabc4a672cfc9e
fanggang7 <fanggang7@jd.com>
2022-10-24 16:04:59 +0800

    feat: 作业APP 接货仓验货 增加常量

---
+++ bluedragon-distribution-sdk/src/main/java/com/jd/bluedragon/distribution/jy/constants/JyConstants.java
@@ -0,0 +1,16 @@
+package com.jd.bluedragon.distribution.jy.constants;
+
+/**
+ * 作业APP相关常量
+ *
+ * @author fanggang7
+ * @copyright jd.com 京东物流JDL
+ * @time 2022-10-24 15:52:32 周一
+ */
+public class JyConstants {
+
+    /**
+     * 接货仓验货拦截件提示码
+     */
+    public static final int WAREHOUSE_INSPECTION_INTERCEPT_CODE = 5001;
+}
