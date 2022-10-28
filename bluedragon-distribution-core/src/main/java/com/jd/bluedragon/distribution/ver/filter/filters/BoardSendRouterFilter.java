cd0b2c8eebb0275973af2a51e7f3addf56342259
吕远 <lvyuan21@jd.com>
2022-10-28 18:09:28 +0800

    1.feat:新增按板扫描的错发强发提示

--- bluedragon-distribution-core/src/main/java/com/jd/bluedragon/distribution/ver/filter/filters/BoardSendRouterFilter.java
+++ bluedragon-distribution-core/src/main/java/com/jd/bluedragon/distribution/ver/filter/filters/BoardSendRouterFilter.java
@@ -6,18 +6,16 @@ import com.jd.bluedragon.core.hint.constants.HintCodeConstants;
 import com.jd.bluedragon.core.hint.service.HintService;
 import com.jd.bluedragon.distribution.api.response.SortingResponse;
 import com.jd.bluedragon.distribution.base.service.SiteService;
+import com.jd.bluedragon.distribution.board.domain.Board;
 import com.jd.bluedragon.distribution.jsf.domain.ValidateIgnore;
 import com.jd.bluedragon.distribution.jsf.domain.ValidateIgnoreRouterCondition;
-import com.jd.bluedragon.distribution.router.RouterService;
-import com.jd.bluedragon.distribution.router.domain.dto.RouteNextDto;
 import com.jd.bluedragon.distribution.rule.domain.Rule;
 import com.jd.bluedragon.distribution.ver.domain.FilterContext;
 import com.jd.bluedragon.distribution.ver.exception.SortingCheckException;
 import com.jd.bluedragon.distribution.ver.filter.Filter;
 import com.jd.bluedragon.distribution.ver.filter.FilterChain;
 import com.jd.bluedragon.utils.BusinessHelper;
-import com.jd.bluedragon.utils.StringHelper;
-import com.jd.bluedragon.utils.WaybillCacheHelper;
+import com.jd.bluedragon.utils.JsonHelper;
 import org.apache.commons.collections.CollectionUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -31,10 +29,9 @@ import java.util.Objects;
 /**
  * Created by xumei3 on 2018/3/21.
  */
-public class RouterFilter implements Filter {
+public class BoardSendRouterFilter implements Filter {
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
-    private static final String WAYBILL_ROUTER_SPLITER = "\\|";
     private static final String RULE_ROUTER = "1122";
     private static final String SWITCH_ON = "1";
 
@@ -42,18 +39,11 @@ public class RouterFilter implements Filter {
     private SiteService siteService;
 
     @Autowired
-    private RouterService routerService;
-    @Autowired
     private UccPropertyConfiguration uccConfiguration;
 
     @Override
     public void doFilter(FilterContext request, FilterChain chain) throws Exception {
 
-        /* 判断如果是填航空仓订单则直接进行返回，不进行下面的下一跳校验 */
-        if (WaybillCacheHelper.isAirWaybill(request.getWaybillCache())) {
-            chain.doFilter(request,chain);
-            return;
-        }
 
         //发货目的地为德邦虚拟分拣中心的不校验
         List<Integer> dpSiteCodeList = uccConfiguration.getDpSiteCodeList();
@@ -72,33 +62,23 @@ public class RouterFilter implements Filter {
 
         //规则没有配，或者配置了内容不等于1才会进行校验
         if (rule == null || !SWITCH_ON.equals(rule.getContent())) {
-            Integer createSiteCode = request.getCreateSiteCode();
             Integer receiveSiteCode = request.getReceiveSiteCode();
-
-            String waybillCode =request.getWaybillCode();
-            if(StringHelper.isEmpty(waybillCode) && request.getWaybillCache()!=null){
-                waybillCode = request.getWaybillCache().getWaybillCode();
-            }
-            RouteNextDto routeNextDto = routerService.matchRouterNextNode(createSiteCode,waybillCode);
-
-            logger.info("RouterFilter根据运单号获取运单路由，运单号[{}]routExistCurrentSite[{}]firstNextSiteId[{}]",
-                    waybillCode,routeNextDto.isRoutExistCurrentSite(),routeNextDto.getFirstNextSiteId());
-            if(routeNextDto.isRoutExistCurrentSite() &&
-                    !isRightReceiveSite(receiveSiteCode, routeNextDto)) {
+            Board board = request.getBoard();
+            if(!isRightReceiveSite(receiveSiteCode, board.getDestinationId())) {
                 // 如果存在忽略校验，则继续走下一步
                 final ValidateIgnore validateIgnore = request.getValidateIgnore();
                 if(validateIgnore != null && validateIgnore.getValidateIgnoreRouterCondition() != null){
                     final ValidateIgnoreRouterCondition validateIgnoreCondition = validateIgnore.getValidateIgnoreRouterCondition();
                     final List<Long> receiveSiteIdList = validateIgnoreCondition.getReceiveSiteIdList();
                     if(CollectionUtils.isNotEmpty(validateIgnoreCondition.getReceiveSiteIdList()) && Objects.equals(validateIgnoreCondition.getMatchType(), ValidateIgnore.MATCH_TYPE_IN)){
-                        if(routeNextDto.getFirstNextSiteId() != null && receiveSiteIdList.contains(Long.valueOf(routeNextDto.getFirstNextSiteId()))){
-                            logger.info("RouterFilter validateIgnore: waybillCode: {} firstNextSiteId: {} receiveSiteIdList: {}", waybillCode, routeNextDto.getFirstNextSiteId(), receiveSiteIdList);
+                        if(board.getDestinationId() != null && receiveSiteIdList.contains(Long.valueOf(board.getDestinationId()))){
+                            logger.info("RouterFilter validateIgnore: board: {} receiveSiteIdList: {}", JsonHelper.toJson(request.getBoard()), receiveSiteIdList);
                             chain.doFilter(request, chain);
                             return;
                         }
                     }
                 }
-                String siteName = siteService.getDmsShortNameByCode(routeNextDto.getFirstNextSiteId());
+                String siteName = siteService.getDmsShortNameByCode(board.getDestinationId());
                 Map<String, String> argsMap = new HashMap<>();
                 argsMap.put(HintArgsConstants.ARG_FIRST, siteName);
                 throw new SortingCheckException(SortingResponse.CODE_CROUTER_ERROR,
@@ -110,8 +90,10 @@ public class RouterFilter implements Filter {
         chain.doFilter(request, chain);
     }
 
-    private boolean isRightReceiveSite(Integer receiveSiteCode, RouteNextDto routeNextDto) {
-        return CollectionUtils.isNotEmpty(routeNextDto.getNextSiteIdList())
-                && Objects.equals(routeNextDto.getFirstNextSiteId(),receiveSiteCode);
+    private boolean isRightReceiveSite(Integer receiveSiteCode, Integer boardDestinationId) {
+        return Objects.equals(boardDestinationId, receiveSiteCode);
     }
+
+
+
 }
