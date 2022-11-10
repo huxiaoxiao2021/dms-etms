ca8a9b7fa61f3de313e2deab0a32d78b0cfb6a55
weixiaofeng12 <weixiaofeng12@jd.com>
2022-11-10 16:25:03 +0800

    初始化组板岗持久层基础代码

---
+++ bluedragon-distribution-core/src/main/java/com/jd/bluedragon/external/gateway/service/JyComboardGatewayService.java
@@ -0,0 +1,108 @@
+package com.jd.bluedragon.external.gateway.service;
+
+import com.jd.bluedragon.common.dto.comboard.request.*;
+import com.jd.bluedragon.common.dto.comboard.response.*;
+import com.jd.bluedragon.common.dto.base.response.JdCResponse;
+
+public interface JyComboardGatewayService {
+    /**
+     * 查询场地滑道列表数据
+     */
+    JdCResponse<CrossDataResp> listCrossData(CrossDataReq request);
+
+    /**
+     * 查询场地某个滑道下笼车列表数据
+     */
+    JdCResponse<TableTrolleyResp> listTableTrolleyUnderCross(TableTrolleyReq request);
+
+    /**
+     * 创建本岗位(小组)的常用滑道笼车(CTT:CrossTableTrolley)集合（“混扫01”）
+     */
+    JdCResponse<CreateGroupCTTResp> createGroupCTTData(CreateGroupCTTReq request);
+    /**
+     * 变更（添加或者移除滑道笼车流向）
+     */
+    JdCResponse addCTT2Group(AddCTTReq request);
+    JdCResponse removeCTTFromGroup(RemoveCTTReq request);
+
+    /**
+     * 查询(本岗位或本场地)常用滑道笼车流向集合(【“混扫01”，“混扫02”，“混扫03”】)
+     */
+    JdCResponse<CTTGroupDataResp> listCTTGroupData(CTTGroupDataReq request);
+
+    /**
+     * 查询某个混扫任务下的流向列表数据（包含流向的基础数据和统计相关数据）-支撑下钻1(流向下钻)和混扫切换页面
+     * @param request
+     * @return
+     */
+    JdCResponse<SendFlowDataResp> listSendFlowUnderCTTGroup(SendFlowDataReq request);
+
+    /**
+     *查询流向作业详情(支撑扫描页的流向详情查询)
+     */
+    JdCResponse<SendFlowDetailResp> querySendFlowDetail(SendFlowDetailReq request);
+    /**
+     *查询板作业详情
+     */
+    JdCResponse<BoardResp> queryBoardDetail(BoardReq request);
+    /**
+     * 完结板
+     */
+    JdCResponse finishBoard(BoardReq request);
+
+    /**
+     * 结束混扫任务的所有流向的板
+     */
+    JdCResponse finishBoardsUnderCTTGroup(CTTGroupReq request);
+
+
+    /**
+     * 扫描(组板+发货)
+     */
+    JdCResponse<ComboardScanResp> comboardScan(ComboardScanReq request);
+
+
+    /**
+     *查询流向下组板统计信息列表-支撑下钻2
+     */
+    JdCResponse<BoardStatisticsResp> queryBoardStatisticsUnderSendFlow(BoardStatisticsReq request);
+
+
+    /**
+     * 查询板下的已扫统计信息（已扫包裹、已扫箱子）-支撑下钻4
+     * @param request
+     * @return
+     */
+    JdCResponse<HaveScanStatisticsResp> queryHaveScanStatisticsUnderBoard(HaveScanStatisticsReq request);
+    /**
+     * 查询箱子内部的包裹详情
+     */
+    JdCResponse<PackageDetailResp> listPackageDetailRespUnderBox(BoxQueryReq request);
+    /**
+     * 查询流向待扫统计数据-支撑下钻5
+     */
+    JdCResponse<WaitScanStatisticsResp> queryWaitScanStatisticsUnderSendFlow(WaitScanStatisticsReq request);
+
+    /**
+     * 查询流向下待扫包裹明细
+     * @param request
+     * @return
+     */
+    JdCResponse<PackageDetailResp> listPackageDetailRespUnderSendFlow(SendFlowQueryReq request);
+
+    /**
+     * 查询某个板下的异常扫描统计数据--支撑板异常下钻3和下钻6
+     */
+    JdCResponse<BoardExcepStatisticsResp> queryExcepScanStatisticsUnderBoard(BoardExcepStatisticsReq request);
+
+    /**
+     * 查询“混扫01”多个流向下的异常扫描统计数据-支撑下钻7
+     * @param request
+     * @return
+     */
+    JdCResponse<SendFlowExcepStatisticsResp> queryExcepScanStatisticsUnderCTTGroup(SendFlowExcepStatisticsReq request);
+
+    /**
+     * 取消组板
+     */
+}
---
+++ bluedragon-distribution-web/src/main/java/com/jd/bluedragon/distribution/external/gateway/service/impl/JyComboardGatewayServiceImpl.java
@@ -0,0 +1,107 @@
+package com.jd.bluedragon.distribution.external.gateway.service.impl;
+
+import com.jd.bluedragon.common.UnifiedExceptionProcess;
+import com.jd.bluedragon.common.dto.base.response.JdCResponse;
+import com.jd.bluedragon.common.dto.comboard.request.*;
+import com.jd.bluedragon.common.dto.comboard.response.*;
+import com.jd.bluedragon.external.gateway.service.JyComboardGatewayService;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@UnifiedExceptionProcess
+public class JyComboardGatewayServiceImpl implements JyComboardGatewayService {
+    @Override
+    public JdCResponse<CrossDataResp> listCrossData(CrossDataReq request) {
+        return null;
+    }
+
+    @Override
+    public JdCResponse<TableTrolleyResp> listTableTrolleyUnderCross(TableTrolleyReq request) {
+        return null;
+    }
+
+    @Override
+    public JdCResponse<CreateGroupCTTResp> createGroupCTTData(CreateGroupCTTReq request) {
+        return null;
+    }
+
+    @Override
+    public JdCResponse addCTT2Group(AddCTTReq request) {
+        return null;
+    }
+
+    @Override
+    public JdCResponse removeCTTFromGroup(RemoveCTTReq request) {
+        return null;
+    }
+
+    @Override
+    public JdCResponse<CTTGroupDataResp> listCTTGroupData(CTTGroupDataReq request) {
+        return null;
+    }
+
+    @Override
+    public JdCResponse<SendFlowDataResp> listSendFlowUnderCTTGroup(SendFlowDataReq request) {
+        return null;
+    }
+
+    @Override
+    public JdCResponse<SendFlowDetailResp> querySendFlowDetail(SendFlowDetailReq request) {
+        return null;
+    }
+
+    @Override
+    public JdCResponse<BoardResp> queryBoardDetail(BoardReq request) {
+        return null;
+    }
+
+    @Override
+    public JdCResponse finishBoard(BoardReq request) {
+        return null;
+    }
+
+    @Override
+    public JdCResponse finishBoardsUnderCTTGroup(CTTGroupReq request) {
+        return null;
+    }
+
+    @Override
+    public JdCResponse<ComboardScanResp> comboardScan(ComboardScanReq request) {
+        return null;
+    }
+
+    @Override
+    public JdCResponse<BoardStatisticsResp> queryBoardStatisticsUnderSendFlow(BoardStatisticsReq request) {
+        return null;
+    }
+
+    @Override
+    public JdCResponse<HaveScanStatisticsResp> queryHaveScanStatisticsUnderBoard(HaveScanStatisticsReq request) {
+        return null;
+    }
+
+    @Override
+    public JdCResponse<PackageDetailResp> listPackageDetailRespUnderBox(BoxQueryReq request) {
+        return null;
+    }
+
+    @Override
+    public JdCResponse<WaitScanStatisticsResp> queryWaitScanStatisticsUnderSendFlow(WaitScanStatisticsReq request) {
+        return null;
+    }
+
+    @Override
+    public JdCResponse<PackageDetailResp> listPackageDetailRespUnderSendFlow(SendFlowQueryReq request) {
+        return null;
+    }
+
+    @Override
+    public JdCResponse<BoardExcepStatisticsResp> queryExcepScanStatisticsUnderBoard(BoardExcepStatisticsReq request) {
+        return null;
+    }
+
+    @Override
+    public JdCResponse<SendFlowExcepStatisticsResp> queryExcepScanStatisticsUnderCTTGroup(SendFlowExcepStatisticsReq request) {
+        return null;
+    }
+}
