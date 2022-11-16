107aa6caa465d8a59c0e35fd890745b0880b869f
chenlingfeng10 <chenlingfeng10@jd.com>
2022-11-16 19:36:11 +0800

    评审意见修改：合流车判断类重新封装

--- bluedragon-distribution-core/src/main/java/com/jd/bluedragon/distribution/transport/service/impl/TransportRelatedJSFServiceImpl.java
+++ bluedragon-distribution-core/src/main/java/com/jd/bluedragon/distribution/transport/service/impl/TransportRelatedJSFServiceImpl.java
@@ -1,8 +1,11 @@
 package com.jd.bluedragon.distribution.transport.service.impl;
 
+import com.jd.bluedragon.Constants;
 import com.jd.bluedragon.distribution.base.domain.InvokeResult;
 import com.jd.bluedragon.distribution.transport.service.TransportRelatedJSFService;
 import com.jd.bluedragon.distribution.transport.service.TransportRelatedService;
+import com.jd.ump.annotation.JProEnum;
+import com.jd.ump.annotation.JProfiler;
 import org.springframework.stereotype.Service;
 
 @Service("transportRelatedJSFService")
@@ -11,6 +14,8 @@ public class TransportRelatedJSFServiceImpl implements TransportRelatedJSFServic
     private TransportRelatedService transportRelatedService;
 
     @Override
+    @JProfiler(jAppName = Constants.UMP_APP_NAME_DMSWEB,jKey = "com.jd.bluedragon.distribution.transport.service.impl.TransportRelatedJSFServiceImpl.isMergeCar",
+            mState = {JProEnum.TP, JProEnum.FunctionError})
     public InvokeResult<String> isMergeCar(Integer siteCode, String transWorkCode, String sealCarCode, String simpleCode, String vehicleNumber) {
         InvokeResult<String> invokeResult = new InvokeResult<>();
         String isMergeCar = transportRelatedService.isMergeCar(siteCode, transWorkCode, sealCarCode, simpleCode, vehicleNumber);
