a3343c41f6ca23f4553dd7d28d4174189c537d3a
liangjiaqi6 <liangjiaqi6@jd.com>
2022-11-17 10:29:17 +0800

    货量预测接口

--- bluedragon-distribution-core/src/main/java/com/jd/bluedragon/external/gateway/service/PkgPredictGateWayService.java
+++ bluedragon-distribution-core/src/main/java/com/jd/bluedragon/external/gateway/service/PkgPredictGateWayService.java
@@ -5,6 +5,7 @@ import com.jd.bluedragon.common.dto.base.response.JdCResponse;
 import com.jd.bluedragon.common.dto.predict.request.WorkWaveInspectedNotSendDetailsReq;
 import com.jd.bluedragon.common.dto.predict.request.WorkWaveInspectedNotSendPackageCountReq;
 import com.jd.bluedragon.common.dto.predict.response.WorkWaveInspectedNotSendDetailsResponse;
+import com.jd.bluedragon.common.dto.predict.response.WorkWaveInspectedNotSendPackageCountResponse;
 
 /**
  * 货量预测
@@ -13,9 +14,11 @@ public interface PkgPredictGateWayService {
 
     /**
      * 查询当前波次已验未发的包裹量
+     * 和当前波次产品类型已验未发的包裹量
+     *
      * @return
      */
-    JdCResponse<Integer> queryCurrentWorkWaveInspectedNotSendPackageCount(WorkWaveInspectedNotSendPackageCountReq req);
+    JdCResponse<WorkWaveInspectedNotSendPackageCountResponse> queryCurrentWorkWaveInspectedNotSendPackageCount(WorkWaveInspectedNotSendPackageCountReq req);
 
     /**
      * 查询当前波次已验未发的运单明细，分页
