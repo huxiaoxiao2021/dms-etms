e9ae4072f01a1b6034e064d1436330e2ade8732d
吕远 <lvyuan21@jd.com>
2022-11-17 15:14:50 +0800

    1.feat:去掉ConsumerLogFilter和ProviderLogFilter 2.feat:dms_package_status取消发货消息修改逻辑代码还原

--- bluedragon-distribution-core/src/main/java/com/jd/bluedragon/distribution/delivery/processor/DeliveryBaseHandler.java
+++ bluedragon-distribution-core/src/main/java/com/jd/bluedragon/distribution/delivery/processor/DeliveryBaseHandler.java
@@ -740,7 +740,6 @@ public abstract class DeliveryBaseHandler implements IDeliveryBaseHandler {
         status.setOperatorId(tSendM.getUpdateUserCode());
         status.setRemark("取消发货，批次号为：" +sendDetail.getSendCode());
         status.setCreateSiteCode(tSendM.getCreateSiteCode());
-        status.setSendCode(sendDetail.getSendCode());
 
         BaseStaffSiteOrgDto dto = baseMajorManager.getBaseSiteBySiteId(tSendM.getCreateSiteCode());
 
