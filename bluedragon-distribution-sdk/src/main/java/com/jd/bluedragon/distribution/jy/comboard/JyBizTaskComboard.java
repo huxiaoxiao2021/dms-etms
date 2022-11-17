e559ebbeabecb9a5f0144d490df02b53511db09d
weixiaofeng12 <weixiaofeng12@jd.com>
2022-11-17 16:54:08 +0800

    调整实体

---
+++ bluedragon-distribution-sdk/src/main/java/com/jd/bluedragon/distribution/jy/comboard/JyBizTaskComboard.java
@@ -0,0 +1,195 @@
+package com.jd.bluedragon.distribution.jy.comboard;
+
+import java.util.Date;
+
+public class JyBizTaskComboard {
+    private Long id;
+
+    private String bizId;
+
+    private String boardCode;
+
+    private String sendCode;
+
+    private Integer status;
+
+    private Long startSiteId;
+
+    private String startSiteName;
+
+    private Long endSiteId;
+
+    private String endSiteName;
+
+    private String createUserErp;
+
+    private String createUserName;
+
+    private String updateUserErp;
+
+    private String updateUserName;
+
+    private Date createTime;
+
+    private Date updateTime;
+
+    private Boolean yn;
+
+    private Date ts;
+
+    private Boolean bulkFlag;
+
+    private Integer count;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getBizId() {
+        return bizId;
+    }
+
+    public void setBizId(String bizId) {
+        this.bizId = bizId;
+    }
+
+    public String getBoardCode() {
+        return boardCode;
+    }
+
+    public void setBoardCode(String boardCode) {
+        this.boardCode = boardCode;
+    }
+
+    public String getSendCode() {
+        return sendCode;
+    }
+
+    public void setSendCode(String sendCode) {
+        this.sendCode = sendCode;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public Long getStartSiteId() {
+        return startSiteId;
+    }
+
+    public void setStartSiteId(Long startSiteId) {
+        this.startSiteId = startSiteId;
+    }
+
+    public String getStartSiteName() {
+        return startSiteName;
+    }
+
+    public void setStartSiteName(String startSiteName) {
+        this.startSiteName = startSiteName;
+    }
+
+    public Long getEndSiteId() {
+        return endSiteId;
+    }
+
+    public void setEndSiteId(Long endSiteId) {
+        this.endSiteId = endSiteId;
+    }
+
+    public String getEndSiteName() {
+        return endSiteName;
+    }
+
+    public void setEndSiteName(String endSiteName) {
+        this.endSiteName = endSiteName;
+    }
+
+    public String getCreateUserErp() {
+        return createUserErp;
+    }
+
+    public void setCreateUserErp(String createUserErp) {
+        this.createUserErp = createUserErp;
+    }
+
+    public String getCreateUserName() {
+        return createUserName;
+    }
+
+    public void setCreateUserName(String createUserName) {
+        this.createUserName = createUserName;
+    }
+
+    public String getUpdateUserErp() {
+        return updateUserErp;
+    }
+
+    public void setUpdateUserErp(String updateUserErp) {
+        this.updateUserErp = updateUserErp;
+    }
+
+    public String getUpdateUserName() {
+        return updateUserName;
+    }
+
+    public void setUpdateUserName(String updateUserName) {
+        this.updateUserName = updateUserName;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    public Boolean getYn() {
+        return yn;
+    }
+
+    public void setYn(Boolean yn) {
+        this.yn = yn;
+    }
+
+    public Date getTs() {
+        return ts;
+    }
+
+    public void setTs(Date ts) {
+        this.ts = ts;
+    }
+
+    public Boolean getBulkFlag() {
+        return bulkFlag;
+    }
+
+    public void setBulkFlag(Boolean bulkFlag) {
+        this.bulkFlag = bulkFlag;
+    }
+
+    public Integer getCount() {
+        return count;
+    }
+
+    public void setCount(Integer count) {
+        this.count = count;
+    }
+}
