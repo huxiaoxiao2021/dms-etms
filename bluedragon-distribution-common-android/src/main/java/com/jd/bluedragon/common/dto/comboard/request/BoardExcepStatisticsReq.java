cb2080ffdc28a1a03a6af6b2f1adbeb885501488
weixiaofeng12 <weixiaofeng@jrmf360.com>
2022-11-18 17:53:02 +0800

    新增异常查询类型

--- bluedragon-distribution-common-android/src/main/java/com/jd/bluedragon/common/dto/comboard/request/BoardExcepStatisticsReq.java
+++ bluedragon-distribution-common-android/src/main/java/com/jd/bluedragon/common/dto/comboard/request/BoardExcepStatisticsReq.java
@@ -7,6 +7,10 @@ import java.io.Serializable;
 public class BoardExcepStatisticsReq extends BaseReq implements Serializable {
     private static final long serialVersionUID = 597519678244355595L;
     private String boardCode;
+    /**
+     * 异常类型
+     */
+    private Integer excepType;
     private Integer pageNo;
     private Integer pageSize;
 
@@ -32,5 +36,13 @@ public class BoardExcepStatisticsReq extends BaseReq implements Serializable {
 
     public void setPageSize(Integer pageSize) {
         this.pageSize = pageSize;
+    }
+
+    public Integer getExcepType() {
+        return excepType;
+    }
+
+    public void setExcepType(Integer excepType) {
+        this.excepType = excepType;
     }
 }
