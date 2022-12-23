21464897ae5c0de404f34b2e1ebbcef8e722c965
liuluntao1 <liuluntao1@jd.com>
2022-12-23 10:35:24 +0800

    积分1.0 (注释)

--- bluedragon-distribution-sdk/src/main/java/com/jd/bluedragon/distribution/integral/service/IntegralJsfService.java
+++ bluedragon-distribution-sdk/src/main/java/com/jd/bluedragon/distribution/integral/service/IntegralJsfService.java
@@ -1,22 +1,22 @@
-package com.jd.bluedragon.distribution.integral.service;
-
-
-import com.jd.bluedragon.common.dto.base.response.JdCResponse;
-import com.jd.bluedragon.common.dto.integral.request.IntegralRequest;
-import com.jd.bluedragon.common.dto.integral.response.JyIntegralDetailDTO;
-import com.jd.bluedragon.common.dto.integral.response.JyIntroductionDTO;
-
-/**
- * @author liuluntao1
- * @description
- * @date 2022/12/22
- */
-public interface IntegralJsfService {
-    JdCResponse<JyIntegralDetailDTO> getSimpleJyIntegralInfo(IntegralRequest request);
-
-    JdCResponse<JyIntegralDetailDTO> getJyBaseScoreDetail(IntegralRequest request);
-
-    JdCResponse<JyIntegralDetailDTO> getJyIntegralCoefficientDetail(IntegralRequest request);
-
-    JdCResponse<JyIntroductionDTO> getJyIntegralIntroduction();
-}
+//package com.jd.bluedragon.distribution.integral.service;
+//
+//
+//import com.jd.bluedragon.common.dto.base.response.JdCResponse;
+//import com.jd.bluedragon.common.dto.integral.request.IntegralRequest;
+//import com.jd.bluedragon.common.dto.integral.response.JyIntegralDetailDTO;
+//import com.jd.bluedragon.common.dto.integral.response.JyIntroductionDTO;
+//
+///**
+// * @author liuluntao1
+// * @description
+// * @date 2022/12/22
+// */
+//public interface IntegralJsfService {
+//    JdCResponse<JyIntegralDetailDTO> getSimpleJyIntegralInfo(IntegralRequest request);
+//
+//    JdCResponse<JyIntegralDetailDTO> getJyBaseScoreDetail(IntegralRequest request);
+//
+//    JdCResponse<JyIntegralDetailDTO> getJyIntegralCoefficientDetail(IntegralRequest request);
+//
+//    JdCResponse<JyIntroductionDTO> getJyIntegralIntroduction();
+//}
