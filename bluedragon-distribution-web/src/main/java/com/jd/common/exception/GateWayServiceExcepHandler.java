ec5df0d9378d7cf5ab510c3175d3f78118a990cc
weixiaofeng12 <weixiaofeng12@jd.com>
2023-01-09 19:35:14 +0800

    调整异常处理

--- bluedragon-distribution-web/src/main/java/com/jd/common/exception/GateWayServiceExcepHandler.java
+++ bluedragon-distribution-web/src/main/java/com/jd/common/exception/GateWayServiceExcepHandler.java
@@ -5,10 +5,9 @@ import com.jd.bluedragon.common.dto.base.response.MSCodeMapping;
 import com.jd.bluedragon.distribution.jy.exception.JyBizException;
 import com.jd.bluedragon.distribution.ministore.exception.MiniStoreBizException;
 import com.jd.bluedragon.enums.EnvEnum;
+import com.jd.bluedragon.utils.JsonHelper;
 import com.jd.bluedragon.utils.ObjectHelper;
-import com.jd.fastjson.JSONObject;
 import java.lang.reflect.Method;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import org.aspectj.lang.ProceedingJoinPoint;
@@ -28,54 +27,54 @@ import static com.jd.bluedragon.common.dto.base.response.JdCResponse.CODE_ERROR;
 @Aspect
 @Order(-1)
 public class GateWayServiceExcepHandler {
+    private static final Logger log = LoggerFactory.getLogger(GateWayServiceExcepHandler.class);
 
-  private static final Logger log = LoggerFactory.getLogger(GateWayServiceExcepHandler.class);
-
-  @Value("${app.config.runningMode}")
-  protected String env;
+    @Value("${app.config.runningMode}")
+    protected String env;
 
   @Around("execution(* com.jd.bluedragon.distribution.external.gateway.service.impl..*.*(..)) && @within(com.jd.bluedragon.common.UnifiedExceptionProcess)")
   public JdCResponse serviceExceptionHandler(ProceedingJoinPoint jp) {
     JdCResponse jdCResponse;
     try {
-      Class<?> targetClass = jp.getTarget().getClass();
+      jdCResponse = (JdCResponse) jp.proceed();
+    } catch (Throwable throwable) {
+      Map<String, Object> params =getRequestArgs(jp);
+      log.error("UnifiedExceptionProcess catch exception：{}，requestArgs：{}", throwable,JsonHelper.toJson(params));
+            if (throwable instanceof MiniStoreBizException) {
+                MiniStoreBizException exception = (MiniStoreBizException) throwable;
+                return new JdCResponse(exception.getCode(), exception.getMessage());
+            }
+            if (throwable instanceof JyBizException) {
+                JyBizException exception = (JyBizException) throwable;
+                if (ObjectHelper.isNotNull(exception.getCode())){
+                    return new JdCResponse(exception.getCode(), exception.getMessage());
+                }
+                return new JdCResponse(CODE_ERROR, exception.getMessage());
+            }
+            if (EnvEnum.TEST.getCode().equals(env) && throwable instanceof Exception){
+                return new JdCResponse(CODE_ERROR, throwable.getMessage());
+            }
+            return new JdCResponse(MSCodeMapping.UNKNOW_ERROR.getCode(), MSCodeMapping.UNKNOW_ERROR.getMessage());
+        }
+        return jdCResponse;
+    }
+
+  private Map<String, Object> getRequestArgs(ProceedingJoinPoint jp) {
+    Map<String, Object> params = new HashMap();
+    try {
       MethodSignature ms = (MethodSignature) jp.getSignature();
       Method method =ms.getMethod();
-      log.info("GateWayServiceExcepHandler===method："+method+"===");
-      String targetClassName = targetClass.getName();
-      log.info("GateWayServiceExcepHandler===targetClassName："+targetClassName+"===");
-      Method targetMethod = targetClass.getDeclaredMethod(ms.getName(), ms.getParameterTypes());
-      log.info("GateWayServiceExcepHandler===targetMethod："+targetMethod+"===");
+      params.put("method",method.getName());
+      //Class<?> targetClass = jp.getTarget().getClass();
+      //Method targetMethod = targetClass.getDeclaredMethod(ms.getName(), ms.getParameterTypes());
       Object[] args = jp.getArgs();
       String[] names = ((CodeSignature) jp.getSignature()).getParameterNames();
-      Map<String, Object> params = new HashMap();
       for (int i = 0; i < names.length; i++) {
         params.put(names[i], args[i]);
       }
-
-      log.info("GateWayServiceExcepHandler请求参数：{}",JSONObject.toJSONString(params));
-
-      jdCResponse = (JdCResponse) jp.proceed();
-    } catch (Throwable throwable) {
-      log.error("GateWayServiceExcepHandler catch exception:", throwable);
-
-      if (throwable instanceof MiniStoreBizException) {
-        MiniStoreBizException exception = (MiniStoreBizException) throwable;
-        return new JdCResponse(exception.getCode(), exception.getMessage());
-      }
-      if (throwable instanceof JyBizException) {
-        JyBizException exception = (JyBizException) throwable;
-        if (ObjectHelper.isNotNull(exception.getCode())) {
-          return new JdCResponse(exception.getCode(), exception.getMessage());
-        }
-        return new JdCResponse(CODE_ERROR, exception.getMessage());
-      }
-      if (EnvEnum.TEST.getCode().equals(env) && throwable instanceof Exception) {
-        return new JdCResponse(CODE_ERROR, throwable.getMessage());
-      }
-      return new JdCResponse(MSCodeMapping.UNKNOW_ERROR.getCode(),
-          MSCodeMapping.UNKNOW_ERROR.getMessage());
+    } catch (Exception e) {
+      log.error("统一异常处理获取请求参数异常",e);
     }
-    return jdCResponse;
+    return params;
   }
 }
