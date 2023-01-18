d285fa2ed3432d90d61bd3ba43320fa8877c23e2
fanggang7 <fanggang7@jd.com>
2023-01-18 17:15:20 +0800

    feat: 得物禁止返调度 提示语编码冲突问题

--- bluedragon-distribution-core/src/main/java/com/jd/bluedragon/core/hint/constants/HintCodeConstants.java
+++ bluedragon-distribution-core/src/main/java/com/jd/bluedragon/core/hint/constants/HintCodeConstants.java
@@ -968,5 +968,5 @@ public class HintCodeConstants {
     public static String JY_MACHINE_CALIBRATE_TASK_CLOSED_AND_NOT_OVER_2_HINT = "80001";
 
     // 此单禁止操作返调度，需由本营业部完成派送，如有疑问可进咚咚群咨询（10201566979）
-    public static String TERMIANL_RE_SORT_DEWU_FORBID = "40018";
+    public static String TERMIANL_RE_SORT_DEWU_FORBID = "40022";
 }
