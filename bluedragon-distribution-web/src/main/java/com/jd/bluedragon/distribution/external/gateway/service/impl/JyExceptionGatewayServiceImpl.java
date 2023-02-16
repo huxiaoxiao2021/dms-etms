5fab4ed618c413c6d7effdbb1d0664ac8eab10eb
chenyaguo <chenyaguo@jd.com>
2023-02-16 10:57:13 +0800

    三无优化

--- bluedragon-distribution-web/src/main/java/com/jd/bluedragon/distribution/external/gateway/service/impl/JyExceptionGatewayServiceImpl.java
+++ bluedragon-distribution-web/src/main/java/com/jd/bluedragon/distribution/external/gateway/service/impl/JyExceptionGatewayServiceImpl.java
@@ -171,4 +171,10 @@ public class JyExceptionGatewayServiceImpl implements JyExceptionGatewayService 
         return result;
     }
 
+    @Override
+    public JdCResponse updateJyBizTaskExceptionOutOfDate() {
+        jyExceptionService.updateJyBizTaskExceptionOutOfDate();
+        return new JdCResponse(200,"success");
+    }
+
 }
