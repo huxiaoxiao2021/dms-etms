9b86a2d8a86ed4e587fb4f35ff236f76b4f62b2c
weixiaofeng12 <weixiaofeng12@jd.com>
2023-03-02 14:14:43 +0800

    自动换识别二次组板

--- bluedragon-distribution-web/src/main/java/com/jd/bluedragon/distribution/board/service/SortBoardJsfServiceImpl.java
+++ bluedragon-distribution-web/src/main/java/com/jd/bluedragon/distribution/board/service/SortBoardJsfServiceImpl.java
@@ -223,8 +223,8 @@ public class SortBoardJsfServiceImpl implements SortBoardJsfService {
             com.jd.bluedragon.common.dto.base.request.OperatorInfo operatorInfo = initOperatorInfo(request.getOperatorInfo());
     		OperatorData operatorData = new OperatorData();
     		operatorData.setOperatorTypeCode(OperatorTypeEnum.AUTO_MACHINE.getCode());
-    		operatorData.setOperatorId(request.getMachineCode());    		
-            
+    		operatorData.setOperatorId(request.getMachineCode());
+
             virtualBoardService.sendWaybillTrace(operatorData,request.getBarcode(), operatorInfo, request.getBoard().getCode(),
                     request.getBoard().getDestination(), WaybillStatus.WAYBILL_TRACK_BOARD_COMBINATION,
                     request.getBizSource());
@@ -311,6 +311,9 @@ public class SortBoardJsfServiceImpl implements SortBoardJsfService {
         req.setDestinationId(request.getBoard().getDestinationId());
         req.setEndSiteName(request.getBoard().getDestination());
         req.setSendCode(request.getBoard().getSendCode());
+        if (request.getCancelLast()){
+            req.setCancelLastSend(true);
+        }
         return req;
     }
 
@@ -707,7 +710,7 @@ public class SortBoardJsfServiceImpl implements SortBoardJsfService {
         domain.setCreateTime(new Date());
         domain.setOperateTime(sendM.getOperateTime());
         domain.setBoardCode(sendM.getBoardCode());
-        
+
         return domain;
     }
 
