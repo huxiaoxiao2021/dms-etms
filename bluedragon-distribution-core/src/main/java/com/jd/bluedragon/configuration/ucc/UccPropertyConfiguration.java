45f909bf81ebe995db89c4b6a04d6c7f2141f894
liuduo8 <liuduo3@jd.com>
2023-04-27 16:28:58 +0800

    feat:登录降级+基础资料JAR包升级

--- bluedragon-distribution-core/src/main/java/com/jd/bluedragon/configuration/ucc/UccPropertyConfiguration.java
+++ bluedragon-distribution-core/src/main/java/com/jd/bluedragon/configuration/ucc/UccPropertyConfiguration.java
@@ -1142,6 +1142,11 @@ public class UccPropertyConfiguration {
     private Boolean waybillSysNonExistPackageInterceptSwitch;
 
     /**
+     * 安卓登录可跳过不处理的逻辑降级开关
+     */
+    private Boolean pdaLoginSkipSwitch;
+
+    /**
      * 设备校准任务时长
      *  单位：毫秒
      */
@@ -2749,6 +2754,14 @@ public class UccPropertyConfiguration {
 
     public void setWaybillSysNonExistPackageInterceptSwitch(Boolean waybillSysNonExistPackageInterceptSwitch) {
         this.waybillSysNonExistPackageInterceptSwitch = waybillSysNonExistPackageInterceptSwitch;
+    }
+
+    public Boolean getPdaLoginSkipSwitch() {
+        return pdaLoginSkipSwitch;
+    }
+
+    public void setPdaLoginSkipSwitch(Boolean pdaLoginSkipSwitch) {
+        this.pdaLoginSkipSwitch = pdaLoginSkipSwitch;
     }
 
     public Long getMachineCalibrateTaskDuration() {
