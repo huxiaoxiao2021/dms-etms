32bc2cefdd3f9637afab46241bc0bad697b56344
chenyaguo <chenyaguo@jd.com>
2023-06-06 15:49:13 +0800

    三无任务升级

--- bluedragon-distribution-core/src/main/java/com/jd/bluedragon/distribution/jy/service/exception/impl/JySanwuExceptionServiceImpl.java
+++ bluedragon-distribution-core/src/main/java/com/jd/bluedragon/distribution/jy/service/exception/impl/JySanwuExceptionServiceImpl.java
@@ -516,8 +516,9 @@ public class JySanwuExceptionServiceImpl extends JyExceptionStrategy implements 
         //修改状态为 status处理中-processingStatus匹配中
         JyBizTaskExceptionEntity update = new JyBizTaskExceptionEntity();
         update.setBizId(mq.getBizId());
-        update.setStatus(JyExpStatusEnum.PROCESSING.getCode());
-        update.setProcessingStatus(JyBizTaskExceptionProcessStatusEnum.WAITING_MATCH.getCode());
+
+        update.setStatus(JyExpStatusEnum.TO_PROCESS.getCode());
+        update.setProcessingStatus(JyBizTaskExceptionProcessStatusEnum.PENDING_ENTRY.getCode());
         update.setHandlerErp(mq.getAssignHandlerErp());
         update.setUpdateUserErp(mq.getPrincipalErp());
         update.setUpdateTime(new Date());
