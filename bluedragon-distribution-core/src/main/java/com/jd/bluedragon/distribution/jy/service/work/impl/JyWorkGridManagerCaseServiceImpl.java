5ecd3396459a31d09e8321152aeab9f8638cd423
wuyoude <wuyoude@jd.com>
2023-06-09 15:09:57 +0800

    依赖名修改

--- bluedragon-distribution-core/src/main/java/com/jd/bluedragon/distribution/jy/service/work/impl/JyWorkGridManagerCaseServiceImpl.java
+++ bluedragon-distribution-core/src/main/java/com/jd/bluedragon/distribution/jy/service/work/impl/JyWorkGridManagerCaseServiceImpl.java
@@ -57,7 +57,7 @@ public class JyWorkGridManagerCaseServiceImpl implements JyWorkGridManagerCaseSe
 	private JyWorkGridManagerCaseItemService jyWorkGridManagerCaseItemService;
 	
 	@Autowired
-	@Qualifier("jyWorkGridManagerCaseItemService")
+	@Qualifier("jyAttachmentDetailService")
 	private JyAttachmentDetailService jyAttachmentDetailService;
 	
 	@Override
