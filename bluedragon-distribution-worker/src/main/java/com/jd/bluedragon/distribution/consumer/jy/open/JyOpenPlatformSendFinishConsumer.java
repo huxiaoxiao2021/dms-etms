4b59534e962da7a0fc8eab4b8642012eb630d2fe
fanggang7 <fanggang7@jd.com>
2023-06-16 12:35:59 +0800

    feat: 德邦转运融合 任务完成消息消费

--- bluedragon-distribution-worker/src/main/java/com/jd/bluedragon/distribution/consumer/jy/open/JyOpenPlatformSendFinishConsumer.java
+++ bluedragon-distribution-worker/src/main/java/com/jd/bluedragon/distribution/consumer/jy/open/JyOpenPlatformSendFinishConsumer.java
@@ -17,41 +17,41 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
- * 消费开放平台发货消息
+ * 消费开放平台发货完成消息
  * @author fanggang7
  * @time 2023-05-26 16:18:39 周五
  */
-@Service("jyOpenPlatformSendConsumer")
-public class JyOpenPlatformSendConsumer extends MessageBaseConsumer {
+@Service("jyOpenPlatformSendFinishConsumer")
+public class JyOpenPlatformSendFinishConsumer extends MessageBaseConsumer {
 
-    private static final Logger log = LoggerFactory.getLogger(JyOpenPlatformSendConsumer.class);
+    private static final Logger log = LoggerFactory.getLogger(JyOpenPlatformSendFinishConsumer.class);
 
     @Autowired
     private JyOpenSendExtraHandleService jyOpenSendExtraHandleService;
 
     @Override
     public void consume(Message message) throws Exception{
-        CallerInfo info = Profiler.registerInfo("JyOpenPlatformSendConsumer.consume", Constants.UMP_APP_NAME_DMSWORKER,false, true);
+        CallerInfo info = Profiler.registerInfo("JyOpenPlatformSendFinishConsumer.consume", Constants.UMP_APP_NAME_DMSWORKER,false, true);
         try {
             if (!JsonHelper.isJsonString(message.getText())) {
-                log.warn("JyOpenPlatformSendConsumer 发货明细消息-消息体非JSON格式，内容为【{}】", message.getText());
+                log.warn("JyOpenPlatformSendFinishConsumer 发货完成消息 消息体非JSON格式，内容为【{}】", message.getText());
                 return;
             }
             // 将mq消息体转换成SendDetail对象
             JYCargoOperateEntity jyCargoOperate = JsonHelper.fromJson(message.getText(), JYCargoOperateEntity.class);
             if (jyCargoOperate == null || StringHelper.isEmpty(jyCargoOperate.getBarcode())) {
-                log.warn("JyOpenPlatformSendConsumer:消息体[{}]转换实体失败或没有合法的包裹号",message.getText());
+                log.warn("JyOpenPlatformSendFinishConsumer:消息体[{}]转换实体失败或没有合法的包裹号",message.getText());
                 return;
             }
             // 发送任务完成和发货明细消息给城配
-            final Result<Boolean> handleResult = jyOpenSendExtraHandleService.afterOpenPlatformSend(jyCargoOperate);
+            final Result<Boolean> handleResult = jyOpenSendExtraHandleService.afterOpenPlatformSendFinish(jyCargoOperate);
             if (!handleResult.isSuccess()) {
-                log.error("JyOpenPlatformSendConsumer handle fail jyOpenSendExtraHandleService.afterOpenPlatformSend {}", JsonHelper.toJson(handleResult));
+                log.error("JyOpenPlatformSendFinishConsumer handle fail jyOpenSendExtraHandleService.afterOpenPlatformSendFinish {}", JsonHelper.toJson(handleResult));
                 throw new MessageException("处理失败 " +  handleResult.getMessage());
             }
         }catch(Exception e){
             Profiler.functionError(info);
-            log.error("JyOpenPlatformSendConsumer exception {}", message.getText(), e);
+            log.error("JyOpenPlatformSendFinishConsumer exception {}", message.getText(), e);
             throw e;
         }finally {
             Profiler.registerInfoEnd(info);
