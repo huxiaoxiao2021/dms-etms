0f15547d998988b27fc43f986d399c7eecf13762
chenyaguo <chenyaguo@jd.com>
2023-06-28 12:05:52 +0800

    电商特惠修改

--- bluedragon-distribution-core/src/main/java/com/jd/bluedragon/common/service/impl/WaybillCommonServiceImpl.java
+++ bluedragon-distribution-core/src/main/java/com/jd/bluedragon/common/service/impl/WaybillCommonServiceImpl.java
@@ -1031,7 +1031,9 @@ public class WaybillCommonServiceImpl implements WaybillCommonService {
         if(waybillExt != null && Constants.PRODUCT_TYPE_MEDICINE_SPECIAL_DELIVERY.equals(waybillExt.getProductType())){
             target.setTransportMode(TextConstants.COMMON_TEXT_MEDICINE_DELIVET);
         }
-        log.info("waybillExt.getProductType() -{}",waybillExt.getProductType());
+        if(waybillExt != null){
+            log.info("waybillExt.getProductType() -{}",waybillExt.getProductType());
+        }
         /*** 产品类型为ed-m-0059时:医药专送 */
         if(waybillExt != null && Constants.E_COMMERCE_SPECIAL_OFFER_SERVICE.equals(waybillExt.getProductType())){
             log.info("电商特惠----");
