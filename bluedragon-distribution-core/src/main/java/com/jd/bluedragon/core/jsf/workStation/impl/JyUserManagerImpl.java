e0a402cf9c8e18a3c6d7f00c1db1985582fecc81
wuyoude <wuyoude@jd.com>
2023-07-03 12:32:17 +0800

    jyUser类名修改

--- bluedragon-distribution-core/src/main/java/com/jd/bluedragon/core/jsf/workStation/impl/JyUserManagerImpl.java
+++ bluedragon-distribution-core/src/main/java/com/jd/bluedragon/core/jsf/workStation/impl/JyUserManagerImpl.java
@@ -6,10 +6,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.alibaba.fastjson.JSON;
 import com.jd.bluedragon.core.jsf.workStation.JyUserManager;
-import com.jdl.basic.api.domain.user.JyUser;
-import com.jdl.basic.api.domain.user.JyUserQueryCondition;
+import com.jdl.basic.api.domain.user.JyUserDto;
+import com.jdl.basic.api.domain.user.JyUserQueryDto;
 import com.jdl.basic.api.service.user.UserJsfService;
 import com.jdl.basic.common.utils.Result;
 
@@ -31,15 +30,15 @@ public class JyUserManagerImpl implements JyUserManager {
 	public Result<List<String>> queryUserListBySiteAndPosition(Integer siteCode,String userPositionCode,String userPositionName) {
 		Result<List<String>> result = Result.success();
 		List<String> userList = new ArrayList<>();
-		JyUserQueryCondition condition = new JyUserQueryCondition();
+		JyUserQueryDto condition = new JyUserQueryDto();
 		condition.setSiteCode(siteCode);
 		condition.setPositionCode(userPositionCode);
 		condition.setPositionName(userPositionName);
         try {
             log.info("获取岗位人员列表列表数据 queryUserListBySiteAndPosition： siteCode={},userPositionCode={},userPositionName={}",siteCode,userPositionCode,userPositionName);
-    		com.jd.dms.java.utils.sdk.base.Result<List<JyUser>> apiResult = userJsfService.queryUserListBySiteAndPosition(condition);
+    		com.jd.dms.java.utils.sdk.base.Result<List<JyUserDto>> apiResult = userJsfService.queryUserListBySiteAndPosition(condition);
     		if(apiResult != null && apiResult.getData() != null) {
-    			for(JyUser user: apiResult.getData()) {
+    			for(JyUserDto user: apiResult.getData()) {
     				userList.add(user.getUserErp());
     			}
     		}            
