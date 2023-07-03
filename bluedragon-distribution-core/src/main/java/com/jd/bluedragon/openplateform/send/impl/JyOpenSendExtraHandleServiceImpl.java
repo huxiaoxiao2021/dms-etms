33a68336d18bb09c25e6830da97ca597cc822db8
fanggang7 <fanggang7@jd.com>
2023-07-03 21:49:50 +0800

    feat: 德邦转运融合 打印日志

--- bluedragon-distribution-core/src/main/java/com/jd/bluedragon/openplateform/send/impl/JyOpenSendExtraHandleServiceImpl.java
+++ bluedragon-distribution-core/src/main/java/com/jd/bluedragon/openplateform/send/impl/JyOpenSendExtraHandleServiceImpl.java
@@ -150,14 +150,14 @@ public class JyOpenSendExtraHandleServiceImpl implements JyOpenSendExtraHandleSe
             if(CollectionUtils.isNotEmpty(boxDetailList)){
                 for (SendDetail sendDetail : boxDetailList) {
                     final JyTysSendPackageDetailDto jyTysSendPackageDetailDto = this.genJyTysSendPackageDetailDto(jyCargoOperate, sendDetail.getPackageBarcode(), currentOperate, user);
-                    // log.info("sendTysSendMq4Urban transportSendPackageProducer topic: {} send {}", transportSendPackageProducer.getTopic(), JSON.toJSONString(jyTysSendPackageDetailDto));
+                    log.info("sendTysSendMq4Urban transportSendPackageProducer topic: {} send {}", transportSendPackageProducer.getTopic(), JSON.toJSONString(jyTysSendPackageDetailDto));
                     sendDetailMQList.add(this.genMessage4JyTysSendPackageDetailDto(jyTysSendPackageDetailDto));
                 }
             }
         } else {
             final JyTysSendPackageDetailDto jyTysSendPackageDetailDto = this.genJyTysSendPackageDetailDto(jyCargoOperate, jyCargoOperate.getBarcode(), currentOperate, user);
             sendDetailMQList.add(this.genMessage4JyTysSendPackageDetailDto(jyTysSendPackageDetailDto));
-            // log.info("sendTysSendMq4Urban transportSendPackageProducer topic: {} send {}", transportSendPackageProducer.getTopic(), JSON.toJSONString(jyTysSendPackageDetailDto));
+            log.info("sendTysSendMq4Urban transportSendPackageProducer topic: {} send {}", transportSendPackageProducer.getTopic(), JSON.toJSONString(jyTysSendPackageDetailDto));
         }
         transportSendPackageProducer.batchSendOnFailPersistent(sendDetailMQList);
     }
@@ -277,11 +277,13 @@ public class JyOpenSendExtraHandleServiceImpl implements JyOpenSendExtraHandleSe
         sendDetailDto.setIsCancel(0);
         sendDetailDto.setCreateSiteCode(createSiteCode);
         final List<String> packageCodeList = sendDetailService.queryPackageCodeBySendCode(sendDetailDto);
-        // log.info("sendSendFinishMq4Urban packageCodeList {}", JsonHelper.toJson(packageCodeList));
+        if(log.isInfoEnabled()){
+            log.info("sendSendFinishMq4Urban packageCodeListSize {}", JsonHelper.toJson(packageCodeList));
+        }
         // 进一步分批，批量发送mq
         int batchSize = 50;
         int batchCount = Double.valueOf(Math.ceil(packageCodeList.size() / (double) batchSize)).intValue();
-        // log.info("sendSendFinishMq4Urban batchCount {}", batchCount);
+        log.info("sendSendFinishMq4Urban batchCount {}", batchCount);
         for (int i = 0; i < batchCount; i++) {
             int start = i * batchSize;
             int end = start + batchSize;
@@ -292,7 +294,7 @@ public class JyOpenSendExtraHandleServiceImpl implements JyOpenSendExtraHandleSe
             List<Message> sendDetailMQList = new ArrayList<>();
             for (String packageCode : packageCodeSubList) {
                 final JyTysSendPackageDetailDto jyTysSendPackageDetailDto = this.genJyTysSendPackageDetailDto(jyCargoOperate, packageCode, currentOperate, user);
-                // log.info("sendSendFinishMq4Urban transportSendPackageProducer topic: {} send {}", transportSendPackageProducer.getTopic(), JSON.toJSONString(jyTysSendPackageDetailDto));
+                log.info("sendSendFinishMq4Urban transportSendPackageProducer topic: {} send {}", transportSendPackageProducer.getTopic(), JSON.toJSONString(jyTysSendPackageDetailDto));
                 sendDetailMQList.add(this.genMessage4JyTysSendPackageDetailDto(jyTysSendPackageDetailDto));
             }
             transportSendPackageProducer.batchSendOnFailPersistent(sendDetailMQList);
