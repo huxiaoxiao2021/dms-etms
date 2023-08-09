e11439c9eb38156d184279576dad7eb94a0d3129
fanggang7 <fanggang7@jd.com>
2023-08-09 16:27:22 +0800

    feat: 杭州亚运会发货全程跟踪增加安检节点 改为真实节点

--- bluedragon-distribution-core/src/main/java/com/jd/bluedragon/distribution/waybill/domain/WaybillStatus.java
+++ bluedragon-distribution-core/src/main/java/com/jd/bluedragon/distribution/waybill/domain/WaybillStatus.java
@@ -274,7 +274,7 @@ public class WaybillStatus {
      * 操作码：20700
      * topic:0_7600
      */
-    public static final Integer WAYBILL_TRACK_SECURITY_CHECK = 6500;
+    public static final Integer WAYBILL_TRACK_SECURITY_CHECK = 20700;
 
     /**
      * https://cf.jd.com/pages/viewpage.action?pageId=1050064709
