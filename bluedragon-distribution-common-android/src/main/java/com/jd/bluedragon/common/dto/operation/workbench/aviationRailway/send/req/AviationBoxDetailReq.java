e4100907b7cd43527774a3ec4207c6e48ad88688
liwenji3 <liwenji3@jd.com>
2023-08-23 21:36:14 +0800

    空铁发货岗 箱号分页查询包裹明细

---
+++ bluedragon-distribution-common-android/src/main/java/com/jd/bluedragon/common/dto/operation/workbench/aviationRailway/send/req/AviationBoxDetailReq.java
@@ -0,0 +1,61 @@
+package com.jd.bluedragon.common.dto.operation.workbench.aviationRailway.send.req;
+
+import com.jd.bluedragon.common.dto.base.request.CurrentOperate;
+import com.jd.bluedragon.common.dto.base.request.User;
+
+/**
+ * @author liwenji
+ * @description 
+ * @date 2023-08-23 18:08
+ */
+public class AviationBoxDetailReq {
+    private User user;
+
+    private CurrentOperate currentOperate;
+
+    private Integer pageNumber;
+
+    private Integer pageSize;
+    
+    private String boxCode;
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public CurrentOperate getCurrentOperate() {
+        return currentOperate;
+    }
+
+    public void setCurrentOperate(CurrentOperate currentOperate) {
+        this.currentOperate = currentOperate;
+    }
+
+    public Integer getPageNumber() {
+        return pageNumber;
+    }
+
+    public void setPageNumber(Integer pageNumber) {
+        this.pageNumber = pageNumber;
+    }
+
+    public Integer getPageSize() {
+        return pageSize;
+    }
+
+    public void setPageSize(Integer pageSize) {
+        this.pageSize = pageSize;
+    }
+
+    public String getBoxCode() {
+        return boxCode;
+    }
+
+    public void setBoxCode(String boxCode) {
+        this.boxCode = boxCode;
+    }
+}
