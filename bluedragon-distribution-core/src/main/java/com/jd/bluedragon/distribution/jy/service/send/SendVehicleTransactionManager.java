3271e4a385690b2b5ad3f5708cc6ddc573a5032a
liwenji3 <liwenji3@jd.com>
2023-08-31 11:17:39 +0800

    任务完成单测

--- bluedragon-distribution-core/src/main/java/com/jd/bluedragon/distribution/jy/service/send/SendVehicleTransactionManager.java
+++ bluedragon-distribution-core/src/main/java/com/jd/bluedragon/distribution/jy/service/send/SendVehicleTransactionManager.java
@@ -281,7 +281,7 @@ public class SendVehicleTransactionManager {
         //
         jyBizTaskSendAviationPlanService.updateStatus(aviationPlanEntity);
 
-        if (JyBizTaskSendDetailStatusEnum.SEALED.getCode().equals(updateStatus)) {
+        if (JyBizTaskSendDetailStatusEnum.SEALED.getCode().equals(updateStatus.getCode())) {
             //封车节点做数据汇总
             statisticsSummaryService.insertSelective(summaryEntity);
         }
