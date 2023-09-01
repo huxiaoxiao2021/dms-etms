5c772ff99cc2c630f5a7a4f979ae42d204bcef91
hujiping1 <hujiping@jd.com>
2023-09-01 18:38:43 +0800

    港澳-获取老单信息

--- bluedragon-distribution-core/src/main/java/com/jd/bluedragon/distribution/print/waybill/handler/InternationalPdfPrintHandler.java
+++ bluedragon-distribution-core/src/main/java/com/jd/bluedragon/distribution/print/waybill/handler/InternationalPdfPrintHandler.java
@@ -3,6 +3,7 @@ package com.jd.bluedragon.distribution.print.waybill.handler;
 import com.google.common.collect.Lists;
 import com.jd.bluedragon.CloudPrintConstants;
 import com.jd.bluedragon.Constants;
+import com.jd.bluedragon.common.domain.WaybillExtVO;
 import com.jd.bluedragon.core.base.InternationalCloudPrintManager;
 import com.jd.bluedragon.distribution.api.request.WaybillPrintRequest;
 import com.jd.bluedragon.distribution.handler.InterceptHandler;
@@ -12,10 +13,12 @@ import com.jd.bluedragon.distribution.print.domain.WaybillPrintOperateTypeEnum;
 import com.jd.bluedragon.dms.utils.BusinessUtil;
 import com.jd.bluedragon.dms.utils.WaybillUtil;
 import com.jd.bluedragon.utils.NoticeUtils;
+import com.jd.etms.waybill.domain.WaybillExt;
 import com.jdl.print.dto.render.OutputConfigDTO;
 import com.jdl.print.dto.render.RenderResultDTO;
 import com.jdl.print.dto.render.ReprintDataDTO;
 import com.jdl.print.dto.render.ReprintQueryRenderDTO;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -30,6 +33,7 @@ import java.util.Objects;
  * @author hujiping
  * @date 2023/7/18 9:12 PM
  */
+@Slf4j
 @Service
 public class InternationalPdfPrintHandler implements InterceptHandler<WaybillPrintContext,String> {
 
@@ -39,15 +43,16 @@ public class InternationalPdfPrintHandler implements InterceptHandler<WaybillPri
     @Override
     public InterceptResult<String> handle(WaybillPrintContext context) {
         InterceptResult<String> interceptResult = context.getResult();
-        // 目前只处理港澳类运单
-        if(context.getWaybill().getWaybillExtVO() == null 
-                || !BusinessUtil.isGAWaybill(context.getWaybill().getWaybillExtVO().getStartFlowDirection(), context.getWaybill().getWaybillExtVO().getEndFlowDirection())){
-            return interceptResult;
-        }
         WaybillPrintRequest request = context.getRequest();
         // 目前只处理包裹补打、换单打印
         if(!Objects.equals(request.getOperateType(), WaybillPrintOperateTypeEnum.PACKAGE_AGAIN_PRINT.getType()) 
                 && !Objects.equals(request.getOperateType(), WaybillPrintOperateTypeEnum.SWITCH_BILL_PRINT.getType())){
+            return interceptResult;
+        }
+
+        // 目前只处理港澳类运单
+        if(!checkIsGA(context)){
+            log.warn("单号:{}为非港澳订单", request.getPackageBarCode());
             return interceptResult;
         }
         
@@ -63,6 +68,24 @@ public class InternationalPdfPrintHandler implements InterceptHandler<WaybillPri
         
         interceptResult.toBreak(WaybillPrintMessages.CODE_INTERNATIONAL_SUC, "云打印成功");
         return interceptResult;
+    }
+    
+    
+    private boolean checkIsGA(WaybillPrintContext context) {
+        WaybillExtVO waybillExtVO = context.getWaybill().getWaybillExtVO();
+        String startFlowDirection = waybillExtVO == null ? null : waybillExtVO.getStartFlowDirection();
+        String endFlowDirection = waybillExtVO == null ? null : waybillExtVO.getEndFlowDirection();
+        if(Objects.equals(context.getRequest().getOperateType(), WaybillPrintOperateTypeEnum.SWITCH_BILL_PRINT.getType())){
+            // 换单打印需判断老单是否为港澳单
+            if(context.getOldBigWaybillDto() != null && context.getOldBigWaybillDto().getWaybill() != null){
+                WaybillExt oldWaybillExt = context.getOldBigWaybillDto().getWaybill().getWaybillExt();
+                startFlowDirection = oldWaybillExt == null ? null : oldWaybillExt.getStartFlowDirection();
+                endFlowDirection = oldWaybillExt == null ? null : oldWaybillExt.getEndFlowDirection();
+            }else {
+                log.warn("根据新单号:{}未获取老单运单信息", context.getRequest().getPackageBarCode());
+            }
+        }
+        return BusinessUtil.isGAWaybill(startFlowDirection, endFlowDirection);
     }
 
     private String generateCloudPrintPdfUrl(WaybillPrintRequest request) {
