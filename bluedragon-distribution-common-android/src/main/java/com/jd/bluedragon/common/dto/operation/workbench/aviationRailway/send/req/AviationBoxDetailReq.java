bda04073bc6978b0f50b15783bd1865fd60e32fd
liwenji3 <liwenji3@jd.com>
2023-09-01 15:21:15 +0800

    修改实体类

--- bluedragon-distribution-common-android/src/main/java/com/jd/bluedragon/common/dto/operation/workbench/aviationRailway/send/req/AviationBoxDetailReq.java
+++ bluedragon-distribution-common-android/src/main/java/com/jd/bluedragon/common/dto/operation/workbench/aviationRailway/send/req/AviationBoxDetailReq.java
@@ -1,5 +1,6 @@
 package com.jd.bluedragon.common.dto.operation.workbench.aviationRailway.send.req;
 
+import com.jd.bluedragon.common.dto.base.request.BaseReq;
 import com.jd.bluedragon.common.dto.base.request.CurrentOperate;
 import com.jd.bluedragon.common.dto.base.request.User;
 
@@ -10,35 +11,15 @@ import java.io.Serializable;
  * @description 
  * @date 2023-08-23 18:08
  */
-public class AviationBoxDetailReq implements Serializable {
+public class AviationBoxDetailReq extends BaseReq implements Serializable {
 
     private static final long serialVersionUID = 4784612639942744950L;
-
-    private User user;
-
-    private CurrentOperate currentOperate;
 
     private Integer pageNumber;
 
     private Integer pageSize;
     
     private String boxCode;
-
-    public User getUser() {
-        return user;
-    }
-
-    public void setUser(User user) {
-        this.user = user;
-    }
-
-    public CurrentOperate getCurrentOperate() {
-        return currentOperate;
-    }
-
-    public void setCurrentOperate(CurrentOperate currentOperate) {
-        this.currentOperate = currentOperate;
-    }
 
     public Integer getPageNumber() {
         return pageNumber;
