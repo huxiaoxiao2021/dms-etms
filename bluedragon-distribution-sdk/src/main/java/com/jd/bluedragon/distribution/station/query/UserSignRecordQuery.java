a10166323e23a998c071bf0db3baa38d2a8e54b8
ext.xuwenrui3 <ext.xuwenrui3@jd.com>
2023-09-05 11:30:41 +0800

    修改补签查询

--- bluedragon-distribution-sdk/src/main/java/com/jd/bluedragon/distribution/station/query/UserSignRecordQuery.java
+++ bluedragon-distribution-sdk/src/main/java/com/jd/bluedragon/distribution/station/query/UserSignRecordQuery.java
@@ -161,23 +161,11 @@ public class UserSignRecordQuery extends BasePagerCondition {
 	private List<String> businessKeyList;
 
 	private List<Integer> jobCodeList;
-
-	/**
-	 * 身份正号模糊查询
-	 */
-	private String idCardLike;
+	
 	/**
 	 * 补签人姓名模糊查询
 	 */
 	private String userNameLike;
-
-	public String getIdCardLike() {
-		return idCardLike;
-	}
-
-	public void setIdCardLike(String idCardLike) {
-		this.idCardLike = idCardLike;
-	}
 
 	public String getUserNameLike() {
 		return userNameLike;
