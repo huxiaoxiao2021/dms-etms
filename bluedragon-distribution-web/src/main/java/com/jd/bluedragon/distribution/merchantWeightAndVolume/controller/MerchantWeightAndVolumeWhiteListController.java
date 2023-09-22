314f2da21ddb4e47096d136ba6d829195dc15ca9
hujiping1 <hujiping@jd.com>
2023-09-22 13:28:22 +0800

    刷数

--- bluedragon-distribution-web/src/main/java/com/jd/bluedragon/distribution/merchantWeightAndVolume/controller/MerchantWeightAndVolumeWhiteListController.java
+++ bluedragon-distribution-web/src/main/java/com/jd/bluedragon/distribution/merchantWeightAndVolume/controller/MerchantWeightAndVolumeWhiteListController.java
@@ -1,8 +1,11 @@
 package com.jd.bluedragon.distribution.merchantWeightAndVolume.controller;
 
+import com.google.common.collect.Lists;
 import com.jd.bluedragon.Constants;
 import com.jd.bluedragon.common.domain.ExportConcurrencyLimitEnum;
+import com.jd.bluedragon.common.dto.task.request.TaskPdaRequest;
 import com.jd.bluedragon.common.service.ExportConcurrencyLimitService;
+import com.jd.bluedragon.core.base.BaseMajorManager;
 import com.jd.bluedragon.distribution.base.controller.DmsBaseController;
 import com.jd.bluedragon.distribution.base.domain.InvokeResult;
 import com.jd.bluedragon.distribution.basic.DataResolver;
@@ -11,14 +14,21 @@ import com.jd.bluedragon.distribution.basic.PropertiesMetaDataFactory;
 import com.jd.bluedragon.distribution.merchantWeightAndVolume.domain.MerchantWeightAndVolumeCondition;
 import com.jd.bluedragon.distribution.merchantWeightAndVolume.domain.MerchantWeightAndVolumeDetail;
 import com.jd.bluedragon.distribution.merchantWeightAndVolume.service.MerchantWeightAndVolumeWhiteListService;
+import com.jd.bluedragon.distribution.sorting.domain.SortingBizSourceEnum;
+import com.jd.bluedragon.distribution.task.domain.Task;
 import com.jd.bluedragon.distribution.web.ErpUserClient;
+import com.jd.bluedragon.external.gateway.service.TaskGatewayService;
 import com.jd.bluedragon.utils.CsvExporterUtils;
 import com.jd.bluedragon.utils.DateHelper;
+import com.jd.bluedragon.utils.JsonHelper;
+import com.jd.ql.basic.dto.BaseStaffSiteOrgDto;
 import com.jd.ql.dms.common.domain.JdResponse;
 import com.jd.ql.dms.common.web.mvc.api.PagerResult;
 import com.jd.uim.annotation.Authorization;
 import com.jd.ump.annotation.JProEnum;
 import com.jd.ump.annotation.JProfiler;
+import lombok.Data;
+import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -57,6 +67,9 @@ public class MerchantWeightAndVolumeWhiteListController extends DmsBaseControlle
 
     @Autowired
     private ExportConcurrencyLimitService exportConcurrencyLimitService;
+
+    @Autowired
+    private BaseMajorManager baseMajorManager;
 
     /**
      * 返回主页面
@@ -125,6 +138,86 @@ public class MerchantWeightAndVolumeWhiteListController extends DmsBaseControlle
         }
         return response;
     }
+    
+    @Autowired
+    private TaskGatewayService taskGatewayService;
+
+    /**
+     * 刷数导入
+     * @return
+     */
+    @Authorization(Constants.DMS_WEB_TOOL_BUSIWEIGHTANDVOLUMEWHITELIST_R)
+    @RequestMapping(value = "/toImportBrush", method = RequestMethod.POST)
+    @ResponseBody
+    public JdResponse toImportBrush(@RequestParam("importExcelFile") MultipartFile file) {
+        if(log.isInfoEnabled()){
+            log.info("uploadExcelFile brush begin...");
+        }
+        JdResponse response = new JdResponse();
+        try {
+            String fileName = file.getOriginalFilename();
+            if (!fileName.endsWith(SUFFIX_NAME)) {
+                return new JdResponse(JdResponse.CODE_FAIL,"文件格式不对!");
+            }
+
+            DataResolver dataResolver = ExcelDataResolverFactory.getDataResolver(1);
+            List<SortingBrushVO> dataList
+                    = dataResolver.resolver(file.getInputStream(), SortingBrushVO.class, new PropertiesMetaDataFactory("/excel/brush.properties"));
+            if(CollectionUtils.isEmpty(dataList)){
+                log.warn("刷数excel解析失败!");
+                return response;
+            }
+            // 200一批分批处理
+            List<List<SortingBrushVO>> bathList = Lists.partition(dataList, 200);
+            bathList.forEach(singleList -> {
+                // singleList deal
+                singleList.forEach(item -> {
+                    // fill request
+                    fillRequest(item);
+                    // build task && send
+                    buildTaskAndSend(item);
+                });
+                // sleep
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            });
+            
+        } catch (Exception e) {
+            this.log.error("导入异常!",e);
+            return new JdResponse(JdResponse.CODE_FAIL, e.getMessage());
+        }
+        return response;
+    }
+
+    private void buildTaskAndSend(SortingBrushVO item) {
+        TaskPdaRequest pdaRequest = new TaskPdaRequest();
+        pdaRequest.setBoxCode(item.getBoxCode());
+        pdaRequest.setKeyword1(String.valueOf(item.getSiteCode()));
+        pdaRequest.setKeyword2(item.getBoxCode());
+        pdaRequest.setReceiveSiteCode(item.getReceiveSiteCode());
+        pdaRequest.setSiteCode(item.getSiteCode());
+        pdaRequest.setType(Task.TASK_TYPE_SORTING);
+        pdaRequest.setBody(JsonHelper.toJson(item));
+        if(log.isInfoEnabled()){
+            log.info("分拣刷数任务发送: {}", JsonHelper.toJson(item));
+        }
+        taskGatewayService.addTasksCommonly(pdaRequest);
+    }
+
+    private void fillRequest(SortingBrushVO item) {
+        item.setBizSource(SortingBizSourceEnum.ANDROID_SORTING.getCode());
+        item.setFeatureType(0);
+        item.setIsCancel(0);
+        item.setIsLoss(0);
+        BaseStaffSiteOrgDto operateSite = baseMajorManager.getBaseSiteBySiteId(item.getSiteCode());
+        item.setSiteName(operateSite == null ? null : operateSite.getSiteName());
+        BaseStaffSiteOrgDto receiveSite = baseMajorManager.getBaseSiteBySiteId(item.getReceiveSiteCode());
+        item.setReceiveSiteName(receiveSite == null ? null : receiveSite.getSiteName());
+    }
+
 
     /**
      * 导出
@@ -165,5 +258,23 @@ public class MerchantWeightAndVolumeWhiteListController extends DmsBaseControlle
         }
         return  result;
     }
-
+    
+    
+    @Data
+    static class SortingBrushVO {
+        private Integer bizSource;
+        private String boxCode;
+        private Integer businessType;
+        private Integer featureType;
+        private Integer isCancel;
+        private Integer isLoss;
+        private Integer receiveSiteCode;
+        private String receiveSiteName;
+        private String packageCode;
+        private Integer siteCode;
+        private String siteName;
+        private Integer userCode;
+        private String userName;
+        private String operateTime;
+    }
 }
