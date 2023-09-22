de7223283d4be60af3480726540ebdbed9e3a697
laoqingchang1 <laoqingchang1@jd.com>
2023-09-22 15:24:51 +0800

    刷数逻辑开发

--- bluedragon-distribution-web/src/main/webapp/static/js/merchantWeightAndVolume/merchantWeightAndVolumeWhiteList.js
+++ bluedragon-distribution-web/src/main/webapp/static/js/merchantWeightAndVolume/merchantWeightAndVolumeWhiteList.js
@@ -1,6 +1,5 @@
 var queryUrl = '/merchantWeightAndVolume/whiteList/listData';
 var importUrl = '/merchantWeightAndVolume/whiteList/toImport';
-var importBrushUrl = '/merchantWeightAndVolume/whiteList/toImportBrush';
 var exportUrl = '/merchantWeightAndVolume/whiteList/toExport';
 $(function () {
 
@@ -196,12 +195,6 @@ $(function () {
         $('#improt_modal').modal('show');
     });
 
-    // 刷数导入
-    $('#btn_import_brush').click(function(){
-        $('#importExcelFile_brush').val(null);
-        $('#import_modal_brush').modal('show');
-    });
-
     //导出
     function initExport(tableInit) {
         $('#btn_export').click(function () {
@@ -236,7 +229,6 @@ $(function () {
     pageInit().init();
     initExport(tableInit());
     initImportExcel();
-    initImportBrushExcel();
 });
 
 //自定义确认框
@@ -382,48 +374,6 @@ function initImportExcel(){
             error:function(err){
                 Jd.alert("网络连接失败,稍后重试");
                 $('#btn_upload').attr("disabled",false);
-            }
-        });
-    });
-}
-
-function initImportBrushExcel(){
-    //上传按钮
-    $('#btn_upload_brush').on('click',function(e){
-        $('#btn_upload_brush').attr("disabled",true);
-        var inputValue = $('#importExcelFile_brush').val().trim();
-        var index1 = inputValue.lastIndexOf(".");
-        var index2 = inputValue.length;
-        var suffixName = inputValue.substring(index1+1,index2);
-        if(inputValue == ''){
-            Jd.alert('请先浏览文件在上传!');
-            $('#btn_upload_brush').attr("disabled",false);
-            return;
-        }
-        if(suffixName != 'xls'){
-            Jd.alert('请上传指定Excel文件!');
-            $('#btn_upload').attr("disabled",false);
-            return;
-        }
-        var form = document.getElementById('brush_upload_excel_form'),
-            formData = new FormData(form);
-        $.ajax({
-            url:importBrushUrl,
-            type:"post",
-            data:formData,
-            processData:false,
-            contentType:false,
-            success:function(res){
-                if(res && res.code == 200){
-                    Jd.alert("上传成功!");
-                }else{
-                    Jd.alert("上传失败!" + res.message);
-                }
-                $('#btn_upload_brush').attr("disabled",false);
-            },
-            error:function(err){
-                Jd.alert("网络连接失败,稍后重试");
-                $('#btn_upload_brush').attr("disabled",false);
             }
         });
     });
