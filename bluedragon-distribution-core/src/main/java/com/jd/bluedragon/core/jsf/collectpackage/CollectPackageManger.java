84fbe21a9cc8f6ebd612cda2cc1ce8da303638af
weixiaofeng12 <weixiaofeng12@jd.com>
2023-10-25 18:25:31 +0800

    抽象集包统计查询能力

--- bluedragon-distribution-core/src/main/java/com/jd/bluedragon/core/jsf/collectpackage/CollectPackageManger.java
+++ bluedragon-distribution-core/src/main/java/com/jd/bluedragon/core/jsf/collectpackage/CollectPackageManger.java
@@ -1,2 +1,40 @@
-package com.jd.bluedragon.core.jsf.collectpackage;public interface CollectPackageManger {
+package com.jd.bluedragon.core.jsf.collectpackage;
+
+import com.jd.bluedragon.common.dto.base.response.JdCResponse;
+import com.jd.bluedragon.common.dto.collectpackage.request.StatisticsUnderFlowQueryReq;
+import com.jd.bluedragon.common.dto.collectpackage.request.StatisticsUnderTaskQueryReq;
+import com.jd.bluedragon.common.dto.collectpackage.response.StatisticsUnderFlowQueryResp;
+import com.jd.bluedragon.common.dto.collectpackage.response.StatisticsUnderTaskQueryResp;
+import com.jd.bluedragon.core.jsf.collectpackage.dto.ListTaskStatisticDto;
+import com.jd.bluedragon.core.jsf.collectpackage.dto.ListTaskStatisticQueryDto;
+import com.jd.bluedragon.core.jsf.collectpackage.dto.StatisticsUnderTaskDto;
+import com.jd.bluedragon.core.jsf.collectpackage.dto.StatisticsUnderTaskQueryDto;
+
+public interface CollectPackageManger {
+
+    /**
+     * 查询单个集包任务统计信息
+     *
+     * @param dto 查询任务统计请求
+     * @return 查询任务统计响应
+     */
+    StatisticsUnderTaskDto queryTaskStatistic(StatisticsUnderTaskQueryDto dto);
+
+    /**
+     * 查询多个集包任务统计信息
+     *
+     * @param dto 查询任务统计请求
+     * @return 查询任务统计响应
+     */
+    ListTaskStatisticDto listTaskStatistic(ListTaskStatisticQueryDto dto);
+
+    /**
+     * 查询流向下包裹明细
+     *
+     * @param request 查询请求对象
+     * @return 查询响应对象
+     */
+    StatisticsUnderFlowQueryResp listPackageUnderFlow(StatisticsUnderFlowQueryReq request);
+
+
 }
--- bluedragon-distribution-core/src/main/java/com/jd/bluedragon/core/jsf/collectpackage/CollectPackageMangerImpl.java
+++ bluedragon-distribution-core/src/main/java/com/jd/bluedragon/core/jsf/collectpackage/CollectPackageMangerImpl.java
@@ -1,2 +1,25 @@
-package com.jd.bluedragon.core.jsf.collectpackage;public class CollectPackageMangerImpl {
+package com.jd.bluedragon.core.jsf.collectpackage;
+
+import com.jd.bluedragon.common.dto.collectpackage.request.StatisticsUnderFlowQueryReq;
+import com.jd.bluedragon.common.dto.collectpackage.response.StatisticsUnderFlowQueryResp;
+import com.jd.bluedragon.core.jsf.collectpackage.dto.ListTaskStatisticDto;
+import com.jd.bluedragon.core.jsf.collectpackage.dto.ListTaskStatisticQueryDto;
+import com.jd.bluedragon.core.jsf.collectpackage.dto.StatisticsUnderTaskDto;
+import com.jd.bluedragon.core.jsf.collectpackage.dto.StatisticsUnderTaskQueryDto;
+
+public class CollectPackageMangerImpl implements CollectPackageManger{
+    @Override
+    public StatisticsUnderTaskDto queryTaskStatistic(StatisticsUnderTaskQueryDto dto) {
+        return null;
+    }
+
+    @Override
+    public ListTaskStatisticDto listTaskStatistic(ListTaskStatisticQueryDto dto) {
+        return null;
+    }
+
+    @Override
+    public StatisticsUnderFlowQueryResp listPackageUnderFlow(StatisticsUnderFlowQueryReq request) {
+        return null;
+    }
 }
