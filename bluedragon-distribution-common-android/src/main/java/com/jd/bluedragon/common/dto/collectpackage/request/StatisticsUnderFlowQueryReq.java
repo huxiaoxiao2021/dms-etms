9a30a07c4f79d1d7cb223aba3e42e5cc5f6eb0a0
weixiaofeng12 <weixiaofeng12@jd.com>
2023-10-30 11:02:22 +0800

    小件集包查询任务流向下的包裹明细

--- bluedragon-distribution-common-android/src/main/java/com/jd/bluedragon/common/dto/collectpackage/request/StatisticsUnderFlowQueryReq.java
+++ bluedragon-distribution-common-android/src/main/java/com/jd/bluedragon/common/dto/collectpackage/request/StatisticsUnderFlowQueryReq.java
@@ -7,6 +7,26 @@ public class StatisticsUnderFlowQueryReq extends StatisticsUnderTaskQueryReq imp
 
     private Long endSiteId;
 
+    private Integer pageNo;
+
+    private Integer pageSize;
+
+    public Integer getPageNo() {
+        return pageNo;
+    }
+
+    public void setPageNo(Integer pageNo) {
+        this.pageNo = pageNo;
+    }
+
+    public Integer getPageSize() {
+        return pageSize;
+    }
+
+    public void setPageSize(Integer pageSize) {
+        this.pageSize = pageSize;
+    }
+
     public Long getEndSiteId() {
         return endSiteId;
     }
