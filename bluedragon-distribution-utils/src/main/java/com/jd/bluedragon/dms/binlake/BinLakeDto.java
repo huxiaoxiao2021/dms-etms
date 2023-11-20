9cbee048a6877245c2094e4789b84dccd0d0a772
luqinglin3 <luqinglin3@jd.com>
2023-11-20 16:07:45 +0800

    binlake抽成工具

--- bluedragon-distribution-utils/src/main/java/com/jd/bluedragon/dms/binlake/BinLakeDto.java
+++ bluedragon-distribution-utils/src/main/java/com/jd/bluedragon/dms/binlake/BinLakeDto.java
@@ -1,6 +1,5 @@
 package com.jd.bluedragon.dms.binlake;
 
-import lombok.Data;
 
 import java.util.List;
 
@@ -8,7 +7,6 @@ import java.util.List;
  * @author liwenji
  * @date 2022-12-04 11:40
  */
-@Data
 public class BinLakeDto {
 
     private String businessId;
@@ -24,4 +22,60 @@ public class BinLakeDto {
     private List<ColumnRecord> afterChangeOfColumns;
 
     private List<ColumnRecord> beforeChangeOfColumns;
+
+    public String getBusinessId() {
+        return businessId;
+    }
+
+    public void setBusinessId(String businessId) {
+        this.businessId = businessId;
+    }
+
+    public String getDatabase() {
+        return database;
+    }
+
+    public void setDatabase(String database) {
+        this.database = database;
+    }
+
+    public String getEventType() {
+        return eventType;
+    }
+
+    public void setEventType(String eventType) {
+        this.eventType = eventType;
+    }
+
+    public String getExecuteTime() {
+        return executeTime;
+    }
+
+    public void setExecuteTime(String executeTime) {
+        this.executeTime = executeTime;
+    }
+
+    public String getTableName() {
+        return tableName;
+    }
+
+    public void setTableName(String tableName) {
+        this.tableName = tableName;
+    }
+
+    public List<ColumnRecord> getAfterChangeOfColumns() {
+        return afterChangeOfColumns;
+    }
+
+    public void setAfterChangeOfColumns(List<ColumnRecord> afterChangeOfColumns) {
+        this.afterChangeOfColumns = afterChangeOfColumns;
+    }
+
+    public List<ColumnRecord> getBeforeChangeOfColumns() {
+        return beforeChangeOfColumns;
+    }
+
+    public void setBeforeChangeOfColumns(List<ColumnRecord> beforeChangeOfColumns) {
+        this.beforeChangeOfColumns = beforeChangeOfColumns;
+    }
 }
