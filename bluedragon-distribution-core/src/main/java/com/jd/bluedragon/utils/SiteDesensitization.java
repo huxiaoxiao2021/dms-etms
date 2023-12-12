bfdf2e3f5f5488cd1cbd3dcfa1bd414c1d985fbd
liangjiaqi6 <liangjiaqi6@jd.com>
2023-12-12 15:07:30 +0800

    toE脱敏

---
+++ bluedragon-distribution-core/src/main/java/com/jd/bluedragon/utils/SiteDesensitization.java
@@ -0,0 +1,22 @@
+package com.jd.bluedragon.utils;
+
+import com.jd.ql.basic.dto.BaseStaffSiteOrgDto;
+
+public class SiteDesensitization {
+
+    public static void desensitizeBaseStaffSiteOrgDto(BaseStaffSiteOrgDto baseStaffSiteOrgDto) {
+        if (baseStaffSiteOrgDto == null) {
+            return;
+        }
+        baseStaffSiteOrgDto.setAddress(null);
+        baseStaffSiteOrgDto.setSitePhone(null);
+        baseStaffSiteOrgDto.setMobilePhone1(null);
+        baseStaffSiteOrgDto.setStaffName(null);
+        baseStaffSiteOrgDto.setAccountNumber(null);
+        baseStaffSiteOrgDto.setErp(null);
+        baseStaffSiteOrgDto.setSitePhone(null);
+        baseStaffSiteOrgDto.setSiteContact(null);
+        baseStaffSiteOrgDto.setSitePhone(null);
+        baseStaffSiteOrgDto.setPhone(null);
+    }
+}
