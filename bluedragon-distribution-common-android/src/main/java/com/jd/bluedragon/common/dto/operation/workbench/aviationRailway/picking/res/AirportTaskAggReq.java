fc4cfbed9c845a3a4e9fb7e18d60bc8784822b88
laoqingchang1 <laoqingchang1@jd.com>
2023-12-18 13:55:23 +0800

    添加setter getter方法

--- bluedragon-distribution-common-android/src/main/java/com/jd/bluedragon/common/dto/operation/workbench/aviationRailway/picking/res/AirportTaskAggReq.java
+++ bluedragon-distribution-common-android/src/main/java/com/jd/bluedragon/common/dto/operation/workbench/aviationRailway/picking/res/AirportTaskAggReq.java
@@ -29,4 +29,52 @@ public class AirportTaskAggReq extends BaseReq implements Serializable {
     private Integer pageNum;
 
     private Integer pageSize;
+
+    public String getPickingNodeCode() {
+        return pickingNodeCode;
+    }
+
+    public void setPickingNodeCode(String pickingNodeCode) {
+        this.pickingNodeCode = pickingNodeCode;
+    }
+
+    public Boolean getNoTaskFlag() {
+        return noTaskFlag;
+    }
+
+    public void setNoTaskFlag(Boolean noTaskFlag) {
+        this.noTaskFlag = noTaskFlag;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public Integer getTaskType() {
+        return taskType;
+    }
+
+    public void setTaskType(Integer taskType) {
+        this.taskType = taskType;
+    }
+
+    public Integer getPageNum() {
+        return pageNum;
+    }
+
+    public void setPageNum(Integer pageNum) {
+        this.pageNum = pageNum;
+    }
+
+    public Integer getPageSize() {
+        return pageSize;
+    }
+
+    public void setPageSize(Integer pageSize) {
+        this.pageSize = pageSize;
+    }
 }
