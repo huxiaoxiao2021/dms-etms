3fdd7c1f347917fb692d7f30dd5263c691f30e86
fanggang7 <fanggang7@jd.com>
2023-12-25 14:35:09 +0800

    feat(箱号类型精简): 去除不必要的文件改动

--- bluedragon-distribution-sdk/src/main/java/com/jd/bluedragon/distribution/box/service/DmsBoxUpsertService.java
+++ bluedragon-distribution-sdk/src/main/java/com/jd/bluedragon/distribution/box/service/DmsBoxUpsertService.java
@@ -1,11 +1,8 @@
 package com.jd.bluedragon.distribution.box.service;
 
-import com.jd.bluedragon.distribution.api.request.box.CreateBoxReq;
-import com.jd.bluedragon.distribution.api.response.box.CreateBoxInfo;
 import com.jd.bluedragon.distribution.base.domain.InvokeResult;
 import com.jd.bluedragon.distribution.box.domain.BoxDto;
 import com.jd.bluedragon.distribution.box.domain.BoxGenReq;
-import com.jd.dms.java.utils.sdk.base.Result;
 
 /**
  * @ClassName DmsBoxUpsertService
@@ -21,5 +18,4 @@ public interface DmsBoxUpsertService {
      * @return
      */
     InvokeResult<BoxDto> generateBox(BoxGenReq req);
-
 }
