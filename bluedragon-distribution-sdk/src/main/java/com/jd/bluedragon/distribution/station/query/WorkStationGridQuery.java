366013b288b167e53435456f624f26cbcb93c997
chenlingfeng10 <chenlingfeng10@jd.com>
2024-01-09 15:05:30 +0800

    破损耗材debug

--- bluedragon-distribution-sdk/src/main/java/com/jd/bluedragon/distribution/station/query/WorkStationGridQuery.java
+++ bluedragon-distribution-sdk/src/main/java/com/jd/bluedragon/distribution/station/query/WorkStationGridQuery.java
@@ -79,6 +79,23 @@ public class WorkStationGridQuery extends BasePagerCondition {
 	 * 唯一业务编码
 	 */
 	private String businessKey;
+
+	/**
+	 * 省区编码
+	 */
+	private String provinceAgencyCode;
+	/**
+	 * 省区名称
+	 */
+	private String provinceAgencyName;
+	/**
+	 * 枢纽编码
+	 */
+	private String areaHubCode;
+	/**
+	 * 枢纽名称
+	 */
+	private String areaHubName;
 	
 	public Integer getOrgCode() {
 		return orgCode;
@@ -192,4 +209,35 @@ public class WorkStationGridQuery extends BasePagerCondition {
 		this.businessKey = businessKey;
 	}
 
+	public String getProvinceAgencyCode() {
+		return provinceAgencyCode;
+	}
+
+	public void setProvinceAgencyCode(String provinceAgencyCode) {
+		this.provinceAgencyCode = provinceAgencyCode;
+	}
+
+	public String getProvinceAgencyName() {
+		return provinceAgencyName;
+	}
+
+	public void setProvinceAgencyName(String provinceAgencyName) {
+		this.provinceAgencyName = provinceAgencyName;
+	}
+
+	public String getAreaHubCode() {
+		return areaHubCode;
+	}
+
+	public void setAreaHubCode(String areaHubCode) {
+		this.areaHubCode = areaHubCode;
+	}
+
+	public String getAreaHubName() {
+		return areaHubName;
+	}
+
+	public void setAreaHubName(String areaHubName) {
+		this.areaHubName = areaHubName;
+	}
 }
