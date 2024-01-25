bfb7ccbe5be99743c1b531ce9a9ff640fc3f578f
laoqingchang1 <laoqingchang1@jd.com>
2024-01-25 17:30:41 +0800

    跨夜场景bugfix

--- bluedragon-distribution-web/src/main/java/com/jd/bluedragon/distribution/external/service/impl/DmsUserScheduleServiceImpl.java
+++ bluedragon-distribution-web/src/main/java/com/jd/bluedragon/distribution/external/service/impl/DmsUserScheduleServiceImpl.java
@@ -81,8 +81,13 @@ public class DmsUserScheduleServiceImpl implements DmsUserScheduleService {
             // 今天排班记录  跨夜场景
             // 开始时间大于结束时间  证明是当天跨夜  允许放行
             if (scheduleDto.getStartTime().compareTo(scheduleDto.getEndTime()) > 0) {
-                response.setData(true);
-                return response;
+                boolean todayCheck = checkEntryTime(scheduleDto.getStartTime(), -allowHours);
+                if (todayCheck) {
+                    response.setData(true);
+                    return response;
+                }
+                scheduleTimes.add("今天跨夜" + scheduleDto.getStartTime() + Constants.SEPARATOR_HYPHEN + scheduleDto.getEndTime());
+                continue;
             }
             // 不是跨夜场景
             // 判断当前时间是否是在合理进入闸机时间范围
