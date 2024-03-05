b4c88fdecb96b95c0789dd1480627e2839907eb5
lvyuan21 <lvyuan21@jd.com>
2024-03-05 13:55:22 +0800

    1.feat:视频追溯-操作流水记录

---
+++ bluedragon-distribution-core/src/main/java/com/jd/bluedragon/distribution/waybill/domain/DmsOperateTrack.java
@@ -0,0 +1,38 @@
+package com.jd.bluedragon.distribution.waybill.domain;
+
+
+import com.jd.etms.waybill.dto.BdTraceDto;
+
+import java.io.Serializable;
+
+public class DmsOperateTrack implements Serializable {
+
+
+    private static final long serialVersionUID = 5029681388784883608L;
+
+    /**
+     * 操作流水表主键
+     */
+    private Long operateFlowId;
+
+    /**
+     * 全程跟踪详细内容
+     */
+    private BdTraceDto bdTraceDto;
+
+    public Long getOperateFlowId() {
+        return operateFlowId;
+    }
+
+    public void setOperateFlowId(Long operateFlowId) {
+        this.operateFlowId = operateFlowId;
+    }
+
+    public BdTraceDto getBdTraceDto() {
+        return bdTraceDto;
+    }
+
+    public void setBdTraceDto(BdTraceDto bdTraceDto) {
+        this.bdTraceDto = bdTraceDto;
+    }
+}
