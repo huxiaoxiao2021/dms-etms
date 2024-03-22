2b90c4ca346a2f5085c42a08b9314c8423a80c66
caozhixing3 <caozhixing3@jd.com>
2024-03-22 17:18:05 +0800

    feat(分拣称重): 增加类

---
+++ bluedragon-distribution-core/src/main/java/com/jd/bluedragon/external/gateway/service/WeightAndVolumeGatewayService.java
@@ -0,0 +1,12 @@
+package com.jd.bluedragon.external.gateway.service;
+
+/**
+ * @program: ql-dms-distribution
+ * @description:
+ * @author: caozhixing3
+ * @date: 2024-03-22 13:55
+ **/
+public interface WeightAndVolumeGatewayService {
+
+
+}
