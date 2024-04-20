de63e76d140612bde970facfc11983eec1cf15b7
fanggang7 <fanggang7@jd.com>
2024-04-20 22:14:39 +0800

    feat(灭霸保温箱二期): 发货、取消发货、验货发送保温箱物资消息

--- bluedragon-distribution-core/src/main/java/com/jd/bluedragon/distribution/material/enums/MaterialFlowActionDetailV2Enum.java
+++ bluedragon-distribution-core/src/main/java/com/jd/bluedragon/distribution/material/enums/MaterialFlowActionDetailV2Enum.java
@@ -10,17 +10,64 @@ import java.util.Objects;
  */
 public enum MaterialFlowActionDetailV2Enum {
 
+    /**
+     * 采购入库
+     */
     PURCHASE_STOCK_IN(10, "采购入库"),
+
+    /**
+     * 节点发出
+     */
     NODE_DELIVERY(20, "节点发出"),
+
+    /**
+     * 节点发出取消
+     */
     NODE_DELIVERY_CANCEL(22, "节点发出取消"),
+
+    /**
+     * 节点中转
+     */
     NODE_TRANSFER(25, "节点中转"),
+
+    /**
+     * 节点接收
+     */
     NODE_RECEIPT(30, "节点接收"),
+
+    /**
+     * 自动扫描
+     */
     AUTO_SCAN(40, "自动扫描"),
+
+    /**
+     * 节点逆向发出
+     */
     ALLOCATION_DELIVERY(50, "节点逆向发出"),
+
+    /**
+     * 节点逆向接收
+     */
     ALLOCATION_RECEIPT(60, "节点逆向接收"),
+
+    /**
+     * 报废
+     */
     SCRAP(70, "报废"),
+
+    /**
+     * 客户保留
+     */
     CUSTOMER_RETENTION(80, "客户保留"),
+
+    /**
+     * 退货
+     */
     RETURN(90, "退货"),
+
+    /**
+     * 破损退货换新
+     */
     DAMAGED_REPLACE(100, "破损退货换新");
 
     /**
