b9e46c71e82d69111c7aa1464e883c9e10eb4d66
zhengchengfa <zhengchengfa@jd.com>
2024-04-28 23:40:00 +0800

    拦截链路去除已解锁数据

--- bluedragon-distribution-core/src/main/java/com/jd/bluedragon/distribution/waybill/service/WaybillServiceImpl.java
+++ bluedragon-distribution-core/src/main/java/com/jd/bluedragon/distribution/waybill/service/WaybillServiceImpl.java
@@ -1105,7 +1105,13 @@ public class WaybillServiceImpl implements WaybillService {
             return claimDamagedCancelWaybill;
         }
 
-        return cancelWaybills.get(0);
+        //剔除掉被解锁数据
+        for(CancelWaybill cancelWaybill1 : cancelWaybills) {
+            if(CancelWaybill.BUSINESS_TYPE_LOCK.equals(cancelWaybill1.getBusinessType())) {
+                return cancelWaybill1;
+            }
+        }
+        return null;
     }
 
     /**
@@ -1131,13 +1137,7 @@ public class WaybillServiceImpl implements WaybillService {
             return claimDamagedCancelWaybill;
         }
 
-        //剔除掉被解锁数据
-        for(CancelWaybill cancelWaybill1 : cancelWaybills) {
-            if(CancelWaybill.BUSINESS_TYPE_LOCK.equals(cancelWaybill1.getBusinessType())) {
-                return cancelWaybill1;
-            }
-        }
-        return null;
+        return cancelWaybills.get(0);
     }
 
     /**
