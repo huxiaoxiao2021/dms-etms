bfe28382fd639187b65fe5980100fc10c099338d
luqinglin3 <luqinglin3@jd.com>
2024-05-08 17:57:35 +0800

    合并代码

--- bluedragon-distribution-sdk/src/main/java/com/jd/bluedragon/distribution/board/domain/BizSourceEnum.java
+++ bluedragon-distribution-sdk/src/main/java/com/jd/bluedragon/distribution/board/domain/BizSourceEnum.java
@@ -1,10 +1,25 @@
 package com.jd.bluedragon.distribution.board.domain;
 
 public enum BizSourceEnum {
+    /**
+     * pda
+     */
     PDA(1),
+    /**
+     * 自动化组板
+     */
     SORTING_MACHINE(2),
+    /**
+     * 打印客户端
+     */
     PRINT_CLIENT(3),
+    /**
+     * 自动化自动装笼
+     */
     SORTING_MACHINE_AUTO_CAGE(4),
+    /**
+     * pda自动装笼
+     */
     PDA_AUTO_CAGE(5);
     private int value;
 
