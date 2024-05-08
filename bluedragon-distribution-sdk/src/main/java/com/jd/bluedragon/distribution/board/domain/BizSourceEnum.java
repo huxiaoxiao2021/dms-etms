76a5bf14dcd2c28708c4916ae36443083ac8fed0
luqinglin3 <luqinglin3@jd.com>
2024-05-08 17:40:22 +0800

    调整逻辑

--- bluedragon-distribution-sdk/src/main/java/com/jd/bluedragon/distribution/board/domain/BizSourceEnum.java
+++ bluedragon-distribution-sdk/src/main/java/com/jd/bluedragon/distribution/board/domain/BizSourceEnum.java
@@ -3,7 +3,9 @@ package com.jd.bluedragon.distribution.board.domain;
 public enum BizSourceEnum {
     PDA(1),
     SORTING_MACHINE(2),
-    PRINT_CLIENT(3);
+    PRINT_CLIENT(3),
+    SORTING_MACHINE_AUTO_CAGE(4),
+    PDA_AUTO_CAGE(5);
     private int value;
 
     BizSourceEnum(int value) {
