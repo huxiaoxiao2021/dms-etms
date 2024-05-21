ef8bf866c85b8c1125949154a79fe4af2dc65d94
shirui73 <shirui73@jd.com>
2024-05-21 16:07:05 +0800

    人闸签退

---
+++ bluedragon-distribution-worker/src/main/java/com/jd/bluedragon/distribution/consumer/user/AttendDetailChangeGateTopicConsumer.java
@@ -0,0 +1,47 @@
+package com.jd.bluedragon.distribution.consumer.user;
+
+import com.jd.bluedragon.core.message.base.MessageBaseConsumer;
+import com.jd.bluedragon.distribution.api.utils.JsonHelper;
+import com.jd.bluedragon.distribution.command.JdResult;
+import com.jd.bluedragon.distribution.consumer.record.DmsHasnoPresiteWaybillMqListener;
+import com.jd.bluedragon.distribution.station.entity.AttendDetailChangeTopicData;
+import com.jd.bluedragon.distribution.station.service.UserSignRecordService;
+import com.jd.jmq.common.message.Message;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * 人闸签退
+ */
+@Service("attendDetailChangeGateTopicConsumer")
+public class AttendDetailChangeGateTopicConsumer extends MessageBaseConsumer {
+
+    private static final Logger log = LoggerFactory.getLogger(DmsHasnoPresiteWaybillMqListener.class);
+
+    @Autowired
+    private UserSignRecordService userSignRecordService;
+    
+    @Override
+    public void consume(Message message) throws Exception {
+    	AttendDetailChangeTopicData mqData = JsonHelper.fromJson(message.getText(), AttendDetailChangeTopicData.class);
+    	if(log.isDebugEnabled()) {
+    		log.debug("AttendDetailChangeGateTopicConsumer：[{}-{}]:[{}]",message.getTopic(),message.getBusinessId(),message.getText());
+    	}
+    	if(mqData == null){
+    		log.warn("消息转换失败！[{}-{}]:[{}]",message.getTopic(),message.getBusinessId(),message.getText());
+    		return;
+    	}
+//		JdResult<Integer> result0 = userSignRecordService.autoHandleSignOutByAttendJmq(mqData);
+		log.info("SR 开始消费 mqData:{}", mqData);
+		JdResult<Integer> result = userSignRecordService.autoHandleSignOutByAttendGateJmq(mqData);
+
+    	//处理异常-重试
+    	if(result != null && result.isError()) {
+    		log.error("AttendDetailChangeGateTopicConsumer-error:[{}-{}]:[{}] 异常原因:[{}]",message.getTopic(),message.getBusinessId(),message.getText(),result.getMessage());
+    		throw new Exception("AttendDetailChangeGateTopicConsumer-error:" + result.getMessage());
+    	}
+    }
+    
+}
